786fd403400dcfed0a1e224002681fbe6dee6332
redu <guilhermec@redu.com.br>
2009-11-03 17:37:48 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@43 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ db/migrate/20091029121124_create_videos.rb
@@ -0,0 +1,15 @@
+class CreateVideos < ActiveRecord::Migration
+  def self.up
+    create_table :videos do |t|
+      t.string :source_content_type
+      t.string :source_file_name
+      t.integer :source_file_size
+      t.string :state
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :videos
+  end
+end
