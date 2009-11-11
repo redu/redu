f7c977e91225df9d86921f8330c1e63d1cc05ab6
redu <guilhermec@redu.com.br>
2009-11-11 19:39:12 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@66 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ db/migrate/20091110202707_add_owner_course.rb
@@ -0,0 +1,9 @@
+class AddOwnerCourse < ActiveRecord::Migration
+  def self.up
+    add_column :courses, :owner, :integer, :null => false
+  end
+
+  def self.down
+    remove_column :courses, :owner
+  end
+end
