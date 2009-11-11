4b4a59ba6a32e0799ee5a7ba2bb7a220f08e39c2
redu <guilhermec@redu.com.br>
2009-11-11 19:40:13 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@67 b630d61d-3281-de11-9aae-0019d1013b4f

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,11 +3,13 @@ class Course < ActiveRecord::Base
   # PLUGINS
   acts_as_commentable
   ajaxful_rateable :stars => 5#, :dimensions => [:speed, :beauty, :price]
+ 
   
   # ASSOCIATIONS
   has_and_belongs_to_many :subjects
   has_and_belongs_to_many :resources
   has_many :acess_key
+  belongs_to :owner , :class_name => "User" , :foreign_key => "owner"
   
   # VALIDATIONS
   validates_presence_of :name
