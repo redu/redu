009188cac3f9f26003a1942e337b9539bc3a210d
guiocavalcanti <contato@guilhermecavalcanti.com>
2009-12-15 17:18:57 +0000

    Resolvendo bugs do wizard de criação de aulas.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@79 b630d61d-3281-de11-9aae-0019d1013b4f

--- app/controllers/create_course_controller.rb
+++ app/controllers/create_course_controller.rb
@@ -1,47 +1,46 @@
 class CreateCourseController < BaseController
-	
-	act_wizardly_for :course,
-		:completed => '/courses',
-		:canceled => '/main',
-		:form_date => :sandbox,
-		:persist_model => :per_page
-		
-		on_get(:step1) do
-			@course.owner = current_user
-		end
-		
-		on_get(:step2) do
-			@resource = Resource.new
-		end
-		
-		on_post(:step2) do
-			if params[:external_resource_type] == "media" then
-				params[:external_resource] = nil
-			else
-				params[:media] = nil
-			end
-			
-		  resource = Resource.new(:title => params[:title], 
-		  	:description => params[:description],
-		  	:owner => current_user,
-		  	:external_resource => params[:external_resource],
-		  	:external_resource_type => params[:external_resource_type],
-		  	:media => params[:media])
-		  
-		  if resource.save! 
-		  	if resource.video?
-		  		resource.convert
-		  	end
-		 	end
-			
-			CourseResourceAssociation.new(:course_id => @course.id, 
-				:resource_id => resource.id, 
-				:main_resource => true).save
-				
-			
-		end
-		
-		def current_user 
-			User.find(session[:user])
-		end
+
+  act_wizardly_for :course,
+  :completed => '/courses',
+  :canceled => '/main',
+  :form_date => :sandbox,
+  :persist_model => :per_page
+
+  on_get(:step1) do
+    @course.owner = current_user
+  end
+
+  on_get(:step2) do
+    @resource = Resource.new
+  end
+
+  on_post(:step2) do
+    if params[:external_resource_type] == "media" then
+      params[:external_resource] = nil
+    else
+      params[:media] = nil
+    end
+
+    resource = Resource.new(:title => params[:title],
+    :description => params[:description],
+    :owner => current_user,
+    :external_resource => params[:external_resource],
+    :external_resource_type => params[:external_resource_type],
+    :media => params[:media])
+
+    resource.save
+
+    if resource.save!
+      if resource.video?
+        resource.convert
+      end
+    end
+
+    CourseResourceAssociation.new(:course_id => @course.id,
+    :resource_id => resource.id,
+    :main_resource => true).save
+
+  end
+
 end
+
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,11 +1,13 @@
 class Course < ActiveRecord::Base
 
+  # VALIDATIONS
+  validates_presence_of :name
+  
   # PLUGINS
   acts_as_commentable
   ajaxful_rateable :stars => 5#, :dimensions => [:speed, :beauty, :price]
   validation_group :step1, :fields=>[:name, :description, :owner]
   validation_group :step2, :fields=>[:courses_resources]
- 
   
   # ASSOCIATIONS
   has_and_belongs_to_many :subjects
@@ -14,8 +16,5 @@ class Course < ActiveRecord::Base
   has_many :course_resource_association
 	has_many :resources, :through => :course_resource_association
   belongs_to :owner , :class_name => "User" , :foreign_key => "owner"
-  
-  # VALIDATIONS
-  #validates_presence_of :name
   
 end
