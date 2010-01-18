1aa86b7288407cf0b477471cc73743df6ecece78
redu <guilhermec@redu.com.br>
2010-01-18 19:02:47 +0000

    Coluna a mais (owner) na tabela school
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@102 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100118173123_add_owner_to_school.rb
+++ db/migrate/20100118173123_add_owner_to_school.rb
@@ -1,9 +1,9 @@
 class AddOwnerToSchool < ActiveRecord::Migration
   def self.up
-    
-    
+    add_column :schools, :owner, :integer
   end
-
+  
   def self.down
+    remove_column :schools, :owner
   end
 end
