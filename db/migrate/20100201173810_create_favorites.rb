181f4131ab515aed25a62b90f734aa8d9435259e
redu <guilhermec@redu.com.br>
2010-02-10 18:51:49 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@148 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100201173810_create_favorites.rb
+++ db/migrate/20100201173810_create_favorites.rb
@@ -1,5 +1,6 @@
 class CreateFavorites < ActiveRecord::Migration
   def self.up
+    drop_table :favorites
     create_table :favorites do |t|
       t.string :favorite_type
       t.integer :favorite_id
