674ce9a594aca978943c6b1c66aa03bec044552b
ksiomelo <cassio.ufpe@gmail.com>
2010-02-11 20:01:12 +0000

    atualizações na interface
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@164 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ lib/pagination_list_link_renderer.rb
@@ -0,0 +1,36 @@
+class PaginationListLinkRenderer < WillPaginate::LinkRenderer
+
+  def to_html
+    links = @options[:page_links] ? windowed_links : []
+
+    links.unshift(page_link_or_span(@collection.previous_page, 'nav', @options[:previous_label]))
+    links.push(page_link_or_span(@collection.next_page, 'nav', @options[:next_label]))
+
+    html = links.join(@options[:separator])
+    @options[:container] ? @template.content_tag(:ul, html, html_attributes) : html
+  end
+
+protected
+
+  def windowed_links
+    visible_page_numbers.map { |n| page_link_or_span(n, (n == current_page ? 'current' : 'number')) }
+  end
+
+  def page_link_or_span(page, span_class, text = nil)
+    text ||= page.to_s
+    if page && page != current_page
+      page_link(page, text, :class => span_class)
+    else
+      page_span(page, text, :class => span_class)
+    end
+  end
+
+  def page_link(page, text, attributes = {})
+    @template.content_tag(:li, @template.link_to(text, url_for(page), attributes))
+  end
+
+  def page_span(page, text, attributes = {})
+    @template.content_tag(:li, text, attributes)
+  end
+
+end
