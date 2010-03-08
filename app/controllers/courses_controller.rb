0457ae306ad7badc6d429696ebfba25ae9b024e1
ksiomelo <cassio.ufpe@gmail.com>
2010-03-08 18:28:44 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@275 b630d61d-3281-de11-9aae-0019d1013b4f

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -117,6 +117,11 @@ class CoursesController < BaseController
     
     @course = Course.find(params[:id])
     
+    if not @course.can_be_deleted_by(current_user)
+      flash[:error] = "Você não tem acesso a este vídeo"
+      redirect_to courses_path
+    else 
+    
     #comentários
     @comments  = @course.comments.find(:all, :limit => 10, :order => 'created_at DESC')
     
@@ -136,6 +141,7 @@ class CoursesController < BaseController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @course }
+    end
     end
       
   end
