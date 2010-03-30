8e54f434962b687ee12343c749454fe48b890afe
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-03-30 20:05:34 +0000

    Removendo migrate invalido,
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@300 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100226212641_rename_title_to_name_in_resources.rb
+++
@@ -1,9 +0,0 @@
-class RenameTitleToNameInResources < ActiveRecord::Migration
-  def self.up
-    rename_column :resources, :title, :name
-  end
-
-  def self.down
-    rename_column :resources, :name, :title
-  end
-end
