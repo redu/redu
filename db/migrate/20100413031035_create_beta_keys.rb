a4667d23a4ca48d727f8eeb4cda2076ed8182e16
ksiomelo <cassio.ufpe@gmail.com>
2010-04-13 13:14:53 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@328 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100413031035_create_beta_keys.rb
+++ db/migrate/20100413031035_create_beta_keys.rb
@@ -2,15 +2,14 @@ class CreateBetaKeys < ActiveRecord::Migration
   def self.up
     create_table :beta_keys do |t|
       t.string :key
+      t.integer :user_id
     end
     
-    add_column :users, :beta_key_id, :integer
     
     BetaKey.create({:key => 'abcdef'})
   end
 
   def self.down
     drop_table :beta_keys
-    remove_column :users, :beta_key_id
   end
 end
