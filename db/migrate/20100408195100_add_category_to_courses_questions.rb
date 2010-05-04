c75210b90bd3a89bca0c0d68a90509eeb4aa039d
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-05-04 17:46:48 +0000

    Corrigindo erro de sintaxe.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@334 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100408195100_add_category_to_courses_questions.rb
+++ db/migrate/20100408195100_add_category_to_courses_questions.rb
@@ -1,9 +1,9 @@
 class AddCategoryToCoursesQuestions < ActiveRecord::Migration
   def self.up
     remove_column :questions, :skill_id
-    add_column :questions, :category_id
-    add_column :questions, :subcategory_id
-    add_column :questions, :subsubcategory_id
+    add_column :questions, :category_id, :integer
+    add_column :questions, :subcategory_id, :integer
+    add_column :questions, :subsubcategory_id, :integer
     
    # add_column :course, :category_id
    # add_column :course, :subcategory_id
