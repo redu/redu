59bf3263e309378c78ef9223886f5cd82feec060
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-05-27 19:28:03 +0000

    Consertando migrations.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@363 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/070_add_pages.rb
+++ db/migrate/070_add_pages.rb
@@ -1,7 +1,7 @@
 class AddPages < ActiveRecord::Migration
   def self.up
-    Page.create(:title=>:about.l, :body=>"<p>#{:your_about_text_goes_here.l}</p>", :published_as=>"live", :page_public=>true)
-    Page.create(:title=>:faq.l, :body=>"<p>#{:your_faq_text_goes_here.l}</p>", :published_as=>"live", :page_public=>true)
+#    Page.create(:title=>:about.l, :body=>"<p>#{:your_about_text_goes_here.l}</p>", :published_as=>"live", :page_public=>true)
+#    Page.create(:title=>:faq.l, :body=>"<p>#{:your_faq_text_goes_here.l}</p>", :published_as=>"live", :page_public=>true)
   end
 
   def self.down
--- db/migrate/20100526144420_add_price_to_courses_exams.rb
+++ db/migrate/20100526144420_add_price_to_courses_exams.rb
@@ -1,6 +1,5 @@
 class AddPriceToCoursesExams < ActiveRecord::Migration
   def self.up
-    drop_table :course_prices
      add_column :courses, :price, :decimal, :precision => 8, :scale => 2, :default => 0
      add_column :exams, :price, :decimal, :precision => 8, :scale => 2, :default => 0
   end
