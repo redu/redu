4c792c5c40e3b8354145073fc080cf47cb4de88c
ksiomelo <cassio.ufpe@gmail.com>
2010-05-29 13:40:05 +0000

    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@366 b630d61d-3281-de11-9aae-0019d1013b4f

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-
+	
 var CommunityEngine = {	
 	resize_image: function(img, options ) {
 		this.options = options || {};
