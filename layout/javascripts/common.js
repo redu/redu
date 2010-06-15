e211e37034a529dab4e5af550edafc81b1cd924d
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-06-15 14:09:17 +0000

    Integrando todos os js inline no arquivo common.js Adicionando abas em rede.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@394 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ layout/javascripts/common.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+    $("#user-info").hide()
+    
+    $("#header a.login").click(function(e){
+        e.preventDefault()
+        $("#user-info").toggle()
+        $(this).toggleClass('opened')
+    })
+    
+    $("#user-info").mouseup(function(){
+        return false  
+    })
+    
+    $(document).mouseup(function(e){
+        if($(e.target).parent("#header a.login").length==0) {
+            $("#user-info").hide();
+        }
+    })
+    
+    $("#group .sidebar ul.sub-groups li ul, #group .sidebar ul.sub-groups li a.more").hide()
+
+    $("#group .sidebar ul.sub-groups > li a.sb").click(function(e){
+        e.preventDefault()
+        $(this).toggleClass('opened')
+        $(this).next().slideToggle()
+        $(this).next().next().toggle()
+    })
+
+    $("ul.sortable").sortable({
+        placeholder: 'ui-state-highlight'
+    });
+    $("ul.sortable").disableSelection()
+    
+    $(".tabs").tabs();
+    
+    $(".question-action a:first").click(function(e){
+        $(this).next("div.answer:first").slideToggle()
+        e.preventDefault()
+    })
+    $("div.post-activity").tabs({fx : { opacity : 'toggle'}})
+    
+    $("input[title], textarea[title]").each(function(){
+        if($(this).val() === ''){
+            $(this).val($(this).attr('title'))
+        }
+        
+        $(this).focus(function(){
+              if($(this).val() === $(this).attr('title')){
+                  $(this).val('').toggleClass('inner-label')
+              }
+        })
+        
+        $(this).blur(function(){
+              if($(this).val() === ''){
+                  $(this).val($(this).attr('title')).toggleClass('inner-label')
+              }
+        })
+    })
+    
+    i = 2
+    $("form.post-poll span.add").click(function(){
+        $(this).before("<input type=\"text\" name=\"choice_"+i+"\" title=\"Alternativa "+i+"\"/>")
+        i++
+    })
+    
+    $("ul.groups > li:odd").addClass("odd")
+    
+          $("input[title], textarea[title]").each(function(){
+                if($(this).val() === ''){
+                    $(this).val($(this).attr('title'))
+                }
+                
+                $(this).focus(function(){
+                      if($(this).val() === $(this).attr('title')){
+                          $(this).val('').toggleClass('inner-label')
+                      }
+                })
+                
+                $(this).blur(function(){
+                      if($(this).val() === ''){
+                          $(this).val($(this).attr('title')).toggleClass('inner-label')
+                      }
+                })
+            })
+            
+            i = 2
+            $("form.post-poll span.add").click(function(){
+                $(this).before("<input type=\"text\" name=\"choice_"+i+"\" title=\"Alternativa "+i+"\"/>")
+                i++
+            })
+            
+            $("ul.groups > li:odd").addClass("odd")
+})
