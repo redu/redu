9037f6dee29d2215328950af2bdc69f9a86f8430
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-06-27 21:38:51 +0000

    Criando migration para adicionar os Roles default. Movendo do migration 056.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@413 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/056_create_roles.rb
+++ db/migrate/056_create_roles.rb
@@ -5,28 +5,8 @@ class CreateRoles < ActiveRecord::Migration
       t.column :school_role, :boolean , :null => false
     end
 
-    Role.enumeration_model_updates_permitted = true
-    Role.create(:name => 'admin', :school_role => false)    
-    Role.create(:name => 'moderator', :school_role => false)
-    Role.create(:name => 'member', :school_role => false)   
-    
-    # school roles
-    Role.create(:name => 'school_admin', :school_role => true)
-    Role.create(:name => 'coordinator', :school_role => true)  
-    Role.create(:name => 'teacher', :school_role => true)
-    Role.create(:name => 'student', :school_role => true)
-    
-    
-    Role.enumeration_model_updates_permitted = false
-    
-    add_column :users, :role_id, :integer
-
-    #set all existing users to 'member'
-    User.update_all("role_id = #{Role[:member].id}", ["admin = ?", false])
-    #set admins to 'admin'
-    User.update_all("role_id = #{Role[:admin].id}", ["admin = ?", true])    
-
     remove_column :users, :admin
+    add_column :users, :role_id, :integer
   end
 
   def self.down
