4b6debe416f8abac6f8e750714f497dd0b7131d9
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-07-14 02:47:14 +0000

    Removendo magrations relacionado a Resources (inexistente)
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@429 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100117170812_remove_main_resource_from_course_resource_associations.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMainResourceFromCourseResourceAssociations < ActiveRecord::Migration
-  def self.up
-    remove_column :course_resource_associations, :main_resource
-  end
-
-  def self.down
-    add_column :course_resource_associations, :main_resource, :boolean, {:default => false}
-  end
-end
--- db/migrate/20100122171857_remove_main_resource_from_course.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMainResourceFromCourse < ActiveRecord::Migration
-  def self.up
-    remove_column :courses, :main_resource_id
-  end
-
-  def self.down
-    add_column :courses, :main_resource_id, :integer
-  end
-end
