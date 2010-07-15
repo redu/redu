7bf5f4edf270a02c7d72be88ac53d89e7aac8e45
ksiomelo <cassio.ufpe@gmail.com>
2010-07-15 21:41:51 +0000

    inserir admin
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@441 b630d61d-3281-de11-9aae-0019d1013b4f

--- db/migrate/20100715213315_insert_users.rb
+++ db/migrate/20100715213315_insert_users.rb
@@ -6,14 +6,19 @@ class InsertUsers < ActiveRecord::Migration
           :password => 'teste123',
           :password_confirmation => 'teste123',
           :birthday => 14.years.ago)
-          
-    User.create(:login => 'administrator', 
+       
+=end         
+    theuser = User.create(:login => 'administrator', 
           :email => 'admin@example.com',
-          :password => 'admin123',
-          :password_confirmation => 'admin123',
-          :birthday => 14.years.ago)       
+          :password => 'reduadmin123',
+          :password_confirmation => 'reduadmin123',
+          :birthday => 20.years.ago,
+          :first_name => 'Admin',
+          :last_name => 'Redu',
+          :role_id => 1)  
     
-=end    
+    puts theuser.login
+  
   end
 
   def self.down
