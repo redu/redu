bf524a2f356a908ac28a4540b1ac00e33dfc7748
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-08-06 01:14:18 +0000

    Removendo modelo desnecessario.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@518 b630d61d-3281-de11-9aae-0019d1013b4f

--- app/models/course_subject_association.rb
+++
@@ -1,2 +0,0 @@
-class CourseSubjectAssociation < ActiveRecord::Base
-end
--- test/unit/course_subject_association_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class CourseSubjectAssociationTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
