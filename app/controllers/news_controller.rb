073f661eaf9d32f9f1ed7d2b9fe5b265df91fcf9
julianalucena <julianalucenaa@gmail.com>
2010-08-07 20:35:33 +0000

    Adicionando model e controller de New (Notícia).
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@527 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ app/controllers/news_controller.rb
@@ -0,0 +1,23 @@
+class NewsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
