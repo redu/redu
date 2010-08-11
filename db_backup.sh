64b45ce04a8b79be998d3c29ed246a539280a31d
guiocavalcanti <contato@guilhermecavalcanti.com>
2010-08-11 18:33:25 +0000

    Modificando erro de sintaxe.
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@567 b630d61d-3281-de11-9aae-0019d1013b4f

--- db_backup.sh
+++ db_backup.sh
@@ -2,4 +2,4 @@
 
 CURRENT_PATH = "/u/apps/redu/current"
 
-cd #{CURRENT_PATH} && /home/ubuntu/.gem/ruby/1.8/bin/rake RAILS_ENV=production s3:backup:db >/dev/null 2>&1
+cd ${CURRENT_PATH} && /home/ubuntu/.gem/ruby/1.8/bin/rake RAILS_ENV=production s3:backup:db >/dev/null 2>&1
