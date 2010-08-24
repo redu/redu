764b3d751b94ed0002ad0a61369413d219d1bc49
ksiomelo <cassio.ufpe@gmail.com>
2010-08-24 15:14:47 +0000

    listagens
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@673 b630d61d-3281-de11-9aae-0019d1013b4f

--- lib/flash_session_cookie_middleware.rb
+++ lib/flash_session_cookie_middleware.rb
@@ -14,3 +14,21 @@ class FlashSessionCookieMiddleware
     @app.call(env)
   end
 end
+
+#class FlashSessionCookieMiddleware
+#  def initialize(app, session_key = '_session_id')
+#    @app = app
+#    @session_key = session_key
+#  end
+#
+#  def call(env)
+#    if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+#      params = Rack::Request.new(env).params
+#      unless params[@session_key].nil?
+#        env['HTTP_COOKIE'] = "#{@session_key}=#{params[@session_key]}".freeze
+#      end
+#    end
+#
+#    @app.call(env)
+#  end
+#end
