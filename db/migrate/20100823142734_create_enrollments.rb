990002116273ed5f1a4274c04b25d579abcd8e76
heitornascimento <heitornsouza@gmail.com>
2010-08-31 16:47:27 +0000

    alteração migração de enrollments
    git-svn-id: http://ec2-67-202-28-13.compute-1.amazonaws.com/svn/redu/Redu@734 b630d61d-3281-de11-9aae-0019d1013b4f

---
+++ db/migrate/20100823142734_create_enrollments.rb
@@ -0,0 +1,14 @@
+class CreateEnrollments < ActiveRecord::Migration
+  def self.up
+    create_table :enrollments do |t|
+      t.integer :user_id
+      t.integer :subject_id
+      t.timestamps
+    end
+
+  end
+
+  def self.down
+    drop_table :enrollments
+  end
+end
