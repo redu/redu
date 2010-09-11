c1c99cca8f355d43adb8ece6ba1b7f11e5b695d0
Cassio <cassio@cassio-laptop.(none)>
2010-09-11 20:55:19 -0300

    mongrel

---
+++ mongrel/ruby/1.8/gems/zencoder-2.1.14/lib/zencoder/job.rb
@@ -0,0 +1,40 @@
+class Zencoder
+  class Job < Zencoder
+
+    def self.create(params={}, options={})
+      params = apply_api_key(params, options[:format])
+      HTTP.post("#{options[:base_url] || base_url}/jobs",
+                           encode(params, options[:format]),
+                           options)
+    end
+
+    def self.list(options={})
+      params = {:api_key  => options.delete(:api_key) || api_key,
+                :page     => options.delete(:page) || 1,
+                :per_page => options.delete(:per_page) || 50 }
+
+      HTTP.get("#{options[:base_url] || base_url}/jobs", merge_params(options, params))
+    end
+
+    def self.details(job_id, options={})
+      params = {:api_key => options.delete(:api_key) || api_key}
+      HTTP.get("#{options[:base_url] || base_url}/jobs/#{job_id}", merge_params(options, params))
+    end
+
+    def self.resubmit(job_id, options={})
+      params = {:api_key => options.delete(:api_key) || api_key}
+      HTTP.get("#{options[:base_url] || base_url}/jobs/#{job_id}/resubmit", merge_params(options, params))
+    end
+
+    def self.cancel(job_id, options={})
+      params = {:api_key => options.delete(:api_key) || api_key}
+      HTTP.get("#{options[:base_url] || base_url}/jobs/#{job_id}/cancel", merge_params(options, params))
+    end
+
+    def self.delete(job_id, options={})
+      params = {:api_key => options.delete(:api_key) || api_key}
+      HTTP.delete("#{options[:base_url] || base_url}/jobs/#{job_id}", merge_params(options, params))
+    end
+
+  end
+end
