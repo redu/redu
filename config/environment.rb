9b8fb062a513210dafaadb9315a6aa0cd28bbdff
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-14 15:48:15 -0300

    Organizando Código

--- config/environment.rb
+++ config/environment.rb
@@ -39,29 +39,7 @@ Rails::Initializer.run do |config|
   
 
   config.action_controller.session_store = :active_record_store
-  
-  # Settings in config/environments/* take precedence over those specified here.
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded.
-  
-  # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/extras )
-  
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
-  # config.gem "aws-s3", :lib => "aws/s3"
-  
-  # Only load the plugins named here, in the order given (default is alphabetical).
-  # :all can be used as a placeholder for all plugins not explicitly named
-  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  
-  # Skip frameworks you're not going to use. To use Rails without a database,
-  # you must remove the Active Record framework.
-  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
   config.action_mailer.raise_delivery_errors = true
-  
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       #:enable_starttls_auto => true,
@@ -72,6 +50,7 @@ Rails::Initializer.run do |config|
       :user_name => 'diagnosticarapuama@gmail.com',
       :password => 'apuamaeth0'
   }  
+  
   # Activate observers that should always be running
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
   
@@ -83,7 +62,6 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = 'pt-BR' # ver arquivo globalite.rb
 end
-#require "#{RAILS_ROOT}/vendor/plugins/community_engine/config/boot.rb"
 
 #OpenSocialContainer::Configuration.person_class = 'User'
 #OpenSocialContainer::Configuration.secret = 'secret_password'
