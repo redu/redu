60d99b88246a301a39db07622a97572b7c073398
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-20 20:36:55 -0300

    Organizando código

--- app/models/beta_key.rb
+++ app/models/beta_key.rb
@@ -1,17 +1,12 @@
 class BetaKey < ActiveRecord::Base
-  
+
   belongs_to :user
-  
-  
-   before_create :make_key_code
-   
-   validates_uniqueness_of :key
-  
-  
+  before_create :make_key_code
+  validates_uniqueness_of :key
+
   protected
-  
-  def make_key_code(length = 6)
-    self.key = Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by {rand}.join )[1..length]
-  end
-  
+
+    def make_key_code(length = 6)
+      self.key = Digest::SHA1.hexdigest(Time.now.to_s.split(//).sort_by {rand}.join )[1..length]
+    end
 end
