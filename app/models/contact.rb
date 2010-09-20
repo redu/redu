f132d008e4ba27add52f8e84563274b2ce53b9d8
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-20 15:48:18 -0300

    Removendo obrigatoriedade do campo subject de Contact, alterando a UI para essa modificação e transferindo formulário para um partial

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,7 +5,6 @@ class Contact
  
   validates_presence_of :name, :message => "Seu nome é necessário."
   validates_presence_of :email, :message => "Seu e-mail é necessário."
-  validates_presence_of :subject, :message => "O assunto da mensagem é necessário."
   validates_presence_of :body, :message => "A mensagem é necessária."
   validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i,
     :message => "E-mail inválido."
