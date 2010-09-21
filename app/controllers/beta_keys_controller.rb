a3958a9fae4ac9b26a9c4812d3e3ee0af835e9b7
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-21 09:34:03 -0300

    Preenchendo campo de beta_key nos erros de validação

--- app/controllers/beta_keys_controller.rb
+++ app/controllers/beta_keys_controller.rb
@@ -1,74 +1,49 @@
 class BetaKeysController < BaseController
-  
-  
+
   # GET /access_keys
   # GET /access_keys.xml
   def index
-      @beta_keys = BetaKey.find(:all)
-  
+    @beta_keys = BetaKey.find(:all)
+
     respond_to do |format|
       format.html # index.html.erb
     end
   end
-  
 
   # POST /access_keys
   # POST /access_keys.xml
   def generate
-    
     knumber = params[:number].to_i
-    knumber.times { BetaKey.create()  }  
-    
+    note = params[:note]
+    knumber.times { BetaKey.create(:note => note) }
 
     respond_to do |format|
-        flash[:notice] = knumber.to_s + ' chaves geradas!'
-        format.html { redirect_to(beta_keys_path) }
+      flash[:notice] = knumber.to_s + ' chaves geradas!'
+      format.html { redirect_to(beta_keys_path) }
     end
   end
 
-  
   def print_blank
     @beta_keys = BetaKey.find(:all, :conditions => ["email IS NULL AND user_id IS NULL"])
     render "print_blank", :layout => false
-    
   end
-  
+
   def invite
     email_addresses = params[:emails]
-    
-=begin validacao    
-    invalid_emails = []
-    email_addresses = email_addresses || ''
+    note = params[:note]
     emails = email_addresses.split(",").collect{|email| email.strip }.uniq
-    
-    emails.each{ |email|
-      unless email =~ /[\w._%-]+@[\w.-]+.[a-zA-Z]{2,4}/
-        invalid_emails << email
-      end        
-    }
-    unless invalid_emails.empty?
-      record.errors.add(:email_addresses, " included invalid addresses: <ul>"+invalid_emails.collect{|email| '<li>'+email+'</li>' }.join+"</ul>")
-      record.email_addresses = (emails - invalid_emails).join(', ')
-    end
-=end    
-    
-    emails = email_addresses.split(",").collect{|email| email.strip }.uniq 
-    emails.each{|email|
-      
-      beta_key = BetaKey.create(:email => email)
-    
+
+    emails.each do |email|
+      beta_key = BetaKey.create(:email => email, :note => note)
       UserNotifier.deliver_beta_invitation(email, beta_key.key)
-    }
-    
+    end
+
     respond_to do |format|
       flash[:notice] = "Convites enviados!"
       format.html { redirect_to(beta_keys_url) }
       format.xml  { head :ok }
     end
-    
-    
   end
-  
 
   # DELETE /access_keys/1
   # DELETE /access_keys/1.xml
@@ -81,8 +56,8 @@ class BetaKeysController < BaseController
       format.xml  { head :ok }
     end
   end
-  
-    # DELETE /access_keys/1
+
+  # DELETE /access_keys/1
   # DELETE /access_keys/1.xml
   def remove_all
     @beta_key = BetaKey.delete_all
@@ -92,8 +67,4 @@ class BetaKeysController < BaseController
       format.xml  { head :ok }
     end
   end
-  
- 
-  
-  
 end
