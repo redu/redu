4b339ecdbd5b3d5beebd521aa37cd211c2cf679d
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-21 13:34:07 -0300

    Organizando e documentando o c ódico

--- app/models/status.rb
+++ app/models/status.rb
@@ -5,26 +5,22 @@ class Status < ActiveRecord::Base
   POLL = 2
   ANSWER = 3
 
-  belongs_to :statusable, :polymorphic => true
+  acts_as_taggable
 
+  belongs_to :statusable, :polymorphic => true
   belongs_to :logeable, :polymorphic => true
   belongs_to :in_response_to, :polymorphic => true
   belongs_to :user
   has_many :statuses, :as => :in_response_to
 
-
-  acts_as_taggable
+  validation_group :log, :fields => [] # Grupo para tipo log
+  validation_group :status, :fields => [:text, :kind] # Grupo para tipo status (inclui question)
 
   before_validation :enable_correct_validation_group
-
-  validation_group :log, :fields => []
-  validation_group :status, :fields => [:text, :kind]
-
   validates_presence_of :text
   validates_inclusion_of :kind,
     :in => [0, 1, 2, 3, 4],
     :message => "Tipo inválido"
-
 
   # Inspects object attributes and decides which validation group to enable
   def enable_correct_validation_group
@@ -35,14 +31,12 @@ class Status < ActiveRecord::Base
     end
   end
 
-
   def status?
     self.kind == Status::STATUS
   end
 
   def question?
     self.kind == Status::QUESTION
-
   end
 
   def poll?
@@ -66,16 +60,18 @@ class Status < ActiveRecord::Base
     Status.all(:conditions => ["log = 1 AND user_id = ?", user.id])
   end
 
+  # Dado um usuário, retorna os status deles e de quem ele segue
   def Status.friends_statuses(user, limit = 0, offset = 20)
     sql = "SELECT DISTINCT s.* FROM statuses s " + \
-          "LEFT OUTER JOIN followship f " + \
-          	"ON (f.follows_id = s.user_id) " + \
-          "WHERE f.followed_by_id = #{user.id} OR s.user_id = #{user.id} " + \
-          "ORDER BY s.created_at DESC LIMIT #{limit},#{offset}"
-      
+      "LEFT OUTER JOIN followship f " + \
+      "ON (f.follows_id = s.user_id) " + \
+      "WHERE f.followed_by_id = #{user.id} OR s.user_id = #{user.id} " + \
+      "ORDER BY s.created_at DESC LIMIT #{limit},#{offset}"
+
       Status.find_by_sql(sql)
   end
 
+  # Dada uma rede, retorna os status postados na mesma
   def Status.group_statuses(group)
     sql = "SELECT s.* FROM statuses s, user_school_associations a " + \
       "WHERE a.school_id = #{group.id} " + \
@@ -84,5 +80,4 @@ class Status < ActiveRecord::Base
 
     Status.find_by_sql(sql)
   end
-
 end
