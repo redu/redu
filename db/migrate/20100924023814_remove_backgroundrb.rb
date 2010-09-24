7819e26c79a2cea8b0a11f370549947f48acbe6a
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-09-23 23:44:34 -0300

    Removendo restante do backgroundrb

---
+++ db/migrate/20100924023814_remove_backgroundrb.rb
@@ -0,0 +1,27 @@
+class RemoveBackgroundrb < ActiveRecord::Migration
+  def self.up
+    drop_table :bdrb_job_queues
+  end
+
+  def self.down
+    create_table "bdrb_job_queues", :force => true do |t|
+      t.text     "args"
+      t.string   "worker_name"
+      t.string   "worker_method"
+      t.string   "job_key"
+      t.integer  "taken"
+      t.integer  "finished"
+      t.integer  "timeout"
+      t.integer  "priority"
+      t.datetime "submitted_at"
+      t.datetime "started_at"
+      t.datetime "finished_at"
+      t.datetime "archived_at"
+      t.string   "tag"
+      t.string   "submitter_info"
+      t.string   "runner_info"
+      t.string   "worker_key"
+      t.datetime "scheduled_at"
+    end
+  end
+end
