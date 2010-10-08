63920630b594351f8a200cf8b7d54a672e0dac36
Cassio <cassio@cassio-laptop.(none)>
2010-10-08 12:13:22 -0300

    alteracoes em cursos

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ActiveRecord::Base
-
+  
   # PLUGINS
   acts_as_taggable
   ajaxful_rateable :stars => 5
@@ -7,9 +7,9 @@ class Course < ActiveRecord::Base
     :styles => { :thumb => "100x100>", :nano => "24x24>", 
     :default_url => "/images/:class/missing_pic.jpg"}
   }
-
+  
   # ASSOCIATIONS
-   has_many :statuses, :as => :statusable
+  has_many :statuses, :as => :statusable
   has_many :acess_key
   has_many :resources, :class_name => "CourseResource", :as => :attachable
   has_many :acquisitions
@@ -22,29 +22,29 @@ class Course < ActiveRecord::Base
   belongs_to :courseable, :polymorphic => true
   belongs_to :asset, :polymorphic => true
   belongs_to :simple_category
-
-
+  
+  
   # NESTED
-  #accepts_nested_attributes_for :page
+  accepts_nested_attributes_for :courseable
   accepts_nested_attributes_for :resources,
     :reject_if => lambda { |a| a[:media].blank? },
     :allow_destroy => true
-
+  
   # VALIDATIONS
   validates_presence_of :name
   validates_presence_of :description
   validates_length_of   :description, :within => 30..200
   validates_presence_of :simple_category
   validates_presence_of :courseable_type
-
+  
   validates_associated :courseable
-
+  
   validation_group :step1, :fields=>[:name, :description, :simple_category, :courseable_type]
   
   #validation_group :step2_interactive, :fields=>[:name, :description]
   validation_group :step2, :fields=>[:courseable]
   validation_group :step3, :fields=>[:price]
-
+  
   named_scope :published,
     :conditions => ["state LIKE 'approved' AND public = true"], 
     :include => :owner, :order => 'created_at DESC'
@@ -58,22 +58,22 @@ class Course < ActiveRecord::Base
     :conditions => ["courseable_type LIKE 'Page' AND public = true"], 
     :include => :owner, :order => 'created_at DESC'
   named_scope :limited, lambda { |num| { :limit => num } }
-
+  
   # Máquina de estados para moderação do Redu.
   # O estados do processo de transcoding estao em Seminar
   acts_as_state_machine :initial => :waiting
   state :waiting
   state :approved
   state :rejected
-
+  
   event :approve do
     transitions :from => :waiting, :to => :approved
   end
-
+  
   event :reject do
     transitions :from => :waiting, :to => :rejected
   end
-
+  
   def go_to_moderation
     #FIXME esse metodo é usado em algum lugar?
     if self.published
@@ -84,24 +84,24 @@ class Course < ActiveRecord::Base
       end
     end
   end
-
+  
   def permalink
     APP_URL + "/courses/"+ self.id.to_s+"-"+self.name.parameterize
   end
-
-
+  
+  
   def currently_watching
     sql = "SELECT u.id, u.login, u.login_slug FROM users u, statuses s WHERE"
     sql += " s.user_id = u.id AND s.logeable_type LIKE 'Course' AND s.logeable_id = '#{self.id}'"
     sql +=" AND s.created_at > '#{Time.now.utc-10.minutes}'"
-
+    
     User.find_by_sql(sql)
   end
-
+  
   def thumb_url
     case self.courseable_type
-
-    when 'Seminar'
+      
+      when 'Seminar'
       if self.courseable.external_resource_type == 'youtube'
         'http://i1.ytimg.com/vi/' + self.courseable.external_resource + '/default.jpg'
       elsif self.courseable.external_resource_type == 'upload'
@@ -115,35 +115,40 @@ class Course < ActiveRecord::Base
       else 
         'http://i1.ytimg.com/vi/0QQcj_tLIYo/default.jpg'
       end
-    when 'InteractiveClass'
+      when 'InteractiveClass'
       if self.avatar_file_name
         self.avatar.url(:thumb)
       else
-       # image_path("courses/missing_thumb.png")  # icone aula interativa
+        # image_path("courses/missing_thumb.png")  # icone aula interativa
        '/images/courses/missing_interactive.png'
       end
       
-    when 'Page'
-        if self.avatar_file_name
+      when 'Page'
+      if self.avatar_file_name
         self.avatar.url(:thumb)
       else
-
+        
         'http://i1.ytimg.com/vi/0QQcj_tLIYo/default.jpg'
       end
       #APP_URL + '/images/icon_doc_48.png' #FIXME
     end
-
+    
   end
-
+  
   def has_annotations_by(user)
     Annotation.find(:first, :conditions => ["course_id = ? AND user_id = ?", self.id, user.id])
   end
-
+  
   def to_param #friendly url
     "#{id}-#{name.parameterize}"
   end
   
   def build_courseable(params)
-  puts ' oi'
+    puts ' oi'
+    case self.courseable_type
+      when "Page"
+      # se edicao pega ja existente, senao:
+      self.courseable = Page.new(:body => "teste")
+    end
   end
 end
