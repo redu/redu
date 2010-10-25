a99629fca668817bb0636aaee66b790ecd567de6
Juliana Lucena <julianalucenaa@gmail.com>
2010-10-25 17:06:14 -0300

    Removendo javascript do controlador de schools.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -42,14 +42,7 @@ class SchoolsController < BaseController
     @school = School.find(params[:id])
     current_user.vote(@school, params[:like])
     respond_to do |format|
-      format.js do
-        render :update do |page|
-          page << "$('#like_spinner').hide()"
-          page << "$('#like_link').show()"
-          page << "$('#like_link').attr('onclick', 'return false;')"
-          page << "$('#like_count').html('" + @school.votes_for().to_s + "')" # TODO performance + uma consulta?
-        end
-      end
+      format.js { render :template => 'shared/like.rjs', :locals => { :votes_for => @school.votes_for().to_s} } 
     end
   end
 
