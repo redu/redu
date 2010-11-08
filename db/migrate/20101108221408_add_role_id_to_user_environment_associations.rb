6e2735fe9637f306605d1ab6ee7e697763595112
Juliana Lucena <julianalucenaa@gmail.com>
2010-11-08 20:26:56 -0300

    Bugfix: Readicionando role_id a UserEnvironmentAssociations.

---
+++ db/migrate/20101108221408_add_role_id_to_user_environment_associations.rb
@@ -0,0 +1,9 @@
+class AddRoleIdToUserEnvironmentAssociations < ActiveRecord::Migration
+  def self.up
+    add_column :user_environment_associations, :role_id, :integer
+  end
+
+  def self.down
+    remove_column :user_environment_associations, :role_id
+  end
+end
