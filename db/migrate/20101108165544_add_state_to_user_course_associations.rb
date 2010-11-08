e605a91d13a53dfacf4357651d75faff8585d028
Juliana Lucena <julianalucenaa@gmail.com>
2010-11-08 15:19:40 -0300

    Adicionando state a UserCourseAssociation.

---
+++ db/migrate/20101108165544_add_state_to_user_course_associations.rb
@@ -0,0 +1,9 @@
+class AddStateToUserCourseAssociations < ActiveRecord::Migration
+  def self.up
+    add_column :user_course_associations, :state, :string
+  end
+
+  def self.down
+    remove_column :user_course_associations, :state
+  end
+end
