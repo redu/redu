7c9e9ae30326ac4ed805f8b11ceefb4edd991407
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-11-20 14:39:26 -0300

    Adicionando regras ao arquivo de habilidades

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,8 +2,22 @@ class Ability
   include CanCan::Ability
 
   # Regras de authorization
-  def initialize(user, entity)
+  def initialize(user)
+    if user.nil?
+      can :preview do |object|
+        object.published
+      end
+    else
+      # Gerencial
+      can :manage, :all do |object|
+        user.can_manage? object
+      end
 
+      # Usuário normal
+      can :read, :all do |object|
+        object.published && user.has_access_to?(object)
+      end
+    end
   end
 
 end
