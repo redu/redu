ce710038d7c22bc2bdb9730254ef93bbb0e6ce13
fltiago <fltiago@gmail.com>
2010-11-23 07:51:04 -0300

    Adicionando permissões ao controller de event, retirando o atributo published de bulletin.

---
+++ db/migrate/20101123101735_remove_published_from_bulletin.rb
@@ -0,0 +1,9 @@
+class RemovePublishedFromBulletin < ActiveRecord::Migration
+  def self.up
+    remove_column :bulletins, :published
+  end
+
+  def self.down
+    add_column :bulletins, :published, :boolean, :default => true
+  end
+end
