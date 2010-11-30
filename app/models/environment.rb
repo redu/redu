21325125eb513154d8c9d4d3724c258dc5466494
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-11-30 14:19:31 -0300

    Integrando cabeçalho de environment

--- app/models/environment.rb
+++ app/models/environment.rb
@@ -6,7 +6,7 @@ class Environment < ActiveRecord::Base
   has_many :user_environment_associations, :dependent => :destroy
   belongs_to :owner, :class_name => "User", :foreign_key => "owner"
   has_many :users, :through => :user_environment_associations
-  has_attached_file :avatar, PAPERCLIP_STORAGE_OPTIONS.merge({
+  has_attached_file :avatar, PAPERCLIP_STORAGE_OPTIONS.deep_merge({
     :styles => { :thumb => "140x100>" }
   })
   has_many :invitations, :class_name => "EnvironmentInvitation",
