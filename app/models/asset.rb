b81019a4ef906cfcaa413d33df5430e505af4803
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-12-02 13:03:09 -0300

    Melhorando listagem de Assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,4 +5,17 @@ class Asset < ActiveRecord::Base
 
   named_scope :lectures, :conditions => { :assetable_type => "Lecture" }
   named_scope :exams, :conditions => { :assetable_type => "Exam" }
+
+  # Nome amigável para o tipo de asset
+  def friendly_type
+    case self.assetable_type
+    when "Exam" then "Exame"
+    when "Lecture"
+      case self.assetable.lectureable_type
+      when "Seminar" then "Vídeo-aula"
+      when "InteractiveClass" then "Aula interativa"
+      when "Page" then "Tutorial"
+      end
+    end
+  end
 end
