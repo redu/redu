0a01d15ce0f49776a6fe18717544aae78982bb83
Juliana Lucena <julianalucenaa@gmail.com>
2010-12-03 15:04:39 -0300

    Removendo is_public e views não utilizadas de Subject.

---
+++ db/migrate/20101203180219_remove_is_public_from_subjects.rb
@@ -0,0 +1,9 @@
+class RemoveIsPublicFromSubjects < ActiveRecord::Migration
+  def self.up
+    remove_column :subjects, :is_public
+  end
+
+  def self.down
+    add_column :subjects, :is_public, :boolean
+  end
+end
