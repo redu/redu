488f5f0fd6725e410941d65a1616c923a4a39556
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-12-14 07:33:44 -0300

    Bugfix: só sincronizando arquivos estáticos com o S3 se não estiver em ambiente de staging

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,2 +1,4 @@
-run "echo Syncing public dir with S3:"
-run "export SSL_CERT_DIR=/etc/ssl/certs && cd #{current_path} && bundle exec rake s3commit"
+unless node[:environment][:name] == "production"
+  run "echo Syncing public dir with S3:"
+  run "export SSL_CERT_DIR=/etc/ssl/certs && cd #{current_path} && bundle exec rake s3commit"
+end
