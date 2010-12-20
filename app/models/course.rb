cb40b59ce807a1e27d598723903e6145d815579e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-12-20 09:40:49 -0300

    Bugfix: find de course estava sendo sobrescrito de maneira incorreta

--- app/models/course.rb
+++ app/models/course.rb
@@ -21,7 +21,7 @@ class Course < ActiveRecord::Base
   # Sobreescrevendo ActiveRecord.find para adicionar capacidade de buscar por path do Space
   def self.find(*args)
     if args.is_a?(Array) and args.first.is_a?(String) and (args.first.index(/[a-zA-Z\-_]+/) or args.first.to_i.eql?(0) )
-      find_by_path(args)
+      find_by_path(*args)
     else
       super
     end
