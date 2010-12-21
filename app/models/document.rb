75ad62742205747473d98a4589b86e7e878cf398
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-12-21 09:56:02 -0300

    Validação e formato utilizando a API oferecida pelo scribd_fu

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ActiveRecord::Base
   has_ipaper_and_uses 'Paperclip'
   has_attached_file :attachment, DOCUMENT_STORAGE_OPTIONS
 
-  validate :accepted_content_type
+  validate :content_type
 
   # Deriva o content type olhando diretamente para o arquivo. Workaround para
   # problemas decorrentes da integração uploadify/rails
@@ -12,14 +12,10 @@ class Document < ActiveRecord::Base
     self.attachment_content_type = MIME::Types.type_for(self.attachment_file_name).to_s
   end
 
-  def accepted_content_type
+  def content_type
     self.define_content_type
-    # self.scribdable? retorna falso se o doc. já foi enviado p/ conversão
-    if self.ipaper_id.blank? || self.scribdable?
+    unless self.accepted_content_type?
       self.errors.add(:attachment, "Formato inválido")
-      return false
-    else
-      return true
     end
   end
 
