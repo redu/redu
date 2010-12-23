3b7d72e3cf3cf95535fab151b407382b2f6742b1
fltiago <fltiago@gmail.com>
2010-12-23 13:20:21 -0300

    Retirei public de seminar, space e events. Fiz algumas renomeações de espaço para disciplina.

---
+++ db/migrate/20101223153612_remove_public_profile_from_spaces.rb
@@ -0,0 +1,9 @@
+class RemovePublicProfileFromSpaces < ActiveRecord::Migration
+  def self.up
+    remove_column :spaces, :public_profile
+  end
+
+  def self.down
+    add_column :spaces, :public_profile, :boolean
+  end
+end
