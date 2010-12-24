64dc5b9e523dc882829f9d824802192c53b37bb3
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2010-12-24 19:01:59 -0300

    Sincronizando arquivos estáticos com o S3 no deploy

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,4 @@
-unless node[:environment][:name] == "production"
+if node[:environment][:name] == "production"
   run "echo Syncing public dir with S3:"
   run "export SSL_CERT_DIR=/etc/ssl/certs && cd #{current_path} && bundle exec rake s3commit"
 end
