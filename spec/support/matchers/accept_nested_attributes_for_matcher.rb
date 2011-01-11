6890090e9d7efe7019f54a9ffa385a59eda76b81
Juliana Lucena <julianalucenaa@gmail.com>
2011-01-11 14:59:34 -0300

    Criação do matcher accept_nested_attributes.

---
+++ spec/support/matchers/accept_nested_attributes_for_matcher.rb
@@ -0,0 +1,18 @@
+Spec::Matchers.define :accept_nested_attributes_for do |association_name|
+  match do |actual|
+    actual.methods.include?("#{association_name}_attributes=")
+  end
+
+  failure_message_for_should do |actual|
+    "expected that #{actual.class.to_s} would accept nested attributes for #{association_name}"
+  end
+
+  failure_message_for_should_not do |actual|
+    "expected that #{actual.class.to_s} would not accept nested attributes for #{association_name}"
+  end
+
+  description do
+    "accept nested attrs for #{association_name}"
+  end
+end
+
