71c09e37594bb4227b6092a930e1b02066c2efe6
Juliana Lucena <julianalucenaa@gmail.com>
2011-01-13 09:36:27 -0300

    Bugfix: Resolvendo problema de utilizar Role nas criações das associações (UserCourseAssociation).

--- spec/factories/user_course_association_factory.rb
+++ spec/factories/user_course_association_factory.rb
@@ -1,7 +1,6 @@
 Factory.define :user_course_association do |a|
   a.association :user
   a.association :course
-  #FIXME Colocar Role[:member]. Estava quebrando o rake spec.
-  a.role 2
+  a.role :member
   a.state "waiting"
 end
