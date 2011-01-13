6c44fbda09675962dac7315353355b3164248af4
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-01-13 16:26:42 -0300

    Criando factory, modelo, migration e specs de Plan

---
+++ db/migrate/20110113155429_create_plans.rb
@@ -0,0 +1,21 @@
+class CreatePlans < ActiveRecord::Migration
+  def self.up
+    create_table :plans do |t|
+      t.string :state
+      t.string :name
+      t.integer :video_storage_limit
+      t.integer :members_limit
+      t.integer :file_storage_limit
+      t.decimal :price, :precision => 8, :scale => 2
+      t.references :plan
+      t.references :user
+      t.belongs_to :billable, :polymorphic => true
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :plans
+  end
+end
---
+++ spec/models/plan_spec.rb
@@ -0,0 +1,78 @@
+require 'spec_helper'
+
+describe Plan do
+  subject { Factory(:plan) }
+
+  it { should belong_to :billable }
+  it { should belong_to :user }
+  it { should have_one :changed_to }
+  it { should belong_to :changed_from }
+
+  [:members_limit, :price ].each do |attr|
+    it { should validate_presence_of attr }
+  end
+
+  context "states" do
+    [:close!, :migrate!, :current_state].each do |attr|
+      it "responds to" do
+        should respond_to attr
+      end
+    end
+
+    it "defaults to active" do
+      subject.current_state.should == :active
+    end
+
+    it "closes" do
+      expect {
+        subject.close!
+      }.should change { subject.current_state }.to :closed
+    end
+
+    it "migrates" do
+      expect {
+        subject.migrate!
+      }.should change { subject.current_state }.to :migrated
+    end
+  end
+
+  context "when creating new invoices" do
+    it "accesses global billing date"
+    it "responds to create_invoice" do
+      should respond_to :create_invoice
+    end
+    it "sets the association"
+  end
+
+  context "when migrating to a new plan" do
+    before do
+      @new_plan = subject.migrate_to(:name => "Novo plano",
+                                    :members_limit => 30, :price => 10)
+    end
+
+    it "responds to migrate_to" do
+      should respond_to :migrate_to
+    end
+
+    it "sets state to migrated" do
+      subject.current_state.should == :migrated
+    end
+
+    it "creates a valid and new plan" do
+      subject.should be_valid
+    end
+
+    it "copies the older plan associations" do
+      @new_plan.user.should == subject.user
+      @new_plan.billable.should == subject.billable
+    end
+
+    it "sets changed to/from associations" do
+      subject.changed_to.should == @new_plan
+      @new_plan.changed_from == subject
+    end
+
+    it "sets the invoice correctly"
+  end
+
+end
