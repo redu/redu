61ce198f71546d16886287c9e05092cc56b451d1
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-01-17 16:13:51 -0300

    Notificações de confirmação de pagamento e pagamento atrasado

---
+++ db/migrate/20110114162709_change_due_at_to_date_time_in_invoice.rb
@@ -0,0 +1,9 @@
+class ChangeDueAtToDateTimeInInvoice < ActiveRecord::Migration
+  def self.up
+    change_column :invoices, :due_at, :datetime
+  end
+
+  def self.down
+    change_column :invoices, :due_at, :date
+  end
+end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -10,7 +10,16 @@ describe Invoice do
 
   it { should_not allow_mass_assignment_of :state }
 
+  it { should respond_to :threshold_date }
+
+  context "threshold date" do
+    it "should be 10 days from period end" do
+      subject.threshold_date.should == subject.period_end + 10
+    end
+  end
+
   context "states" do
+
     [:close!, :overdue!, :pay!].each do  |attr|
       it "responds to #{attr}" do
         should respond_to attr
@@ -27,10 +36,28 @@ describe Invoice do
       }.should change(subject, :current_state).to(:closed)
     end
 
-    it "pays" do
-      expect {
+    context "when it pays" do
+      before do
+        UserNotifier.delivery_method = :test
+        UserNotifier.perform_deliveries = true
+        UserNotifier.deliveries = []
+      end
+
+      it "changes current state" do
+        expect {
+          subject.pay!
+        }.should change(subject, :current_state).to(:paid)
+
+        subject.due_at.should  be_close(Time.now, 5.seconds)
+      end
+
+      it "sends confirmation e-mail" do
         subject.pay!
-      }.should change(subject, :current_state).to(:paid)
+
+        mail = UserNotifier.deliveries.last
+        mail.should_not be_nil
+        mail.subject.should =~ /Pagamento N\. #{subject.id} confirmado/
+      end
     end
 
     it "overdues" do
@@ -42,6 +69,9 @@ describe Invoice do
     context "when overdue" do
       before do
         subject.overdue!
+        UserNotifier.delivery_method = :test
+        UserNotifier.perform_deliveries = true
+        UserNotifier.deliveries = []
       end
 
       it "closes" do
@@ -54,6 +84,21 @@ describe Invoice do
         expect {
           subject.close!
         }.should change(subject, :current_state).to(:closed)
+      end
+
+      it "stays on same stage if overdue again" do
+        expect {
+          subject.overdue!
+        }.should_not change(subject, :current_state)
+      end
+
+      it "sends overdue e-mail" do
+        subject.overdue!
+        subject.deliver_overdue_notice
+
+        mail = UserNotifier.deliveries.last
+        mail.should_not be_nil
+        mail.subject.should =~ /Pagamento N\. #{subject.id} pendente/
       end
     end
   end
