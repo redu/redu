baa553c00dcf10b7156ec82c2940aa72664d6074
Juliana Lucena <julianalucenaa@gmail.com>
2011-01-30 15:05:47 -0300

    Adição de linhas para registrar o MIME type de pptx e odp com o MIME:Type gem. Closes #5

--- config/initializers/mimes.rb
+++ config/initializers/mimes.rb
@@ -1,1 +1,4 @@
-Mime::Type.register "application/pdf", :pdf
+# register MIME type with MIME::Type gem
+MIME::Types.add(MIME::Type.from_array("application/vnd.openxmlformats-officedocument.presentationml.presentation", %(pptx)))
+MIME::Types.add(MIME::Type.from_array("application/vnd.oasis.opendocument.presentation", %(odp)))
+
