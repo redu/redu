c141bbb055b3f23c195e28ef8ba94c0c36ec9f21
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-02-03 14:31:38 -0300

    Mostrar e esconder form de resposta a status

--- public/javascripts/new_common.js
+++ public/javascripts/new_common.js
@@ -4,4 +4,11 @@ jQuery(function(){
         $(this).find(".username").toggleClass("hover");
         $(this).find("ul").toggle();
     });
+
+    // Responder status
+    $("a.reply-status, .cancel", ".statuses").live("click", function(e){
+        $(this).parents("ul:first").next(".create-response").slideToggle();
+        $(this).parents(".create-response:first").slideToggle();
+        e.preventDefault();
+    })
 });
