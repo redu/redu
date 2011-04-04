3663b6b811d3824c97fd5d77996cdd8001f03e97
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-04-04 10:04:50 -0300

    Organizando código e utilizando named_scope User.with_keyword para autocomplete de messages

--- app/metal/autocomplete.rb
+++ app/metal/autocomplete.rb
@@ -6,27 +6,16 @@ class Autocomplete
     session = env["rack.session"]
     req = Rack::Request.new(env)
     @params = req.params
-    if env["PATH_INFO"] =~ /^\/autocomplete/ 
-      
-      return  [200, {"Content-Type" => "application/json"}, [""]] unless @params["tag"] and session["user_credentials_id"]
-      
-      #pegar id do usuario atual: session["user_credentials_id"]
-      
-      
-      @users = User.find_by_sql(["SELECT u.id, u.first_name, u.avatar_file_name, u.avatar_content_type, u.avatar_file_size, u.avatar_updated_at FROM users u, friendships f "+
-                  "WHERE u.id = f.friend_id AND f.user_id = ? AND LOWER(first_name) LIKE ?", session["user_credentials_id"], "%" + @params["tag"] + "%"])
-                  
+    if env["PATH_INFO"] =~ /^\/autocomplete/
 
+      return  [200, {"Content-Type" => "application/json"}, [""]] unless @params["tag"] and session["user_credentials_id"]
+
+      @users = User.find(session["user_credentials_id"]).friends.with_keyword(@params["tag"])
       @ab = @users.map{|u| {:key => "<img src=\""+u.avatar.url(:thumb)+"\"/> "+u.first_name, :value => u.id}}
-      
+
       [200, {"Content-Type" => "application/json"}, [@ab.to_json]]
     else
       [404, {"Content-Type" => "application/json"}, [""]]
     end
   end
 end
-
-
-
- 
-  
