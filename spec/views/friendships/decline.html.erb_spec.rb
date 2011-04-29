2853d9c72e99b8cab43df4a292ce04d47a75db1c
fltiago <fltiago@gmail.com>
2011-04-29 13:10:29 -0300

    Alterando os specs de Friendship, e removendo os Specs das Views de Frienship.

--- spec/views/friendships/decline.html.erb_spec.rb
+++ spec/views/friendships/decline.html.erb_spec.rb
@@ -1,12 +1,5 @@
 require 'spec_helper'
 
 describe "/friendships/decline" do
-  before(:each) do
-    render 'friendships/decline'
-  end
 
-  #Delete this example and add some real ones or delete this file
-  it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/friendships/decline])
-  end
 end
