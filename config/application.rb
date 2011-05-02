d9419ceaaebb40b970b5f04c466a48e343e1b079
fltiago <fltiago@gmail.com>
2011-05-02 20:06:31 -0300

    Adição do host para envio de e-mail.

--- config/application.rb
+++ config/application.rb
@@ -56,7 +56,11 @@ module Redu
     config.action_controller.session_store = :active_record_store
 
     # ActionMailer
-    config.action_mailer.default_url_options = { :host => APP_URL.sub('http://', '')}
+    if Rails.env.development?
+      config.action_mailer.default_url_options = { :host => "localhost:3000" }
+    else
+      config.action_mailer.default_url_options = { :host => "www.redu.com.br" }
+    end
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
