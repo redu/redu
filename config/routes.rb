57dc927b9c784c649a95abf8e77b77645cc84e5a
Juliana Lucena <julianalucenaa@gmail.com>
2011-05-02 16:33:32 -0300

    Migração das rotas para Rails 3 e adição de arquivos que contém as rotas geradas antes da migração e depois.

--- config/routes.rb
+++ config/routes.rb
@@ -1,247 +1,366 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'clipboard/:action/:folder_or_file/:id',
-    :controller => 'clipboard',
-    :requirements => { :action         => /(add|remove)/,
+Redu::Application.routes.draw do
+  match 'clipboard/:action/:folder_or_file/:id' => 'clipboard',
+    :constraints => { :action         => /(add|remove)/,
                        :folder_or_file => /(folder|file)/ }
+  match '/jobs/notify' => 'jobs#notify', :as => :notify
+  resources :statuses
+  resources :profiles
+  resources :questions do
+    collection do
+      get :search
+      post :search
+      get :add
+    end
+  end
+  resources :folders
+  resources :annotations
+  resources :bulletins do
+    member do
+      post :rate
+    end
+  end
+  resources :sb_posts
+  resources :topics
+  resources :metro_areas
+  root :to => 'base#site_index', :as => :home
 
-  map.notify '/jobs/notify', :controller => 'jobs', :action => 'notify'
-  map.resources :statuses
-  map.resources :profiles
+  resources :tags
+  match '/tags/:id/:type' => 'tags#show', :as => :show_tag_type
+  match '/search/tags' => 'tags#show', :as => :search_tags
 
-  map.resources :questions, :collection => { :search => [:get, :post], :add => :get }
-  map.resources :folders
-  map.resources :annotations
-  map.resources :bulletins, :member => {:rate => :post}
-  map.resources :sb_posts
-  map.resources :topics
-  map.resources :metro_areas
-  map.home '', :controller => "base", :action => "site_index"
-
-  map.resources :tags, :member_path => '/tags/:id'
-  map.show_tag_type '/tags/:id/:type', :controller => 'tags', :action => 'show'
-  map.search_tags '/search/tags', :controller => 'tags', :action => 'show'
-
-  map.admin_tags        'admin/tags/:action', :controller => 'tags', :defaults => {:action=>:manage}
+  match 'admin/tags/:action' => 'tags', :action => :manage, :as => :admin_tags
 
   # sessions routes
-  map.teaser '', :controller=>'base', :action=>'beta_index'
-  map.login  '/login',  :controller => 'sessions', :action => 'new'
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
-  map.signup_by_id '/signup/:inviter_id/:inviter_code', :controller => 'users', :action => 'new'
+  match  '/login' => 'sessions#new', :as => :login
+  match '/signup' => 'users#new', :as => :signup
+  match '/logout' => 'sessions#destroy', :as => :logout
 
-  map.forgot_password '/forgot_password', :controller => 'users', :action => 'forgot_password'
-  map.forgot_username '/forgot_username', :controller => 'users', :action => 'forgot_username'
-  map.resend_activation '/resend_activation', :controller => 'users', :action => 'resend_activation'
-  map.edit_account_from_email '/account/edit', :controller => 'users', :action => 'edit_account'
-  map.resources :sessions
+  match '/forgot_password' => 'users#forgot_password', :as => :forgot_password
+  match '/forgot_username' => 'users#forgot_username', :as => :forgot_username
+  match '/resend_activation' => 'users#resend_activation',
+    :as => :resend_activation
+  match '/account/edit' => 'users#edit_account', :as => :edit_account_from_email
+  resources :sessions
 
   # RSS
-  map.featured '/featured', :controller => 'posts', :action => 'featured'
-  map.featured_rss '/featured.rss', :controller => 'posts', :action => 'featured', :format => 'rss'
-  map.popular '/popular', :controller => 'posts', :action => 'popular'
-  map.popular_rss '/popular.rss', :controller => 'posts', :action => 'popular', :format => 'rss'
-  map.recent '/recent', :controller => 'posts', :action => 'recent'
-  map.recent_rss '/recent.rss', :controller => 'posts', :action => 'recent', :format => 'rss'
-  map.rss_redirect '/rss', :controller => 'base', :action => 'rss_site_index'
-  map.rss '/site_index.rss', :controller => 'base', :action => 'site_index', :format => 'rss'
+  match '/featured' => 'posts#featured', :as => :featured
+  match '/featured.rss' => 'posts#featured', :format => 'rss', :as => :featured_rss 
+  match '/popular' => 'posts#popular', :as => :popular
+  match '/popular.rss' => 'posts#popular', :format => 'rss', :as => :popular_rss
+  match '/recent' => 'posts#recent', :as => :recent
+  match '/recent.rss' => 'posts#recent', :format => 'rss', :as => :recent_rss
+  match '/rss' => 'base#rss_site_index', :as => :rss_redirect
+  match '/site_index.rss' => 'base#site_index', :format => 'rss', :as => :rss
+
 
   # site routes
-  map.about '/about', :controller => 'base', :action => 'about'
-  map.faq '/faq', :controller => 'base', :action => 'faq'
-  map.removed_page   '/removed_item', :controller => 'base', :action => 'removed_item'
-  map.contact 'contact',  :controller => 'base', :action => 'contact'
+  match '/about' => 'base#about', :as => :about
+  match '/faq' => 'base#faq', :as => :faq
+  match '/removed_item' => 'base#removed_item', :as => :removed_page
+  match 'contact' => 'base#contact', :as => :contact
 
-  map.resources :events
+  resources :events
 
-  # SCHOOL
-  map.resources :spaces, :member => {
-    :vote => :post,
-    :manage => :get,
-    :admin_members => :get,
-    :admin_bulletins => :get,
-    :admin_events => :get,
-    :look_and_feel => :get,
-    :members => :get,
-    :teachers => :get,
-    :take_ownership => :get,
-    :publish => :get,
-    :unpublish => :get,
-    :users => :get
-  },
-    :collection =>{
-    :cancel => :get
-  } do |space|
-    space.resources :folders,
-      :member => { :upload => :get,
-                   :download => :get,
-                   :rename => :get,
-                   :destroy_folder => :delete,
-                   :destroy_file => :delete,
-                   :do_the_upload => [:post, :put],
-                   :update_permissions => :post}
-    space.resources :subjects,
-      :member => { :turn_visible => :post,
-                   :turn_invisible => :post,
-                   :admin_lectures_order => [ :get, :post ],
-                   :statuses => :get,
-                   :users => :get,
-                   :admin_members => :get },
-        :collection => {:cancel => :get} do |subject|
-      subject.resources :lectures,
-        :member => { :rate => :post,
-                     :done => :post },
-        :collection => { :unpublished_preview => :get,
-                         :cancel => :get,
-                         :unpublished => :get,
-                         :published => :get }
-      subject.resources :exams,
-        :member => { :add_question => :get,
-                     :add_resource => :get,
-                     :rate => :post,
-                     :answer => [:get,:post],
-                     :compute_results => :get,
-                     :results => :get,
-                     :review_question => :get },
-          :collection => { :unpublished => :get,
-                           :published => :get,
-                           :history => :get,
-                           :new_exam => :get,
-                           :cancel => :get,
-                           :exam_history => :get,
-                           :sort => :get,
-                           :order => :get,
-                           :questions_database => :get,
-                           :review_question => :get }
+  # Space
+  resources :spaces do
+    member do
+      post :vote
+      get :manage
+      get :admin_members
+      get :admin_bulletins
+      get :admin_events
+      get :look_and_feel
+      get :members
+      get :teachers
+      get :take_ownership
+      get :publish
+      get :unpublish
+      get :users
     end
-    space.resources :bulletins,
-      :member => { :vote => [:post, :get]}
-    space.resources :events,
-      :member => { :vote => [:post,:get], :notify => :post },
-      :collection => { :past => :get, :ical => :get , :day => :get }
-    space.resource :forum, :except => [ :new, :edit, :create, :update, :destroy ] do |forum|
-      forum.resources :topics do |topic|
-        topic.resources :sb_posts
+    collection do
+      get :cancel
+    end
+    
+    resources :folders do
+      member do
+        get :upload
+        get :download
+        get :rename
+        delete :destroy_folder
+        delete :destroy_file
+        post :do_the_upload
+        put :do_the_upload
+        post :update_permissions
       end
-      forum.resources :sb_posts, :except => [:new, :edit, :create, :update, :destroy]
+    end
+
+    resources :subjects do
+      member do
+        post :turn_visible
+        post :turn_invisible
+        get :admin_lectures_order
+        post :admin_lectures_order
+        get :statuses
+        get :users
+        get :admin_members
+      end
+      collection do
+        get :cancel
+      end
+
+      resources :lectures do
+        member do
+          post :rate
+          post :done
+        end
+        collection do
+          get :unpublished_preview
+          get :cancel
+          get :unpublished
+          get :published
+        end
+      end
+
+      resources :exams do
+        member do
+          get :add_question
+          get :add_resource
+          post :rate
+          get :answer
+          post :answer
+          get :compute_results
+          get :results
+          get :review_question
+        end
+        collection do
+          get :unpublished
+          get :published
+          get :history
+          get :new_exam
+          get :cancel
+          get :exam_history
+          get :sort
+          get :order
+          get :questions_database
+          get :review_question
+        end
+      end
+    end
+
+    resources :bulletins do
+      member do
+        get :vote
+        post :vote
+      end
+    end
+
+    resources :events do
+      member do
+        get :vote
+        post :vote
+        post :notify
+      end
+      collection do
+        get :past
+        get :ical
+        get :day
+      end
+    end
+
+    resource :forum, :except => [:new, :edit, :create, :update, :destroy] do
+      resources :topics do
+        resources :sb_posts
+      end
+      resources :sb_posts, :except => [:new, :edit, :create, :update, :destroy]
     end
  end
 
-  # USERS
-  map.resources :users, :member => {
-    :annotations => :get,
-    :activity_xml => :get,
-    :logs => :get,
-    :assume => :get,
-    :change_profile_photo => :put,
-    :edit_account => :get,
-    :update_account => :put,
-    :edit_pro_details => :get,
-    :update_pro_details => :put,
-    :forgot_password => [:get, :post],
-    :signup_completed => :get,
-    :invite => :get,
-    :welcome_complete => :get,
-    :statistics => :any,
-    :learning => :get,
-    :teaching => :get,
-    :deactivate => :put,
-    :crop_profile_photo => [:get, :put],
-    :upload_profile_photo => [:get, :put],
-    :download_curriculum => :get,
-    :home => :get,
-    :mural => :get,
-    :account => :get
-  }, :collection => { :auto_complete => :get } do |user|
-    user.resources :friendships,:only => [:index, :create, :destroy],
-      :member => { :accept => :post, :decline => :post },
-      :collection => { :pending => :get }
-    user.resources :photos, :collection => {:swfupload => :post, :slideshow => :get}
-    user.resources :posts, :collection => {:manage => :get}, :member => {:contest => :get, :send_to_friend => :any, :update_views => :any}
-    user.resources :events # Needed this to make comments work
-    #user.resources :clippings
-    user.resources :activities, :collection => {:network => :get}
-    user.resources :invitations
-    user.resources :spaces, :collection => {:member => :get, :owner => :get}
-    user.resources :questions
-    user.resources :offerings, :collection => {:replace => :put}
-    user.resources :favorites, :only => [:index],
-      :member => { :favorite => :post, :not_favorite => :post }
-    user.resources :messages, :collection => { :index_sent => :get, :delete_selected => :post }
-    user.resources :comments
-    user.resources :photo_manager, :only => ['index']
-    user.resources :albums, :path_prefix => ':user_id/photo_manager', :member => {:add_photos => :get, :photos_added => :post}, :collection => {:paginate_photos => :get}  do |album|
-      album.resources :photos, :collection => {:swfupload => :post, :slideshow => :get}
-    user.resources :statuses,
-      :member => { :respond => :post }
-    user.resources :plans, :only => [:index]
-    user.admin_roles '/:environment_id/roles',
-      :controller => :roles, :action => :show, :conditions => {:method => :get}
-    user.update_roles '/:environment_id/roles',
-      :controller => :roles, :action => :update, :conditions => {:method => :post}
+  # Users
+  resources :users do
+    member do
+      get :annotations
+      get :activity_xml
+      get :logs
+      get :assume
+      put :change_profile_photo
+      get :edit_account
+      put :update_account
+      get :edit_pro_details
+      put :update_pro_details
+      get :forgot_password
+      post :forgot_password
+      get :signup_completed
+      get :invite
+      get :welcome_complete
+      match :statistic
+      get :learning
+      get :teaching
+      put :deactivate
+      get :crop_profile_photo
+      put :crop_profile_photo
+      get :upload_profile_photo
+      put :upload_profile_photo
+      get :download_curriculum
+      get :home
+      get :mural
+      get :account
+    end
+  collection do
+    get :auto_complete
+  end
+
+  resources :friendships, :only => [:index, :create, :destroy] do
+    member do
+      post :accept
+      post :decline
+    end
+    collection do
+      get :pending
     end
   end
-  map.activate 'users/activate/:id', :controller => 'users', :action => 'activate'
+
+    resources :photos do
+      collection do
+        post :swfupload
+        get :slideshow
+      end
+    end
+    resources :posts do
+      collection do
+        get :manage
+      end
+      member do
+        get :contest
+        match :send_to_friend
+        match :update_views
+      end
+    end
+    resources :events # Needed this to make comments work
+    resources :activities do
+      collection do
+        get :network
+      end
+    end
+    resources :invitations
+    resources :spaces do
+      collection do
+        get :member
+        get :owner
+      end
+    end
+    resources :questions
+    resources :offerings do
+      collection do
+        put :replace
+      end
+    end
+    resources :favorites, :only => [:index] do
+      member do
+        post :favorite
+        post :not_favorite
+      end
+    end
+    resources :messages do
+      collection do
+        get :index_sent
+        post :delete_selected
+      end
+    end
+    resources :comments
+    resources :photo_manager, :only => ['index']
+    scope ":user_id/photo_manager" do
+      resources :albums do
+        member do
+          get :add_photos
+          post :photos_added
+        end
+        collection do
+          get :paginate_photos
+        end
+
+        resources :photos do
+          collection do
+            post :swfupload
+            get :slideshow
+          end
+        end
+      end
+    end
+    resources :statuses do
+      member do
+        post :respond
+      end
+    end
+    resources :plans, :only => [:index]
+    get '/:environment_id/roles' => 'roles#show', :as => :admin_roles
+    post '/:environment_id/roles' => 'roles#update', :as => :update_roles
+    end
+  match 'users/activate/:id' => 'users#activate', :as => :activate
 
   # Indexes
-  map.application '', :controller => "base", :action => "site_index"
-  map.privacy 'privacy', :controller => "base", :action => "privacy"
-  map.tos 'tos', :controller => "base", :action => "tos"
-  map.contact 'contact', :controller => "base", :action => "contact"
-  map.learn_index '/learn', :controller => 'base', :action => 'learn_index'
-  map.teach_index '/teach', :controller => 'base', :action => 'teach_index'
-  map.courses_index '/courses', :controller => 'courses', :action => 'index',
-    :conditions => { :method => :get }
+  root :to => "base#site_index", :as => :application
+  match 'privacy' => "base#privacy", :as => :privacy
+  match 'tos' => "base#tos", :as => :tos
+  match 'contact' => "base#contact", :as => :contact
+  match '/learn' => 'base#learn_index', :as => :learn_index
+  match '/teach' => 'base#teach_index', :as => :teach_index
+  match '/courses' => 'courses#index', :as => :courses_index, :via => :get
 
-  map.resources :environments,
-    :member_path => "/:id",
-    :nested_member_path => "/:environment_id",
-    :member => { :preview => :get,
-                 :admin_courses => :get,
-                 :admin_members => :get,
-                 :admin_bulletins => :get,
-                 :destroy_members => :post,
-                 :search_users_admin => :post,
-                 :users => :get } do |environment|
-      environment.resources :courses, :member => {
-        :preview => :get,
-        :admin_spaces => :get,
-        :admin_members_requests => :get,
-        :admin_invitations => :get,
-        :admin_manage_invitations => :get,
-        :invite_members => :post,
-        :accept => :post,
-        :join => :post,
-        :unjoin => :post,
-        :publish => :get,
-        :unpublish => :get,
-        :admin_members => :get,
-        :destroy_members => :post,
-        :destroy_invitations => :post,
-        :search_users_admin => :post,
-        :moderate_members_requests => :post,
-        :users => :get,
-        :accept => :post,
-        :deny => :post
-      } do |course|
-        course.resources :user_course_invitations, :only => [:show]
+  resources :environments do
+    member do
+      get :preview
+      get :admin_courses
+      get :admin_members
+      get :admin_bulletins
+      post :destroy_members
+      post :search_users_admin
+      get :users
+    end
+    resources :courses do
+      member do 
+        get :preview
+        get :admin_spaces
+        get :admin_members_requests
+        get :admin_invitations
+        get :admin_manage_invitations
+        post :invite_members
+        post :accept
+        post :join
+        post :unjoin
+        get :publish
+        get :unpublish
+        get :admin_members
+        post :destroy_members
+        post :destroy_invitations
+        post :search_users_admin
+        post :moderate_members_requests
+        get :users
+        post :accept
+        post :deny
       end
-      environment.resources :bulletins,
-        :member => { :vote => [:post, :get] }
+
+      resources :user_course_invitations, :only => [:show]
+    end
+    resources :bulletins do
+      member do
+        get :vote
+        post :vote
+      end
+    end
   end
 
-  map.resources :plans, :only => [], :member => {
-    :confirm => [:get, :post],
-    :upgrade => [:get, :post]
-  } do |plan|
-    plan.resources :invoices, :only => [:index]
+  resources :plans, :only => [] do
+    member do
+      get :confirm
+      post :confirm
+      get :upgrade
+      post :upgrade
+    end
+
+    resources :invoices, :only => [:index]
   end
 
-  map.payment_success '/payment/callback',
-    :controller => 'payment_gateway', :action => 'callback'
-  map.payment_success '/payment/success',
-    :controller => 'payment_gateway', :action => 'success'
-
+  match '/payment/callback' => 'payment_gateway#callback',
+    :as => :payment_callback
+  match '/payment/success' => 'payment_gateway#success', :as => :payment_success
 end
-#ActionController::Routing::Translator.i18n('pt-BR') # se ativar, buga (falar com cassio)
-#ActionController::Routing::Translator.translate_from_file('lang','i18n-routes.yml')
