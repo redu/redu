aae51044b1eb1177b2fc1900c308e53eeada2a91
fltiago <fltiago@gmail.com>
2011-05-11 14:48:35 -0300

    Atualizando chamadas AJAX de Favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,7 +10,8 @@ class FavoritesController < BaseController
         @exams = Exam.paginate(:all,
                                :joins => :favorites,
                                :conditions => ["favorites.favoritable_type = 'Exam' AND favorites.user_id = ? AND exams.id = favorites.favoritable_id", current_user.id],
-                               :page => params[:page], :order => 'created_at DESC', :per_page => Redu::Application.config.items_per_page)
+                               :page => params[:page], :order => 'created_at DESC',
+                               :per_page => Redu::Application.config.items_per_page)
 
         format.js do
           render :template => 'favorites/exams.rjs'
@@ -24,19 +25,19 @@ class FavoritesController < BaseController
         @lectures = Lecture.paginate(:all,
                                    :joins => :favorites,
                                    :conditions => ["favorites.favoritable_type = 'Lecture' AND favorites.user_id = ? AND lectures.id = favorites.favoritable_id", current_user.id],
-                                   :page => params[:page], :order => 'created_at DESC', :per_page => Redu::Application.config.items_per_page)
-
+                                   :page => params[:page], :order => 'created_at DESC',
+                                   :per_page => Redu::Application.config.items_per_page)
 
       end
     end
   end
 
   def favorite
-     if (params[:id].to_i != 0) &&
+    if (params[:id].to_i != 0) &&
       ((params[:type].eql? 'Lecture') || (params[:type].eql? 'Exam') ||
-      (params[:type].eql? 'Status'))
-         favoritable = Kernel.const_get(params[:type]).find(params[:id])
-     end
+       (params[:type].eql? 'Status'))
+      favoritable = Kernel.const_get(params[:type]).find(params[:id])
+    end
     authorize! :read, favoritable
 
     @favorite = current_user.add_favorite(params[:type], params[:id] )
@@ -55,19 +56,12 @@ class FavoritesController < BaseController
       msg_err = "Recurso não foi adicionado ao seus favoritos"
     end
 
-    if @favorite
-      flash.now[:notice] = msg_ok
-      respond_to do |format|
-        if @favorite.favoritable.class.to_s == 'Status'
-          format.js { render :status_favorite }
-        elsif @favorite.favoritable.class.to_s == 'Lecture'
-          format.js { render :lecture_favorite }
-        end
-      end
-    else
-      flash.now[:error] = msg_err
-      respond_to do |format|
-        format.js
+    flash.now[:notice] = msg_ok
+    respond_to do |format|
+      if @favorite.favoritable.class.to_s == 'Status'
+        format.js { render :status_favorite }
+      elsif @favorite.favoritable.class.to_s == 'Lecture'
+        format.js { render :lecture_favorite }
       end
     end
   end
