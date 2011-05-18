36c914ff4a723082c0f899b93a3a6cf7effcaddd
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-18 16:01:15 -0300

    Customizando find de course em UserCourseInvitiations

--- app/controllers/user_course_invitations_controller.rb
+++ app/controllers/user_course_invitations_controller.rb
@@ -2,7 +2,7 @@ class UserCourseInvitationsController < BaseController
   layout "clean"
 
   load_resource :environment, :find_by => :path
-  load_resource :course, :through => :environment
+  load_resource :course, :through => :environment, :find_by => :path
   load_resource :user_course_invitation, :through => :course
 
   def show
