7a293fafbfca44792b1b95d0da6b5229b865a0b8
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-18 20:14:18 -0300

    Mudando o nome do host de arquivos estáticos

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -32,7 +32,7 @@ Redu::Application.configure do
   config.serve_static_assets = false
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
-  config.action_controller.asset_host = "http://redu_assets.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://redu-assets-staging.s3.amazonaws.com"
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
