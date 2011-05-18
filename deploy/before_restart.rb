b26f8df900eb3001976503c314538c31637350a3
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-18 15:26:54 -0300

    Somente sincronizando com o S3 nos environments production ou staging

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,13 @@
-if node[:environment][:name] == "production"
+if ( env?('production') or env?('staging') )
   run "echo Syncing public dir with S3:"
   run "export SSL_CERT_DIR=/etc/ssl/certs && cd #{current_path} && bundle exec rake s3commit"
+end
+
+def sync_assets
+  run "echo Syncing public dir with S3:"
+  run "export SSL_CERT_DIR=/etc/ssl/certs && cd #{current_path} && bundle exec rake s3commit"
+end
+
+def env?(environment)
+  node[:environment][:name].eql?(environment)
 end
