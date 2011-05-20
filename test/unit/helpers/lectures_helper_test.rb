fc300d6786bc97bdd41ed17f9a51dc0740f3e3b3
fltiago <fltiago@gmail.com>
2011-05-20 16:32:17 -0300

    Removendo tests/

--- test/unit/helpers/lectures_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class LecturesHelperTest < ActionView::TestCase
-end
--- test/unit/resource_observer_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class ResourceObserverTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
