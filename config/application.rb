8445efe5448572bbf52a55c43592608f0dd35417
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-20 15:58:51 -0300

    Setando rspec framework de testes default

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,10 @@ module Redu
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
+    config.generators do |g|
+      g.test_framework :shoulda
+    end
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
