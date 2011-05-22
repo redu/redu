0daf8ccb12491ae08a86a32f2ef936c8acb7c52c
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-22 19:53:55 -0300

    Update no deploy_hook

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,6 +1,5 @@
-run "echo '--------->'"
 if (node[:environment][:name].include?("staging"))
   run "echo Syncing public dir with S3:"
-  run "bundle exec jammit-s3 "
   run "cd config; ln -sf assets-#{node[:environment][:name]}.yml assets.yml"
+  run "bundle exec jammit-s3 "
 end
