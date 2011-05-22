e41059251111199c90d011623aa5f6b2ebd5d9cc
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-22 11:20:24 -0300

    Usando Rails.env

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,5 +1,5 @@
-if (node[:environment][:name] == "staging")
+if (Rails.env.staging? == "staging")
   run "echo Syncing public dir with S3:"
-  run "bundle exec jammit-s3 --config config/assets-#{node[:environment][:name]}.yml"
-  run "ln -sf config/assets-#{node[:environment][:name]}.yml config/assets.yml"
+  run "bundle exec jammit-s3 --config config/assets-#{Rails.env.to_s}.yml"
+  run "ln -sf config/assets-#{Rails.env.to_s}.yml config/assets.yml"
 end
