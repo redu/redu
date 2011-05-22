044ef1fefc29859e7251afdee767f2de5c48e507
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-22 11:27:49 -0300

    Corrigindo erro de sintaxe

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,4 @@
-if (node[:environment][:name].inclue?("staging"))
+if (node[:environment][:name].include?("staging"))
   run "echo Syncing public dir with S3:"
   run "bundle exec jammit-s3 --config config/assets-#{node[:environment][:name]}.yml"
   run "ln -sf config/assets-#{node[:environment][:name]}.yml config/assets.yml"
