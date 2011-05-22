d265f7d0e0b40520683e7d15d85fc87a0bcae0a3
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-22 11:18:16 -0300

    Criando link simbólico p/ o arquivo assets.yml

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,4 +1,5 @@
 if (node[:environment][:name] == "staging")
   run "echo Syncing public dir with S3:"
   run "bundle exec jammit-s3 --config config/assets-#{node[:environment][:name]}.yml"
+  run "ln -sf config/assets-#{node[:environment][:name]}.yml config/assets.yml"
 end
