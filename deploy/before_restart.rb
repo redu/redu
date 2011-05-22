2a446641b52f16b8914b68fcfe3ae1c0d159e26f
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-22 11:48:29 -0300

    Update no deploy_hook

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,6 +1,1 @@
-run "echo #{environment} abc"
-if (node[:environment][:name].include?("staging"))
-  run "echo Syncing public dir with S3:"
-  run "bundle exec jammit-s3 --config config/assets-#{node[:environment][:name]}.yml"
-  run "ln -sf config/assets-#{node[:environment][:name]}.yml config/assets.yml"
-end
+run "echo abc"
