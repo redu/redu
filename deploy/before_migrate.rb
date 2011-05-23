528715ebdfa0eced14f2eba33fb2e25e4bb48ed3
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-05-23 08:27:12 -0300

    Voltando a usar o before_restart

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,5 +1,5 @@
-if (environment.include?("staging") or environment.include?("production"))
-  run "cd config; ln -sf assets-#{environment}.yml assets.yml"
-  run "bundle exec jammit-s3 "
-end
+# if (environment.include?("staging") or environment.include?("production"))
+#   run "cd config; ln -sf assets-#{environment}.yml assets.yml"
+#   run "bundle exec jammit-s3 "
+# end
 
