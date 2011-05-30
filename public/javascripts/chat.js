b79eb0dadaca585325c56eb9a338323f7577bbd9
Juliana Lucena <julianalucenaa@gmail.com>
2011-05-30 15:25:52 -0300

    Implementação de subscribeMyChannel, subscribeNewContact e unsubscribeContact

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -11,20 +11,24 @@ var buildChat = function(opts){
   var pusher;
   var config = {};
   var $userList = $("<div/>", { id : "chat-list" }).append("<ul/>");
+  var getCSSUserId = function(userId) {
+    return "chat-user-" + userId;
+  }
 
-  $.fn.addContact = function(opts){
+  $.fn.addContact = function(member){
     return this.each(function(){
         var $this = $(this);
         var $li = $("<li/>").appendTo($this);
+        $li.attr("id", getCSSUserId(member.id));
         var $role = $("<span/>", { "class" : "roles" }).appendTo($li);
-        $("<img/>", { "src" : opts.thumbnail, "class" : "avatar" }).appendTo($this);
+        $("<img/>", { "src" : member.info.thumbnail, "class" : "avatar" }).appendTo($this);
 
         // Adicionando classe p/ papeis true
-        for(var key in opts.roles){
-          if(opts.roles[key]){ $role.addClass(key); }
+        for(var key in member.info.roles){
+          if(member.info.roles[key]){ $role.addClass(key); }
         }
 
-        $("<span/>", { 'class' : "name" }).text(opts.name).appendTo($this);
+        $("<span/>", { 'class' : "name" }).text(member.info.name).appendTo($this);
     });
   };
 
@@ -38,17 +42,53 @@ var buildChat = function(opts){
       pusher = new Pusher(config.key);
     },
     // Inscreve no canal do usuário logado
-    subscribeMyChannel : function(){},
-    // Increve no canal dado
-    subscribeNewContact : function(channel){},
+    subscribeMyChannel : function(){
+      console.log("subMyChannel")
+      myPresenceCh = pusher.subscribe(config.channel);
+
+      // Escuta evento de confirmação de inscrição no canal
+      myPresenceCh.bind("pusher:subscription_succeeded", function(members){
+          members.each(function(member) {
+              var channels = member.info.friends;
+              // Somente o user atual tem info.friends
+              if(channels){
+                for(var ch in channels) {
+                  pusher.subscribe(ch);
+                }
+              } else {
+                // Para o restante dos membros do canal
+                //(caso em que os contatos entram antes do dono)
+                pusher.subscribe(member.channel)
+              }
+          });
+      });
+
+      // Escuta evento de adição de membro no canal
+      myPresenceCh.bind("pusher:member_added", function(member){
+        uiAddContact(member);
+      });
+
+      // Escuta evento de remoção de membro no canal
+      myPresenceCh.bind("pusher:member_removed", function(member){
+        //uiRemoveContact(member.id);
+      });
+    },
+    // Increve no canal dado (Caso de já estar no chat e aceitar convite de contato)
+    subscribeNewContact : function(channel){
+      pusher.subscribe(channel);
+    },
     // Desinscreve do canal dado
-    unsubscribeContact : function(channel){},
+    unsubscribeContact : function(channel){
+      pusher.unsubscribe(channel);
+    },
     // Adiciona a lista de contatos
     uiAddContact : function(member){
       $userList.find("ul").addContact(member);
     },
     // Remove da lista de contatos
-    uiRemoveContact : function(userId){},
+    uiRemoveContact : function(userId){
+      $("#" + getCSSUserId(userId)).remove();
+    },
   }
 
   return that;
--- spec/javascripts/chat_spec.js
+++ spec/javascripts/chat_spec.js
@@ -1,76 +1,108 @@
 describe('Chat', function () {
-    beforeEach(function () {
-        Pusher = function(key, args){}
-
-        Pusher.prototype.subscribe = function(channel){
-          var data = { data : "opa" }
-          return { bind : function(event, callback) { callback(data); }};
-        }
-    });
-
-    afterEach(function () {
-      // Limpando elementos adicionados ao DOM
-      $("#chat-list").remove();
-    });
+      afterEach(function () {
+          // Limpando elementos adicionados ao DOM
+          $("#chat-list").remove();
+      });
 
 
-    it('defines buildChat', function () {
-        expect(buildChat).toBeDefined();
-    });
+      it('defines buildChat', function () {
+          expect(buildChat).toBeDefined();
+      });
 
-    describe('initialization', function () {
-        var chat, opts;
+      describe('initialization', function () {
+          var chat, opts;
 
-        beforeEach(function () {
-            opts = { key : 'XXX', channel : 'my-channel' };
-            chat = buildChat(opts)
+          beforeEach(function () {
+              opts = { key : 'XXX', channel : 'my-channel' };
+              chat = buildChat(opts)
+          });
+
+          it('defines init', function () {
+              expect(chat).toBeDefined();
+          });
+
+          it('shows a empty list', function() {
+              chat.init();
+
+              expect($("#chat-list")).toExist();
+          });
+
+      });
+
+      describe('ui methods', function () {
+          var chat, member;
+
+          beforeEach(function () {
+              chat = buildChat({ key : 'XXX', channel : 'my-channel' });
+              chat.init();
+
+              member = {
+                "info" : {
+                  "roles" : {
+                    "teacher" : true
+                    ,"member" : false
+                    ,"administrator" : false
+                    ,"tutor" : false
+                  }
+                  ,"name" : "Test user"
+                  ,"thumbnail" : "new/missing_users_thumb_32.png"
+                }
+                ,"id" : "1234"
+              }
+
+              chat.uiAddContact(member);
+          });
+
+            it('adds contact to UI', function () {
+                expect($("#chat-list ul li").length).toBe(1);
+                expect($("#chat-list .name").text()).toBe(member.info.name);
+            });
+
+            it('adds the correct role classes', function() {
+                expect($("#chat-list .roles")).toHaveClass("teacher");
+            });
+
+            it('creates the user link', function() {
+                expect($("#chat-list img").attr("src")).toBe(member.info.thumbnail);
+            });
+
+            it('should set data-userId', function() {
+                expect($("#chat-user-" + member.id)).toExist();
+            });
+
+            it('should remove the user from UI', function() {
+                chat.uiRemoveContact(member.id)
+                expect($("#chat-user-" + member.id)).not.toExist();
+            });
+
         });
 
-        it('defines init', function () {
-            expect(chat).toBeDefined();
+        describe('when subscribing', function () {
+            var chat;
+            beforeEach(function () {
+                opts = { key : 'XXX', channel : 'my-channel' };
+                chat = buildChat(opts)
+                chat.init();
+            });
+
+            it('defines subscribeMyChannel', function() {
+                expect(chat.subscribeMyChannel).toBeDefined();
+            });
+
+            it('defines subscribeNewContact', function() {
+                expect(chat.subscribeNewContact).toBeDefined();
+            });
+
+            xit('subscribes the chat owner to his own pusher channel', function() {
+                spyOn(Pusher, "Channel");
+                chat.subscribeMyChannel();
+                expect(Pusher.Channel).toHaveBeenCalled();
+            });
+
+            xit('calls subscribeNewContact to every friend', function(){
+                var spy = spyOn(Pusher, "Channel");
+                chat.subscribeMyChannel();
+                expect(spy.callCount).toEqual(5);
+            });
         });
-
-        it('shows a empty list', function() {
-            chat.init();
-
-            expect($("#chat-list")).toExist();
-        });
-
     });
-
-    describe('ui methods', function () {
-        var chat, member;
-
-      beforeEach(function () {
-        chat = buildChat({ key : 'XXX', channel : 'my-channel' });
-        chat.init();
-
-        member = {
-          "roles" : {
-            "teacher" : true,
-            "member" : false,
-            "administrator" : false,
-            "tutor" : false,
-          },
-          name : "Test user",
-          thumbnail : "new/missing_users_thumb_32.png"
-        }
-
-        chat.uiAddContact(member);
-      });
-
-      it('adds contact to UI', function () {
-          expect($("#chat-list ul li").length).toBe(1);
-          expect($("#chat-list .name").text()).toBe(member.name);
-      });
-
-      it('adds the correct role classes', function() {
-        expect($("#chat-list .roles")).toHaveClass("teacher");
-      });
-
-      it('creates the user link', function() {
-          expect($("#chat-list img").attr("src")).toBe(member.thumbnail);
-      });
-    });
-});
-
