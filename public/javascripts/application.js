09e929b917a2fa9ecf02b5d723dd3286112403f5
Juliana Lucena <julianalucenaa@gmail.com>
2011-07-01 14:48:11 -0300

    Mostrar link para expandir respostas do Status apenas quando houverem respostas suficientes. Closes #90

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -22,10 +22,23 @@ jQuery(function(){
         e.preventDefault();
     });
 
+    // Esconde as respostas, caso não haja nenhuma
+    $(".responses").each(function(){
+        var responses = $(this).find("> ol > li");
+        if (responses.length == 0) {
+          $(this).hide();
+        }
+    });
+
     // Apenas mostrar as 3 primeiras respostas e mostrar texto "Ver todos os X comentários"
     $(".responses", ".statuses").each(function(i, obj){
-        $(this).find("> ol > li:gt(2)").hide();
-        $(this).find(".toggle-statuses .qty").html($(this).find("ol > li").length);
+        var quantity = $(this).find("ol > li").length;
+        if (quantity >= 3) {
+          $(this).find("> ol > li:gt(2)").hide();
+          $(this).find(".toggle-statuses .qty").html(quantity);
+        } else {
+          $(this).find(".toggle-statuses").remove();
+        }
     });
 
     // Mostrar todas as respostas ao clicar em "Ver todos os X comentários"
