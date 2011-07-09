e22d1ab0970281af976f8686893a933bfa681369
Juliana Lucena <julianalucenaa@gmail.com>
2011-07-09 19:52:53 -0300

    Criação de views para manipulação das Experiences do currículo pelo User.

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -1,17 +1,47 @@
 (function($){
- $.fn.refreshMobileMask = function(){
-    return this.each(function(){
-      var $this = $(this);
-      $this.mask("+99 (99) 9999-9999", { placeholder:" " });
+    $.fn.refreshMobileMask = function(){
+      return this.each(function(){
+          var $this = $(this);
+          $this.mask("+99 (99) 9999-9999", { placeholder:" " });
+      });
+    };
+
+    $.fn.refreshEndDateVisibility = function(){
+      return this.each(function(){
+          $(this).parent().siblings(".end-date").hide();
+      });
+    };
+
+    jQuery(function(){
+        $("#biography .mobile").refreshMobileMask();
+        $(".current-experience #experience_current:checked").refreshEndDateVisibility();
+        $("#experience_current").live("change", function(){
+            $("#curriculum .end-date").slideToggle();
+        });
+
+        $("#curriculum .new-experience-button").live("click", function(){
+            $(this).hide();
+            $("#new_experience").slideToggle();
+            return false;
+        });
+
+        $("#curriculum .edit-experience").live("click", function(){
+            $experiences = $("#curriculum .experiences > li");
+            $experiences.find(".infos").show();
+            $experiences.find("form").fadeOut();
+            $("#new_experience").hide();
+            $("#curriculum .new-experience-button").hide();
+
+            var $infos = $(this).parent();
+            $infos.fadeOut();
+            $infos.siblings("form").fadeIn();
+            return false;
+        });
+
+        $(document).ajaxComplete(function(){
+            $("#biography .mobile").refreshMobileMask();
+            $(".current-experience #experience_current:checked").refreshEndDateVisibility();
+        });
     });
- };
-
- jQuery(function(){
-   $("#biography .mobile").refreshMobileMask();
-
-   $(document).ajaxComplete(function(){
-     $("#biography .mobile").refreshMobileMask();
-   });
- });
 
 })($);
