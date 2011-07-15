490df9ca0bb130eaf21b2dc356534877212a6a14
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-07-15 17:45:34 -0300

    Adicionar colaboradores p/ Parceiro

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,4 +4,21 @@ class Partner < ActiveRecord::Base
   has_many :users
 
   validates_presence_of :name
+
+  def add_collaborator(user)
+    self.users << user
+    self.environments.all(:include => :courses).each do |e|
+      UserEnvironmentAssociation.create(:environment => e,
+                                        :user => user,
+                                        :role => Role[:environment_admin])
+
+      e.courses.each do |c|
+        c.create_hierarchy_associations(user, Role[:environment_admin])
+        ass = UserCourseAssociation.create(:user => user,
+                                     :course => c,
+                                     :role => Role[:environment_admin])
+        ass.approve!
+      end
+    end
+  end
 end
--- spec/models/partner_spec.rb
+++ spec/models/partner_spec.rb
@@ -7,4 +7,35 @@ describe Partner do
   it { should validate_presence_of :name }
   it { should have_many(:environments).through(:partner_environment_associations) }
   it { should have_many(:users) }
+
+  context "when adding new collaborators" do
+    before do
+      3.times do
+        course = Factory(:course)
+        Factory(:partner_environment_association, :partner => subject,
+                :environment => course.environment)
+      end
+
+      @collaborator = Factory(:user)
+      subject.add_collaborator(@collaborator)
+    end
+
+    it "creates the correct association to all environments" do
+      subject.environments.each do |e|
+        e.administrators.should include(@collaborator)
+      end
+    end
+
+    it "creates the correct association to all courses" do
+      courses = subject.environments.collect { |e| e.courses }.flatten
+
+      courses.each do |c|
+        c.administrators.should include(@collaborator)
+      end
+    end
+
+    it "creates the association to partner" do
+      subject.users.should include(@collaborator)
+    end
+  end
 end
