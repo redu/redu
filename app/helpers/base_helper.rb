7f3b2a07d10546ea5c0701f9a36fe0525370c68e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-07-21 21:04:39 -0300

    Padrão de erros p/ mensagens globais e campos em formulários

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -81,6 +81,67 @@ module BaseHelper
     end
   end
 
+  # Sobrescrito para exibir mensagem de erro apenas com o nome
+  # dos campos inválidos.
+  def concave_error_messages_for(*params)
+    options = params.extract_options!.symbolize_keys
+
+    objects = Array.wrap(options.delete(:object) || params).map do |object|
+      object = instance_variable_get("@#{object}") unless object.respond_to?(:to_model)
+      object = convert_to_model(object)
+
+      if object.class.respond_to?(:model_name)
+        options[:object_name] ||= object.class.model_name.human.downcase
+      end
+
+      object
+    end
+
+    objects.compact!
+    count = objects.inject(0) {|sum, object| sum + object.errors.count }
+
+    unless count.zero?
+      html = {}
+      [:id, :class].each do |key|
+        if options.include?(key)
+          value = options[key]
+          html[key] = value unless value.blank?
+        else
+          html[key] = 'error_explanation'
+        end
+      end
+      options[:object_name] ||= params.first
+
+      I18n.with_options :locale => options[:locale], :scope => [:activerecord, :errors, :template] do |locale|
+        header_message = if options.include?(:header_message)
+                           options[:header_message]
+                         else
+                           locale.t :header, :count => count, :model => options[:object_name].to_s.gsub('_', ' ')
+                         end
+
+        message = options.include?(:message) ? options[:message] : locale.t(:body)
+
+        error_messages = objects.sum do |object|
+          object.errors.collect { |attr, error| attr }.map do |attr|
+            object.class.human_attribute_name(attr, :default => attr)
+          end
+        end.join(", ").html_safe
+
+
+
+        contents = ''
+        contents << content_tag(options[:header_tag] || :h2, header_message) unless header_message.blank?
+        contents << content_tag(:p, message) unless message.blank?
+        contents << content_tag(:p, error_messages, :class => "invalid_fields")
+
+        content_tag(:div, contents.html_safe, html)
+      end
+    else
+      ''
+    end
+  end
+
+
 
   def activity_name(item)
     link_user = link_to item.user.display_name, user_path(item.user)
