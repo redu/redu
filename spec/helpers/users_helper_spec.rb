f22d5663e2879b670c46c4a14d1cd0962009f9da
fltiago <fltiago@gmail.com>
2011-08-03 15:27:40 -0300

    Bugfix: modificando o link gerado pelo social network no show de user.

---
+++ spec/helpers/users_helper_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe UsersHelper do
+
+  it "should generate right url" do
+    url1 = "www.example.com.br"
+    url2 = "http://www.example.com.br"
+    url3 = "www.http://.com.br"
+
+    [url1, url2].each do |url|
+      generate_url(url).should == "http://www.example.com.br"
+    end
+    generate_url(url3).should == "http://www.http://.com.br"
+  end
+
+end
