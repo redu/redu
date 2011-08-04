1c5c7551245cee0f0db357b6d637b741b52e327f
Juliana Lucena <julianalucenaa@gmail.com>
2011-08-04 16:49:46 -0300

    No endless de Users#show, não pegando amigos em comum, caso o usuário esteja visualizando seu próprio perfil. Closes #349

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -96,8 +96,12 @@ class UsersController < BaseController
   end
 
   def contacts_endless
-    @contacts = Kaminari::paginate_array(@user.friends_not_in_common_with(current_user)).
-      page(params[:page]).per(4)
+    @contacts = if current_user == @user
+      Kaminari::paginate_array(@user.friends).page(params[:page]).per(8)
+    else
+      Kaminari::paginate_array(@user.friends_not_in_common_with(current_user)).
+        page(params[:page]).per(4)
+    end
 
     respond_to do |format|
       format.js { render_sidebar_endless 'users/item_medium_24', @contacts,
