187c310a37753b24a1c89669e0e05628c80e96c4
fltiago <fltiago@gmail.com>
2011-09-22 15:22:35 -0300

    Colocando focus no textarea quando o usuário clica 'Responder' de um status.

--- public/javascripts/statuses.js
+++ public/javascripts/statuses.js
@@ -28,6 +28,7 @@ jQuery(function(){
     $("a.reply-status, .cancel", ".statuses").live("click", function(e){
         $(this).parents("ul:first").next(".create-response").slideToggle();
         $(this).parents(".create-response:first").slideToggle();
+        $(this).parents("ul:first").next(".create-response").find("textarea").focus();
         e.preventDefault();
     });
 
