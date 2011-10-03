9cac4ef064263ae01e5e00535f236673f7073491
Juliana Lucena <julianalucenaa@gmail.com>
2011-10-03 15:50:29 -0300

    Nova visualização Courses#new.

--- public/javascripts/admin.js
+++ public/javascripts/admin.js
@@ -1,29 +1,35 @@
 (function($){
-    // Padrão de spinner
-    $(".admin-filter-form").live('ajax:before', function(e){
-        var $this = $(this);
-        var $target = $(e.target);
+  // Padrão de spinner
+  $(".admin-filter-form").live('ajax:before', function(e){
+    var $this = $(this);
+    var $target = $(e.target);
 
-        if($this.is($target)){
-          var $submit = $(this).find("input[type=submit]");
-          $submit.loadingStart({ "className" : "concave-loading" });
-        }
+    if($this.is($target)){
+      var $submit = $(this).find("input[type=submit]");
+      $submit.loadingStart({ "className" : "concave-loading" });
+    }
+  });
+
+  $(".admin-filter-form").live('ajax:complete', function(){
+    $(this).find("input[type=submit]").loadingComplete({ "className" : "concave-loading" });
+  });
+
+  $("#course_name, #course_path").live('keyup blur', function(e){
+    var slugedPath = $(this).slug();
+    $("#course_path").val(slugedPath);
+    $("#environment-manage .course-path .course-name").html(slugedPath);
+  });
+
+  // Colorindo tabela de roles
+  $.fn.refreshRoleTable = function(){
+    return $("#environment-manage .admin-role-table tr:even").addClass("even");
+  };
+
+  $(document).ready(function(){
+    $(document).refreshRoleTable();
+
+    $(document).ajaxComplete(function(){
+      $(document).refreshRoleTable();
     });
-
-    $(".admin-filter-form").live('ajax:complete', function(){
-        $(this).find("input[type=submit]").loadingComplete({ "className" : "concave-loading" });
-    });
-
-    // Colorindo tabela de roles
-    $.fn.refreshRoleTable = function(){
-      return $("#environment-manage .admin-role-table tr:even").addClass("even");
-    };
-
-    $(document).ready(function(){
-        $(document).refreshRoleTable();
-
-        $(document).ajaxComplete(function(){
-            $(document).refreshRoleTable();
-        });
-    });
+  });
 })(jQuery);
