97220ae4998c7ee591038772e58c4cca44f887ab
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-10-10 16:50:05 -0300

    Refatorando lógica para verificar se o modelo é capaz de gerar alguma notificação (seja e-mail ou log)

--- vendor/plugins/has_friends_logs/lib/has_friends_logs.rb
+++ vendor/plugins/has_friends_logs/lib/has_friends_logs.rb
@@ -1,4 +1,8 @@
 class Friendship < ActiveRecord::Base
   has_many :logs, :as => :logeable, :order => "created_at DESC",
     :dependent => :destroy
+
+  def notificable?
+    self.accepted?
+  end
 end
