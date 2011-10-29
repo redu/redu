b269b9981709974b3108b1ac19dc5a3b26501700
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2011-10-29 16:31:39 -0300

    Falhando silenciosamente quando não houver erros. Closes #441

--- app/helpers/base_helper.rb
+++ app/helpers/base_helper.rb
@@ -56,7 +56,7 @@ module BaseHelper
 
   # Mostra todos os erros de um determinado atributo em forma de lista
   def concave_errors_for(object, method)
-    errors = object.errors.get(method).collect do |msg|
+    errors = object.errors[method].collect do |msg|
       content_tag(:li, msg)
     end.join.html_safe
 
