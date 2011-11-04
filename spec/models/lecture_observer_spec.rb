62f6fe9cadf815f517428b02f0e742453da11a55
Juliana Lucena <julianalucenaa@gmail.com>
2011-11-04 18:20:19 -0300

    Atualização do lectures_count ao criar ou deletar uma aula.

--- spec/models/lecture_observer_spec.rb
+++ spec/models/lecture_observer_spec.rb
@@ -5,13 +5,39 @@ describe LectureObserver do
     it "logs update" do
       ActiveRecord::Observer.with_observers(:lecture_observer) do
         expect {
-          sub = Factory(:subject)
+          sub = Factory(:subject, :visible => true)
           sub.finalized = true
-          sub.turn_visible!
           sub.save
           lecture = Factory(:lecture, :subject => sub,
                              :owner => sub.owner)
         }.should change(Log, :count).by(1)
+      end
+    end
+  end
+
+  context "refresh lectures_count" do
+    before do
+      @sub = Factory(:subject, :visible => true)
+      @sub.finalized = true
+      @sub.save
+    end
+
+    it "sums 1 after create" do
+      ActiveRecord::Observer.with_observers(:lecture_observer) do
+        expect {
+          lecture = Factory(:lecture, :subject => @sub,
+                            :owner => @sub.owner)
+        }.should change{ @sub.space.lectures_count }.by(1)
+      end
+    end
+
+    it "subtracts 1 after destroy" do
+      ActiveRecord::Observer.with_observers(:lecture_observer) do
+        lecture = Factory(:lecture, :subject => @sub,
+                          :owner => @sub.owner)
+        expect {
+          lecture.destroy
+        }.should change{ @sub.space.lectures_count }.by(-1)
       end
     end
   end
