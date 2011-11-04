8e33938863014a1c19bde6b52c2612d31d46bcfe
Juliana Lucena <julianalucenaa@gmail.com>
2011-11-04 19:28:48 -0300

    Removendo atualização automática da coluna lectures_count.

--- app/models/lecture_observer.rb
+++ app/models/lecture_observer.rb
@@ -1,12 +1,5 @@
 class LectureObserver < ActiveRecord::Observer
   def after_create(lecture)
     Log.setup(lecture, :action => :create)
-    space = lecture.subject.space
-    space.update_attribute(:lectures_count, space.lectures_count + 1)
-  end
-
-  def after_destroy(lecture)
-    space = lecture.subject.space
-    space.update_attribute(:lectures_count, space.lectures_count - 1)
   end
 end
--- spec/models/lecture_observer_spec.rb
+++ spec/models/lecture_observer_spec.rb
@@ -14,31 +14,4 @@ describe LectureObserver do
       end
     end
   end
-
-  context "refresh lectures_count" do
-    before do
-      @sub = Factory(:subject, :visible => true)
-      @sub.finalized = true
-      @sub.save
-    end
-
-    it "sums 1 after create" do
-      ActiveRecord::Observer.with_observers(:lecture_observer) do
-        expect {
-          lecture = Factory(:lecture, :subject => @sub,
-                            :owner => @sub.owner)
-        }.should change{ @sub.space.lectures_count }.by(1)
-      end
-    end
-
-    it "subtracts 1 after destroy" do
-      ActiveRecord::Observer.with_observers(:lecture_observer) do
-        lecture = Factory(:lecture, :subject => @sub,
-                          :owner => @sub.owner)
-        expect {
-          lecture.destroy
-        }.should change{ @sub.space.lectures_count }.by(-1)
-      end
-    end
-  end
 end
