02438a8778fb5f7b58d963cf2800ab523bfe84b5
Juliana Lucena <julianalucenaa@gmail.com>
2011-11-08 14:09:05 -0300

    Link de Subjects no Statuses de criação. Correção na animação de scroll em Subjects#show.

--- public/javascripts/subjects.js
+++ public/javascripts/subjects.js
@@ -1,31 +1,37 @@
 (function($){
+  $(document).ready(function(){
     $.refreshSubjects = function(){
       $("#seminar_external_resource").die();
       $("#seminar_external_resource").ytPreview({ titleField : ".yt-title" });
       // Scroll até o módulo que está sendo visualizado
-      $('html,body').animate({
-        scrollTop: $("#space-subjects .subjects .child .lectures:not(:hidden)").parent().offset().top - 20
-      }, "slow");
+      $('#space-subjects .subjects .child .lectures:not(:hidden)').scrollToSubject();
     }
 
     // Expand de recursos na listagem de módulos
     $(".expand, .subject-name", "#space-subjects .subjects").live("click", function(){
-        var item = $(this).parents("li:first");
-        var expand = item.find(".expand");
-        expand.toggleClass("icon-expand_down-gray_16_18");
-        expand.toggleClass("icon-expand_up-gray_16_18");
-        var itemName = item.find("> .name");
-        itemName.toggleClass("icon-content-lightblue_32_34-before");
-        itemName.toggleClass("icon-subject-lightblue_32_34-before");
-        item.find(".lectures").slideToggle("fast");
-        return false;
+      var item = $(this).parents("li:first");
+      var expand = item.find(".expand");
+      expand.toggleClass("icon-expand_down-gray_16_18");
+      expand.toggleClass("icon-expand_up-gray_16_18");
+      var itemName = item.find("> .name");
+      itemName.toggleClass("icon-content-lightblue_32_34-before");
+      itemName.toggleClass("icon-subject-lightblue_32_34-before");
+      item.find(".lectures").slideToggle("fast");
+      return false;
     });
 
-    $(document).ready(function(){
-        $.refreshSubjects();
+    $.fn.scrollToSubject = function(){
+      return this.each(function(){
+        var $this = $(this);
+        $('body').animate({
+          scrollTop: $this.parent(".child").offset().top - 20
+        }, "slow");
+      });
+    }
+    $.refreshSubjects();
 
-        $(document).ajaxComplete(function(){
-            $.refreshSubjects();
-        });
+    $(document).ajaxComplete(function(){
+      $.refreshSubjects();
     });
+  });
 })(jQuery);
