14d0b3f1a0803b1da4bbc95f7dca03da3e0ec020
Juliana Lucena <julianalucenaa@gmail.com>
2012-01-03 15:33:07 -0300

    Nova landing (por @thaisbione).

---
+++ public/javascripts/landing.js
@@ -0,0 +1,21 @@
+$(function() {
+    $("body").noisy({
+        "intensity": 1,
+        "size": "400",
+        "opacity": 0.079,
+        "fallback":"/images/t-noisy.png",
+        "monochrome": true
+    }).css("background-color", "#e6e6e6");
+
+    $("#user-login").click(function(){
+      if($(this).val() == "Digite seu login e, abaixo, sua senha"){
+        $(this).val("");
+      }
+    });
+
+    $("#user-login").blur(function(){
+      if($(this).val() == ""){
+        $(this).val("Digite seu login e, abaixo, sua senha");
+      }
+    });
+});
