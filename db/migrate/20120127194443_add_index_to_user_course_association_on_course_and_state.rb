cc7c385f017f5c84266901689b57f9c4fd751ba0
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-01-27 17:10:47 -0300

    Adicionando index em UserCourseAssociation

---
+++ db/migrate/20120127194443_add_index_to_user_course_association_on_course_and_state.rb
@@ -0,0 +1,9 @@
+class AddIndexToUserCourseAssociationOnCourseAndState < ActiveRecord::Migration
+  def self.up
+    add_index "user_course_associations", ["state", "course_id"], :name => "index_user_course_associations_on_course_and_state"
+  end
+
+  def self.down
+    remove_index "user_course_associations", :name => "index_user_course_associations_on_course_and_state"
+  end
+end
