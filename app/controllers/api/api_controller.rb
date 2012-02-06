247b5b68bc93132e3e68249cf3ebdace866ef8d4
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-02-06 08:21:04 -0300

    API REST de Environment

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,12 @@
 module Api
   class ApiController < ActionController::Base
+    respond_to :json
+    rescue_from ActiveRecord::RecordNotFound, :with => :error_404
 
+    private
+
+    def error_404
+      respond_with nil, :status => 404
+    end
   end
 end
--- app/controllers/api/environments_controller.rb
+++ app/controllers/api/environments_controller.rb
@@ -3,10 +3,41 @@ module Api
     respond_to :json
 
     def show
-      @environment = Envrionment.find(params[:id])
+      @environment = Environment.find(params[:id])
       @environment.extend(EnvironmentRepresenter)
 
       respond_with @environment
+    end
+
+    def index
+      @environments = Environment.all
+      @environments.collect { |e| e.extend(EnvironmentRepresenter) }
+
+      respond_with @environments
+    end
+
+    def destroy
+      @environment = Environment.find(params[:id])
+      @environment.extend(EnvironmentRepresenter)
+
+      @environment.destroy
+
+      respond_with @environment, :status => 200
+    end
+
+    def create
+      @environment = Environment.new(params[:environment]) do |e|
+        e.owner = current_user
+        debugger
+      end
+      @environment.extend(EnvironmentRepresenter)
+
+      if @environment.save
+        respond_with @environment
+      else
+        respond_with @environment, :status => 400
+      end
+
     end
   end
 end
