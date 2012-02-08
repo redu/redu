4c49fa799e6f7c3f145bef72248b6a29fecf3e49
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-02-08 17:24:30 -0300

    Consertando identação (estou sendo filmado pela globo fazendo de conta que trabalho)

--- app/representers/course_representer.rb
+++ app/representers/course_representer.rb
@@ -1,20 +1,20 @@
-  module CourseRepresenter
-    include Roar::Representer::JSON
-    include Roar::Representer::Feature::Hypermedia
+module CourseRepresenter
+  include Roar::Representer::JSON
+  include Roar::Representer::Feature::Hypermedia
 
-    property :id
-    property :name
-    property :description
-    property :created_at
-    property :workload
-    property :path
+  property :id
+  property :name
+  property :description
+  property :created_at
+  property :workload
+  property :path
 
-    link :self do
-      environment_course_url(self.environment, self)
-    end
-
-    link :spaces do
-      api_course_spaces_url(self)
-    end
-
+  link :self do
+    environment_course_url(self.environment, self)
   end
+
+  link :spaces do
+    api_course_spaces_url(self)
+  end
+
+end
