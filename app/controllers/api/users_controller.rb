a33f191fdb5e70d5a16f8088db60ab6d3ee981ed
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-03-05 20:35:57 -0300

    Listagem de usuários no Space com possibilidade de filtro por papel

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -4,5 +4,17 @@ module Api
       @user = User.find(params[:id])
       respond_with @user
     end
+
+    def index
+      @space = Space.find(params[:space_id])
+
+      @users = @space.users
+      if role = params[:role]
+        @users = @users.
+          where(:user_space_associations => { :role => Role[role.to_sym] })
+      end
+
+      respond_with(:api, @space, @users)
+    end
   end
 end
--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -6,6 +6,8 @@ describe "User" do
       @user = Factory(:user, :mobile => "+55 (81) 9194-5317",
                       :localization => "Recife", :birth_localization => "Recife")
       get "/api/users/#{@user.id}", :format => 'json'
+
+      @application, @current_user, @token = generate_token
     end
 
     it "should return status 200 (ok)" do
@@ -30,6 +32,66 @@ describe "User" do
       link = parse(response.body)['links'].detect { |l| l['rel'] == 'enrollments' }
 
       link.should_not be_nil
+    end
+  end
+
+  context "when GET /space/:space_id/users" do
+    before do
+      @environment = Factory(:complete_environment)
+      @course = @environment.courses.first
+      @space = @course.spaces.first
+
+      @members = 3.times.collect do
+        user = Factory(:user)
+        @course.join(user)
+        user
+      end
+      @members << @course.owner
+    end
+
+    it "should return 200" do
+      get "/api/spaces/#{@space.id}/users", :oauth_token => @token,
+        :format => 'json'
+
+      response.code.should == '200'
+    end
+
+    it "should return the correct number of users" do
+      get "/api/spaces/#{@space.id}/users", :oauth_token => @token,
+        :format => 'json'
+
+      parse(response.body).length.should == @members.length
+    end
+
+    it "should return the correct members" do
+      get "/api/spaces/#{@space.id}/users", :oauth_token => @token,
+        :format => 'json'
+
+      parse(response.body).collect { |m| m['id'] }.to_set.should ==
+        @members.collect(&:id).to_set
+    end
+
+    it "should filter by members" do
+      get "/api/spaces/#{@space.id}/users", :role => 'member',
+        :oauth_token => @token, :format => 'json'
+
+      parse(response.body).length.should == @members.length - 1
+    end
+
+    it "should filter by members" do
+      get "/api/spaces/#{@space.id}/users", :role => 'member',
+        :oauth_token => @token, :format => 'json'
+
+      parse(response.body).length.should == @members.length - 1
+    end
+  end
+
+  context "when GET /space/:space_id/user to a non-existent space" do
+    it "should return code 404 (not existent)" do
+      get "/api/spaces/2198219/users", :oauth_token => @token,
+        :format => 'json'
+
+      response.code.should == '404'
     end
   end
 end
