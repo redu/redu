3a3bc3ef9a4523d57d9061549314e93628a75fa8
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-03-21 16:22:27 -0300

    Testando melhor Myfiles. #655

--- spec/models/myfile_spec.rb
+++ spec/models/myfile_spec.rb
@@ -9,12 +9,20 @@ describe Myfile do
   end
 
   context "when creating a file with existing title in different folders" do
-    it "should not remove the previous one" do
+    it "should rename the current one" do
       Factory(:myfile, :folder => @another_folder,
               :attachment_file_name => subject.attachment_file_name)
       expect { subject.reload }.to_not raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
+  context "when creating a file with existing title in same folder" do
+    it "should not be valid" do
+      f = Factory.build(:myfile, :folder => @folder,
+              :attachment_file_name => subject.attachment_file_name)
 
+      f.valid?
+      f.errors[:attachment_file_name].should_not be_empty
+    end
+  end
 end
