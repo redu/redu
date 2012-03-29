a34048de06569561a66bc9221facb9b14c39baa0
Matheus Santana <matheus@redu.com.br>
2012-03-29 18:32:41 -0300

    Refatoração da configuração de aplicações nos provedores do omniauth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # Configuração de production environment.
-  provider :facebook, '191555477625856', '27e285f90a3ee1db7a3b61641ae14694'
-  # Configuração de development/test environment.
-  # provider :facebook, '142857189169463', 'ea0f249a4df83b250c3364ccf097f35c'
+  Redu::Application.config.omniauth.each do |service, app_info|
+    provider service, app_info[:app_id], app_info[:app_secret] 
+  end
 end
