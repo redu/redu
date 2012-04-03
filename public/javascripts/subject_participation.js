adbf971cc8df531a2dc839a5e828d51ceb046a4d
Jéssica Barbalho <jessica@redu.com.br>
2012-04-03 15:06:31 -0300

    Javascript para carregamento do bullet, inclusão de d3.chart nos assets

--- public/javascripts/subject_participation.js
+++ public/javascripts/subject_participation.js
@@ -35,7 +35,9 @@ $(document).ready(function() {
         }]
     }
 
-    var loadGraph = function(json) {
+    // Pie Chart
+    var loadGraph = function(json, incremento) {
+      options.chart.renderTo = 'subject-participation-chart'+incremento;
       options.series[0].data[0] = ['Quantidade de respostas aos pedidos de ajuda', json.answered_helps];
       options.series[0].data[1] = {
         name: 'Quantidade de pedidos de ajuda',
@@ -46,4 +48,41 @@ $(document).ready(function() {
 
       chart = new Highcharts.Chart(options);
     };
+
+    // Carregamento do Bullet Charts
+    var w = 960,
+        h = 50,
+        m = [5, 40, 20, 120];
+
+    var d3chart = d3.chart.bullet()
+        .width(w - m[1] -m[3])
+        .height(h -m[0] -m[2]);
+
+    // URL activities_d3
+    d3.json("#", function(data) {
+      var vis = d3.select("#subject-participation-chart").selectAll("svg")
+        .data(data)
+        .enter().append("svg")
+        .attr("class", "bullet")
+        .attr("width", w)
+        .attr("height", h)
+        .append("g")
+        .attr("transform", "translate(" + m[3] + "," + m[0] + ")")
+        .call(d3chart);
+
+      var title = vis.append("g")
+        .attr("text-anchor", "end")
+        .attr("transform", "translate(-6," + (h - m[0] - m[2]) / 2 + ")");
+
+      title.append("text")
+        .attr("class", "title")
+        .text();
+
+      title.append("text")
+        .attr("class", "subtitle")
+        .attr("dy", "1em")
+        .text();
+
+      chart.duration(1000);
+    });
 });
