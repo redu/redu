4af84b55c77b99a8849b9bd77661dae44fec23fe
Matheus Santana <matheus@redu.com.br>
2012-04-05 17:50:07 -0300

    Resolvido o problema do token CSRF (atualização do jquery 1.0.3 para 1.0.12 - run bundle install if necessary).

--- public/javascripts/embedly/embedly.js
+++ public/javascripts/embedly/embedly.js
@@ -4,10 +4,21 @@ $(document).ready(function(){
     var text = $(this).val();
     var regex = /(\b(((https?|ftp|file):\/\/)|(www))[\-A-Z0-9+&@#\/%?=~_|!:,.;]*[\-A-Z0-9+&@#\/%=~_|])/ig;
     var resultArray = text.match(regex);
-    if(resultArray != null){
-      link = resultArray[0];
-
-    }
+      if(resultArray != null){
+        link = resultArray[0];
+        console.log(link);
+        var url = escape(link);
+        var key = '1068f47e735911e181904040d3dc5c07';
+        var api_url = 'http://api.embed.ly/1/oembed?key=' + key + 
+                      '&url=' + url;
+                      //+ '&callback=?';
+        //jQuery JSON call
+        $.getJSON( api_url, {crossDomain:  true}, function(json) {
+            var html = json.html;
+            //$('#videodiv').html(html);
+            console.log(json);
+        });
+      }
     }
   });
 });
