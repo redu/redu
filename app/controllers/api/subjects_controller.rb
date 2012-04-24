d882f1f8056c8112eb96345f6411ac2a2af59113
Brunno Gomes <brunnolgp@gmail.com>
2012-04-24 13:18:02 -0300

    finaliza autorização de módulo

--- app/controllers/api/subjects_controller.rb
+++ app/controllers/api/subjects_controller.rb
@@ -4,17 +4,20 @@ module Api
     # GET /api/spaces/:space_id/subjects
     def index
       @space = Space.find(params[:space_id])
+      authorize! :read, @space
       @subjects = @space.try(:subjects) || []
       respond_with(:api, @subjects)
     end
 
     def show
       @subject = Subject.find(params[:id])
+      authorize! :read, @subject
       respond_with(@subject)
     end
 
     def destroy
       @subject = Subject.find(params[:id])
+      authorize! :destroy, @subject
       @subject.destroy
       respond_with(@subject)
     end
