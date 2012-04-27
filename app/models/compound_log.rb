8190982a93507441830380e4abfa79e5bc9d6465
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-04-26 23:06:57 -0300

    refactoring compound logs

--- app/models/compound_log.rb
+++ app/models/compound_log.rb
@@ -22,44 +22,15 @@ class CompoundLog < Status
   # If the interval time of compound has expired,
   # a new compound log is created
   def self.current_compostable(log, interval=24)
-
-    compound_log = case log.logeable.class.to_s
-                   when "Friendship"
-                     find_friendship_compound(log, interval)
-                   when "UserCourseAssociation"
-                     find_uca_compound(log, interval)
-                   else
-                     nil
-                   end
-  end
-
-  def self.find_friendship_compound(log, interval)
     compound_logs = CompoundLog.where(:user_id => log.user,
                                       :logeable_type => log.logeable.class.to_s)
 
     compound_log = compound_logs.order("created_at ASC").limit(1).last
 
     compound_log = nil if (compound_log and compound_log.expired?(interval))
-    compound_log ||= CompoundLog.create(:statusable => log.user,
+    compound_log ||= CompoundLog.create(:statusable => log.statusable,
                                         :compound => true,
                                         :logeable_type => log.logeable.class.to_s,
                                         :user => log.user)
   end
-
-  def self.find_uca_compound(log, interval)
-  # FIXME: modificar busca e criação do compound para uca
-  # o compound deve agrupar todos os usuários matrículados no curso
-    compound_logs = CompoundLog.where(:user_id => log.user,
-                                      :logeable_type => log.logeable.class.to_s)
-
-    compound_log = compound_logs.order("created_at ASC").limit(1).last
-
-    compound_log = nil if (compound_log and compound_log.expired?(interval))
-    compound_log ||= CompoundLog.create(:statusable => log.user,
-                                        :compound => true,
-                                        :logeable_type => log.logeable.class.to_s,
-                                        :user => log.user)
-  end
-  private_class_method :find_uca_compound
-  private_class_method :find_friendship_compound
 end
--- app/models/log_observer.rb
+++ app/models/log_observer.rb
@@ -2,7 +2,9 @@ class LogObserver < ActiveRecord::Observer
 
   def after_create(log)
     # Compõe apenas logs com logeable Friendship e UserCourseAssociation
-    job = CompoundLogJob.new(log.id)
-    Delayed::Job.enqueue(job, :queue => 'general')
+    if ["Friendship", "UserCourseAssociation"].include? log.logeable.class.to_s
+      job = CompoundLogJob.new(log.id)
+      Delayed::Job.enqueue(job, :queue => 'general')
+    end
   end
 end
