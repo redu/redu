426f2a19968bc413b5ff128bc6ebd131ca608627
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-05-04 14:40:34 -0300

    Removendo código relacionado a StudendProfile. Closes #769

--- db/migrate/20120413113457_refactoring_student_profile_and_enrollment.rb
+++ db/migrate/20120413113457_refactoring_student_profile_and_enrollment.rb
@@ -11,16 +11,8 @@ class RefactoringStudentProfileAndEnrollment < ActiveRecord::Migration
     add_index :enrollments, ["user_id", "subject_id"],
       :name => "idx_enrollments_u_id_and_sid"
 
-    # Migrando os dados de student_profile antes de remover a tabelas
-    StudentProfile.all.each do |sp|
-      sp.enrollment.update_attributes({:graduaded => sp.graduaded, :grade => sp.grade})
-    end
-
     # Alterando o usando o enrollment_id na table de asset_reports
     add_column :asset_reports, :enrollment_id, :integer
-    AssetReport.all.each do |ap|
-      ap.update_attributes({:enrollment_id => ap.student_profile.enrollment_id})
-    end
     remove_column :asset_reports, :student_profile_id
 
     drop_table :student_profiles
