ccc9de0d1d0170ecfb436839dd74101a4d1b3f93
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-05-10 13:13:28 -0300

    ajustes review @guiocavalcanti, registro dos observers e criação dos templates.

--- db/migrate/20120416211246_add_compound_and_compound_log_id_and_compound_visible_at_to_statuses.rb
+++ db/migrate/20120416211246_add_compound_and_compound_log_id_and_compound_visible_at_to_statuses.rb
@@ -10,4 +10,8 @@ class AddCompoundAndCompoundLogIdAndCompoundVisibleAtToStatuses < ActiveRecord::
     remove_column :statuses, :compound
     remove_column :statuses, :compound_visible_at, :datetime
   end
+
+  add_index :statuses, [:compound], :name => 'index_statuses_compound'
+  add_index :statuses, [:compound_log_id], :name => 'index_statuses_compound_log_id'
+  add_index :statuses, [:compound_visible_at], :name => 'index_statuses_compound_visible_at'
 end
