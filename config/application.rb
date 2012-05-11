df769943d066319a295671a2fe78c98c6d467f77
Tarcisio Coutinho <tcs5cin@gmail.com>
2012-05-10 22:58:28 -0300

    integração agrupamento ingresso no curso.

--- config/application.rb
+++ config/application.rb
@@ -196,7 +196,8 @@ module Redu
                                         :education_observer,
                                         :experience_observer,
                                         :enrollment_observer,
-                                        :log_observer]
+                                        :log_observer,
+                                        :user_course_association_observer]
     end
 
     # Redu logger
