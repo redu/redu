d80e8c8537c5e7187d5478cbd245ed01fc599922
Jéssica Barbalho <jessica@redu.com.br>
2012-05-23 14:37:13 -0300

    Preparando javascript do treemap para as requisições do server-side

--- public/javascripts/treemap.js
+++ public/javascripts/treemap.js
@@ -1,4 +1,6 @@
-var SpaceTreemap = function () {
+var StudentsTreemap = function () {
+
+  // Configurações do layout do treemap
   var w = 748,
       h = 400,
       x = d3.scale.linear().range([0, w]),
@@ -9,12 +11,14 @@ var SpaceTreemap = function () {
       root,
       node;
 
+  // Layout vazio do treemap
   var treemap = d3.layout.treemap()
     .round(false)
     .size([w, h])
     .sticky(true)
     .value(function(d) { return d.size; });
 
+  // Div onde o treemap será carregado
   var svg = d3.select(".panel .panel").append("div")
     .attr("class", "chart")
     .style("width", w + "px")
@@ -25,35 +29,9 @@ var SpaceTreemap = function () {
     .append("svg:g")
     .attr("transform", "translate(.5,.5)");
 
-  d3.json("http://localhost:3000/vis/dashboard/flare.json", function(data) {
-    node = root = buildJSON(data, data);
-
-    var nodes = treemap.nodes(root)
-    .filter(function(d) { return !d.children; });
-
-    var cell = svg.selectAll("g")
-      .data(nodes)
-     .enter().append("svg:g")
-      .attr("class", "cell")
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-      .attr("title", function (d) { return d.name })
-
-    cell.append("svg:rect")
-      .attr("width", function(d) { return d.dx - 1; })
-      .attr("height", function(d) { return d.dy - 1; })
-      .style("fill", function(d) { return fill(d.grade); });
-
-    cell.append("svg:text")
-      .attr("x", function(d) { return d.dx / 2; })
-      .attr("y", function(d) { return d.dy / 2; })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "middle")
-      .text(function(d) { return d.name; })
-      .style("opacity", function(d) { d.w = this.getComputedTextLength(); return d.dx > d.w ? 1 : 0; });
-
-    $(".cell").tipTip();
   });
 
+  // Função para preenchimento das cores da células, se grade = -1 preenchimento vazio
   var fill = function (grade){
     if (grade < -1.0) {
       return white;
@@ -79,13 +57,17 @@ var SpaceTreemap = function () {
     };
   }
 
+  // Função que retorna o tamanho da célula proveniente do JSON
   var size = function (d) {
     return d.size;
   }
 
-  var loadTreemap = function (spaceId, urlVis, urlApi) {
+  // TODO IMPLEMENTAR COM URLS VERDADEIRAS
+  // Função para carregamento do treemap, é preciso duas urls, uma que acessará a plicação de visualização
+  // obtendo os dados de participação e nota dos alunos, e uma que acessará a API do Redu obtendo as
+  // informações adicionais das células, que são os nomes dos alunos.
+  var loadTreemap = function (spaceId, "http://localhost:3000/vis/dashboard/flare.json", urlApi) {
     var url = urlVis + "?space_id=" + spaceId;
-
     // Requisição via AJAX puro para que funcione em todos os browsers
     $.ajax({
       cache: false,
@@ -95,8 +77,8 @@ var SpaceTreemap = function () {
       dataType: 'jsonp',
       success: function (jsonVis) {
 
+        // Ao obter o dado de Vis, faz logo em seguida a requisição para API
         url = urlApi;
-
         $.ajax({
           cache: false,
           crossDomain: true,
@@ -104,17 +86,20 @@ var SpaceTreemap = function () {
           method: "GET",
           dataType: 'jsonp',
           success: function (jsonApi) {
-            data = buildJSON(jsonVis, jsonApi);
-            node = root = data;
+            // Constrói o JSON característico do treemap usando os dados retornados das duas requisições
+            root = buildJSON(jsonVis, jsonApi);
 
+            // Carrega o treemap com o JSON montado
             var nodes = treemap.nodes(root)
               .filter(function(d) { return !d.children; });
 
+            // Inicialização das células com seus atributos
             var cell = svg.selectAll("g")
               .data(nodes)
               .enter().append("svg:g")
               .attr("class", "cell")
               .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+              .attr("title", function (d) { return d.name })
 
             cell.append("svg:rect")
               .attr("width", function(d) { return d.dx - 1; })
@@ -128,16 +113,22 @@ var SpaceTreemap = function () {
               .attr("text-anchor", "middle")
               .text(function(d) { return d.name; })
               .style("opacity", function(d) { d.w = this.getComputedTextLength(); return d.dx > d.w ? 1 : 0; });
+
+            $(".cell").tipTip();
           }
         })
       }
     })
   }
 
+  // TODO FUNÇÃO AINDA EM CONSTRUÇÃO
+  // Função para construção padrão do JSON do treemap
   var buildJSON = function (vis, api) {
+    // JSON conterá apenas um nó pai com seus filhos
     var json = {"name": "treemap",
                 "children": []};
 
+    // JSON característico da API
     api = [{
       "id": 1,
         "last_name": "User",
@@ -170,25 +161,31 @@ var SpaceTreemap = function () {
         "first_name": "Guilherme"
     }]
 
+    // Para cada aluno do Space eu monto um filho no JSON que será retornado
     $.each(vis.children, function (index, object) {
       var item = {};
-      var user = searchUser(1,api)[0];
+      // Busca o nome do aluno nos dados da API
+      var user = searchUser(1, api)[0];
       item.name = user.first_name + " " + user.last_name;
       item.size = object.size;
       item.grade = object.grade;
 
+      // Adiciona novo filho ao JSON
       json.children.push(item);
     })
 
     return json;
   }
 
-  var searchUser = function (user, api) {
+  // Função que busca o nome do usuário pelo seu id
+  var searchUser = function (userId, api) {
+    // O retorno conterá apenas um elemento visto que um id só pode pertencer a um usuário
     return api.filter(function (item) {
-      return item.id === user;
+      return item.id === userId;
     });
   }
 
+  // Função visível para a view, carregamento do treemap
   return {
     load: function (spaceId, url) {
       loadTreemap(spaceId, url);
