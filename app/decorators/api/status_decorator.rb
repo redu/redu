e0a3bbac216d45cce8cf0227baeaf76aa795f637
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-05-23 15:00:22 -0300

    Removendo arquivos não utilizados

--- app/decorators/api/status_decorator.rb
+++
@@ -1,9 +0,0 @@
-module StatusDecorator
-  extend ActiveSupport::Concern
-
-  def extended(base)
-    base.class_eval do
-      validates :statusable_type, :inclusion => { :in => ['Lecture'] }
-    end
-  end
-end
