67f30abd30e48242ab99d0fe63e8ba10fa4608f0
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-05-27 16:41:28 -0300

    Utilizando OauthToken extraído pelo middleware rack do oauth-plugin

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -43,19 +43,9 @@ module Api
     protected
 
     def current_user
-      token = get_header_token
-      token ||= params[:oauth_token]
-      token = OauthToken.verify_token token
+      token = env["oauth.token"]
       token.try(:user)
     end
 
-    def get_header_token
-      header_value = request.headers["HTTP_AUTHORIZATION"]
-      # matches oauth_token="dCmauo5E3GVIpi5oh6c8kBIqapFCFfnvZUE6vdds"
-      # and oauth_token = 'dCmauo5E3GVIpi5oh6c8kBIqapFCFfnvZUE6vdds'
-      reg = /oauth_token(?:\s+)?=(?:\s+)?(?:\"|\')(\w+)(?:\"|\')\s?/i
-      token = reg.match(header_value)[1] unless header_value.nil?
-      token
-    end
   end
 end
