8e81b013febb5e29697d584210a096fe08063e3d
Juliana Lucena <julianalucenaa@gmail.com>
2012-05-31 00:53:48 -0300

    Aumento no tamanho máximo da requisição (Rack). f23148e1b8fa561bb9213149d734aa772e1e38e8

--- config/initializers/rack.rb
+++ config/initializers/rack.rb
@@ -1,6 +1,6 @@
 # https://github.com/rack/rack/issues/318
 # Necessário para exercícios com mais de 20 questões e mais ou menos
-# umas 4 alternativas cada.
+# umas 4 alternativas cada. (Teste com 102 questões e 5 altenativas cada).
 if Rack::Utils.respond_to?("key_space_limit=")
-  Rack::Utils.key_space_limit = 250000
+  Rack::Utils.key_space_limit = 300000
 end
