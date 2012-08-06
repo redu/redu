b15682cdc13b1613e383c0fd89f87db07363cc80
Juliana Lucena <julianalucenaa@gmail.com>
2012-08-05 21:59:59 -0300

    Melhoria em UserCacheObserver e na lib ViewCaches.

--- lib/view_caches.rb
+++ lib/view_caches.rb
@@ -1,8 +1,14 @@
 # Este módulo contém todos os métodos necessários para expirar
 # caches de fragmentos.
 #
-# Os métodos para expirar os caches podem receber tanto um único objeto,
-# como uma lista de objetos.
+# Os métodos para expirar os caches podem receber tanto único objeto,
+# uma lista de objetos ou simplesmente vários objetos (sem a necessidade
+# de estarem em uma lista)
+#
+# Para expirar a cache do fragmento do sidebar da home (lista de Environments):
+# expire_sidebar_environments_for(user1)
+# expire_sidebar_environments_for(user1, user2)
+# expire_sidebar_environments_for([user1, user2])
 module ViewCaches
   def expire_sidebar_environments_for(users)
     expire_fragments('home_sidebar_environments', users)
@@ -29,8 +35,14 @@ module ViewCaches
   end
 
   protected
-  def expire_fragments(name, entities)
-    ([] << entities).flatten.each do |entity|
+  # Método genérico para expirar caches de fragmento para um ou
+  # mais de um elemento.
+  #
+  # expire_fragments('my_fragment_name', user1)
+  # expire_fragments('my_fragment_name', user1, user2)
+  # expire_fragments('my_fragment_name', [user1, user2])
+  def expire_fragments(name, *entities)
+    entities.flatten.each do |entity|
       ActionController::Base.new.expire_fragment([name, entity.id])
     end
   end
