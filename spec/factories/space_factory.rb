c9725126e644a2fd903faf634290a2c6b938dbfc
embs <embs@cin.ufpe.br>
2012-08-07 16:51:04 -0300

    Factories completas que demoram uma eternidade para rodar.

--- spec/factories/space_factory.rb
+++ spec/factories/space_factory.rb
@@ -1,9 +1,16 @@
 FactoryGirl.define do
+  # Disciplina simples
   factory :space do |s|
     s.sequence(:name) {|n| "Espaço #{n}"}
     s.description "Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
 
     s.association :owner, :factory => :user
     s.association :course, :factory => :course
+  end
+  # Disciplina completa, com módulos
+  factory :complete_space, :parent => :space do
+    after_create do |space|
+      space.subjects << Factory(:complete_subject, :space => space)
+    end
   end
 end
