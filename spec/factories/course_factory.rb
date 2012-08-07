ce4ed9b2e51792ca37c1b362de1e181238022c71
embs <embs@cin.ufpe.br>
2012-08-07 16:51:04 -0300

    Factories completas que demoram uma eternidade para rodar.

--- spec/factories/course_factory.rb
+++ spec/factories/course_factory.rb
@@ -1,4 +1,5 @@
 FactoryGirl.define do
+  # Curso simples
   factory :course do
     sequence(:name){ |n| "Curso #{n}" }
     sequence(:path){ |n| "curso-#{n}" }
@@ -6,13 +7,13 @@ FactoryGirl.define do
     association :environment
     association :quota, :factory => :quota
   end
-
+  # Curso completo, com disciplinas
   factory :complete_course, :parent => :course do
     description "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
     workload 40
 
     after_create do |c|
-      c.spaces << Factory(:space, :owner => c.owner, :course => c)
+      c.spaces << Factory(:complete_space, :owner => c.owner, :course => c)
     end
   end
 
--- spec/factories/subject_factory.rb
+++ spec/factories/subject_factory.rb
@@ -1,7 +1,17 @@
-Factory.define :subject do |s|
-  s.sequence(:name){ |n| "Módulo #{n}" }
-  s.description "Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
-  s.association :owner, :factory => :user
-  s.space {|sub| Factory(:space, :owner => sub.owner,
-                         :course => Factory(:course, :owner => sub.owner))}
+FactoryGirl.define do
+  # Módulo simples
+  factory :subject do |s|
+    s.sequence(:name){ |n| "Módulo #{n}" }
+    s.description "Lorem ipsum dolor sit amet, consectetur magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation"
+    s.association :owner, :factory => :user
+    s.space {|sub| Factory(:space, :owner => sub.owner,
+                           :course => Factory(:course, :owner => sub.owner))}
+  end
+  # Módulo completo, com aulas
+  factory :complete_subject, :parent => :subject do 
+    after_create do |subject|
+      subject.lectures << Factory(:lecture)
+    end
+  end
+
 end
