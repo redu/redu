cc474f8912ce373becc089a87950aece5219279e
Juliana Lucena <julianalucenaa@gmail.com>
2012-08-10 14:57:22 -0300

    Adição de shared_example para teste de cache.

---
+++ spec/support/shared_examples/cache_writing_shared_examples.rb
@@ -0,0 +1,16 @@
+shared_examples_for 'cache writing' do
+  render_views
+
+  before do
+    @controller = controller
+    @controller.stub(:current_user) { user }
+  end
+
+  it 'writes a cache' do
+    performing_cache do |cache|
+      requisition
+
+      cache.should exist(cache_identifier)
+    end
+  end
+end
