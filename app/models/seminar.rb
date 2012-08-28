0f255f369f50c9146c9d419345eae5e4508c143e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-08-28 12:44:04 -0300

    Validando ausência de URL do youtube ou URL incorreta. Closes #837

--- app/models/seminar.rb
+++ app/models/seminar.rb
@@ -53,11 +53,12 @@ class Seminar < ActiveRecord::Base
   end
   # Retorna parâmetro da URL que identifica unicamente o vídeo
   def truncate_youtube_url
-      if self.external_resource_type.eql?('youtube')
-        capture = self.external_resource.scan(/youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)[&\w;=\+_\-]*/)[0][0]
-        # TODO criar validacao pra essa url
-        self.external_resource = capture
-      end
+    if self.external_resource_type.eql?('youtube')
+      capture = self.external_resource.scan(/youtube\.com\/watch\?v=([A-Za-z0-9._%-]*)[&\w;=\+_\-]*/)
+      # Pegando texto capturado ou retornando nil se o regex falhar
+      capture = capture.try(:first).try(:first)
+      self.external_resource = capture
+    end
   end
 
   # Converte o video para FLV (Zencoder)
