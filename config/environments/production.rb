22e030307d3ffd6b4fcaed7287bcf778dcfc5d76
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-08-28 13:21:57 -0300

    Usando o sist. de arquivo p/ armazenar cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,9 @@ Redu::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Use a different cache store in production
+  config.cache_store = :file_store, "#{config.root}/tmp/cache"
+
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
@@ -23,9 +26,6 @@ Redu::Application.configure do
 
   # Use a different logger for distributed setups
   # config.logger = SyslogLogger.new
-
-  # Use a different cache store in production
-  # config.cache_store = :mem_cache_store
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
