4c88e4b32b39d8708f099b08b838410100c7a910
fltiago <fltiago@gmail.com>
2012-08-31 13:48:00 -0300

    Refatorando o EnvironmentCache e modificando a sua localização. Closes #943

--- spec/controllers/cache/environments_cache_spec.rb
+++ spec/controllers/cache/environments_cache_spec.rb
@@ -1,28 +1,31 @@
 require 'spec_helper'
 
 describe "EnvironmentsController" do
+  render_views
+  let(:user) { Factory(:user) }
+  let(:environment) { Factory(:environment, :owner => user) }
+
   before do
     @controller = EnvironmentsController.new
+    @controller.stub(:current_user) { user }
   end
-  render_views
 
   context "show" do
     before do
-      @user = Factory(:user)
-      @environment = Factory(:environment, :owner => @user)
       @courses = []
       3.times do
-        @courses << Factory(:course, :environment => @environment,
-                           :owner => @environment.owner)
+        @courses << Factory(:course, :environment => environment,
+                           :owner => environment.owner)
       end
-      controller.stub(:current_user).and_return(@user)
       @user2 = Factory(:user)
       @course = @courses.first
     end
 
+    let(:cache_identifier) { "views/course_members_count/#{@course.id}" }
+
     it "should cache course members count" do
       performing_cache do |cache|
-        get :show, :id => @environment.path, :locale => "pt-BR"
+        get :show, :id => environment.path, :locale => "pt-BR"
         @courses.each do |c|
           cache.should exist("views/course_members_count/#{c.id}")
         end
@@ -30,26 +33,22 @@ describe "EnvironmentsController" do
     end
 
     it "should expire cache when members are added" do
-      performing_cache do |cache|
-        get :show, :id => @environment.path, :locale => "pt-BR"
+      performing_cache(cache_identifier) do |cache|
         ActiveRecord::Observer.with_observers(:user_course_association_cache_observer) do
           @course.join(@user2)
-          cache.should_not \
-            exist("views/course_members_count/#{@course.id}")
+          cache.should_not exist(cache_identifier)
         end
       end
     end
 
     it "should expire cache when members are deleted" do
       @course.join(@user2)
-      performing_cache do |cache|
-        get :show, :id => @environment.path, :locale => "pt-BR"
+      performing_cache(cache_identifier) do |cache|
         ActiveRecord::Observer.with_observers(:user_course_association_cache_observer) do
           uca = UserCourseAssociation.where(:user_id => @user2.id,
                                       :course_id => @course.id).first
           uca.destroy
-          cache.should_not \
-            exist("views/course_members_count/#{@course.id}")
+          cache.should_not exist(cache_identifier)
         end
       end
     end
@@ -57,15 +56,13 @@ describe "EnvironmentsController" do
     it "should expire cache when role are changed" do
       @course.join(@user2)
 
-      performing_cache do |cache|
-        get :show, :id => @environment.path, :locale => "pt-BR"
+      performing_cache(cache_identifier) do |cache|
         ActiveRecord::Observer.with_observers(:user_course_association_cache_observer) do
           uca = UserCourseAssociation.where(:user_id => @user2.id,
                                             :course_id => @course.id).first
           uca.role = Role[:teacher]
           uca.save
-          cache.should_not \
-            exist("views/course_members_count/#{@course.id}")
+          cache.should_not exist(cache_identifier)
         end
       end
     end
