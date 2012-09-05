bb373122676ebdf95f9d06ea662d3615bab2ce66
embs <embs@cin.ufpe.br>
2012-09-05 13:34:55 -0300

    Renomeia coluna 'graduaded' para 'graduated' na tabela enrollments. Closes #952

---
+++ db/migrate/20120905135343_rename_column_graduaded_to_graduated_in_enrollments.rb
@@ -0,0 +1,9 @@
+class RenameColumnGraduadedToGraduatedInEnrollments < ActiveRecord::Migration
+  def self.up
+    rename_column :enrollments, :graduaded, :graduated
+  end
+
+  def self.down
+    rename_column :enrollments, :graduated, :graduaded
+  end
+end
