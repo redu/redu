2928842f90be70820f1354dda703a34c587788b7
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-09-06 23:48:21 -0300

    Colocando chat fora do #pjax-container

--- public/javascripts/loading.js
+++ public/javascripts/loading.js
@@ -1,9 +1,9 @@
 (function(){
   // Mensagem "Carregando..."
-  $("body").bind('start.pjax', function() {
+  $("#pjax-container").bind('start.pjax', function() {
     $('#loading-message').show();
   });
-  $("body").bind('end.pjax', function() {
+  $("#pjax-container").bind('end.pjax', function() {
     $('#loading-message').hide();
   });
 })();
