ee55bb650f12075de359c5e590f817f197bffa13
Filipe W. Lima <fwl.ufpe@gmail.com>
2012-09-24 05:56:12 -0300

    Adicionado o conteúdo do cadastro na janela modal.

--- config/compass.rb
+++ config/compass.rb
@@ -3,7 +3,3 @@ project_type = :rails
 css_dir = "public/stylesheets"
 sass_dir = "public/stylesheets/scss"
 images_dir = "public/images"
-
-config.after_initialize do
-  Sass::Plugin.options[:never_update] = true
-end
