b37d4e1b0302bfddeeee5684d7d7ac58a087f15e
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-10-23 14:37:04 -0300

    Removendo puts e atualizando Untied::Publisher

--- app/doorkeepers/base_doorkeeper.rb
+++ app/doorkeepers/base_doorkeeper.rb
@@ -4,7 +4,6 @@ class BaseDoorkeeper
   def initialize
     %w(user space environment course space subject lecture user_environment_association user_course_association enrollment).
       each do |klass|
-      puts klass.classify.constantize
       watch klass.classify.constantize, :after_create, :after_update
     end
   end
