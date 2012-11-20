b7741b7d92b4ef6fea43c6a7606aa55c8bfe99c5
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-11-20 09:54:04 -0300

    Documenta initializer do untied

--- config/initializers/untied.rb
+++ config/initializers/untied.rb
@@ -1,3 +1,10 @@
+# O Untied é responsável por propagar os eventos definidos em
+# app/doorkeepers/base_doorkeeper.rb para outros serviços. É possível debugar
+# os eventos produzidos em produção tunnelando os eventos recebidos pelo
+# RabbitMQ no walledgarden. Por exemplo:
+#   ssh -N -L 5672:127.0.0.1:5672 deploy@walledgarden.redu.com.br
+#
+# Para mais informações visite http://github.com/redu/untied
 Untied::Publisher.configure do |config|
   config.deliver_messages = !(Rails.env.test? || Rails.env.development?)
   # config.deliver_messages = true
