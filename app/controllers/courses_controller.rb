30140f456be1a204f39337d25e75557ca619571b
jessicabarbalho <jessica@redu.com.br>
2012-11-20 12:11:32 -0300

    Troca mensagem de acesso negado. Closes #1060

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,7 +13,7 @@ class CoursesController < BaseController
 
   rescue_from CanCan::AccessDenied do |exception|
     session[:return_to] = request.fullpath
-    flash[:notice] = "Você não tem acesso a essa página"
+    flash[:notice] = "Essa área só pode ser vista após você acessar o Redu com seu nome e senha."
     redirect_to preview_environment_course_path(@environment, @course)
   end
 
