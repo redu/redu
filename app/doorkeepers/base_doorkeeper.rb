60d5479e07489532de560ca9ffa56a8dbf9054a4
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-11-22 14:48:58 -0300

    Adiciona evento after_destroy ao BaseDoorkeeper

--- app/doorkeepers/base_doorkeeper.rb
+++ app/doorkeepers/base_doorkeeper.rb
@@ -4,10 +4,11 @@ class BaseDoorkeeper
   def initialize
     %w(space environment course space subject lecture user_environment_association
        user_course_association enrollment).each do |klass|
-      watch klass.classify.constantize, :after_create, :after_update
+      watch klass.classify.constantize, :after_create, :after_update, :after_destroy
     end
 
-    watch User, :after_create, :after_update, :represent_with => Untied::UserRepresenter
+    watch User, :after_create, :after_update, :after_destroy,
+      :represent_with => Untied::UserRepresenter
     watch UserSpaceAssociation, :after_create, :after_destroy
   end
 end
