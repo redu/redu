f04ba7fadb0c4b1d30f1b40d5e3dad7bdcb29dc7
embs <embs@cin.ufpe.br>
2012-12-14 15:55:47 -0300

    Separa a implementação de reindexação de entidades através dos observers SolrProfileReindexerObserver, SolrHierarchyReindexerObserver e SolrAudienceReindexerObserver.

---
+++ app/observers/solr_profile_indexer_observer.rb
@@ -0,0 +1,28 @@
+class SolrProfileIndexerObserver < ActiveRecord::Observer
+  # Observer responsável pela indexação de User
+
+  observe :user, :experience, :education, :high_school,
+    :higher_education, :complementary_course, :event_education
+
+  def after_save(object)
+    user_for(object).index!
+  end
+
+  def after_destroy(object)
+    user_for(object).index!
+  end
+
+  private
+
+  def user_for(record)
+    klass = record.class.to_s
+    if klass == "User"
+      record
+    elsif klass == "Education" || klass == "Experience"
+      record.user
+    elsif klass == "HighSchool" || klass == "HigherEducation" ||
+                   klass == "ComplementaryCourse" || klass == "EventEducation"
+      record.education.user
+    end
+  end
+end
