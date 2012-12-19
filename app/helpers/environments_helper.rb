4ba225cd91d75b96373486fa9b2b371cba625f53
jessicabarbalho <jessica@redu.com.br>
2012-12-19 17:24:03 -0300

    Dentro dos ambientes do usuário só mostra os cursos que ele tem acesso, usa helper para para verificação. Closes #1189

--- app/helpers/environments_helper.rb
+++ app/helpers/environments_helper.rb
@@ -1,2 +1,6 @@
 module EnvironmentsHelper
+  # Define os cursos que o usuário tem acesso dentro de um ambiente
+  def user_environment_courses(environment)
+    environment.courses & @user.courses
+  end
 end
