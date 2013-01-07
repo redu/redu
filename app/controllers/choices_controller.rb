1561b5633b0113e6140a0ae69c23430534af6bb4
Jéssica Barbalho <jessica@redu.com.br>
2013-01-07 11:55:41 -0300

    Submete form de choice via AJAX

--- app/controllers/choices_controller.rb
+++ app/controllers/choices_controller.rb
@@ -1,24 +1,23 @@
 class ChoicesController < BaseController
+  respond_to :js
+
   load_resource :exercise
   load_resource :question
 
   def create
-    result = @exercise.result_for(current_user, false)
+    @result = @exercise.result_for(current_user, false)
 
-    if can?(:update, result) && alternative_id = alternative_if_exists(params)
+    if can?(:update, @result) && alternative_id = alternative_if_exists(params)
       @alternative = @question.alternatives.find(alternative_id)
       @question.choose_alternative(@alternative, current_user)
     end
 
-    @next_question = previous_or_next(params[:commit], @question)
-
     respond_to do |format|
-      format.html do
-        if @next_question
-          redirect_to exercise_question_path(@exercise, @next_question)
+      format.js do
+        if params[:commit].nil?
+          render :js => "window.location = '#{ edit_exercise_result_path(@exercise, @result) }'"
         else
-          redirect_to \
-            edit_exercise_result_path(@exercise, result)
+          render :partial => "questions/choice_form", :locals => locals_form
         end
       end
     end
@@ -26,19 +25,18 @@ class ChoicesController < BaseController
 
   protected
 
-  # Retorna a próxima ou questão anterio baseada na mensagem de commit
-  # anteiror ou próxima
-  def previous_or_next(commit, current)
-    commit ||= ""
-
-    if commit.match(/Anterior/)
-      current.previous_item
-    else
-      current.next_item
-    end
-  end
-
   def alternative_if_exists(params)
     return params[:choice][:alternative_id] if params[:choice]
+  end
+
+  def locals_form
+    review = !@result.nil? || @can_manage_lecture
+    choice = @question.choices.
+      first(:conditions => { :user_id => current_user.id}) || @question.choices.build
+
+    hash = { :exercise => @exercise, :question => @question,
+             :first => @question.first_item, :last => @question.last_item,
+             :review => review, :choice => choice, :result => @result }
+    hash
   end
 end
