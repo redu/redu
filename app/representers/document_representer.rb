c6191b1b9ef5946f2a15a927a34c40397f3fad6a
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-01-18 19:16:20 -0300

    Declara links em DocumentRepresenter no fim da classe

--- app/representers/document_representer.rb
+++ app/representers/document_representer.rb
@@ -5,15 +5,15 @@ module DocumentRepresenter
 
   property :mimetype
 
+  def mimetype
+    self.lectureable.attachment_content_type
+  end
+
   link :raw do
     self.lectureable.attachment.url
   end
 
   link :scribd do
     self.lectureable.scribd_url
-  end
-
-  def mimetype
-    self.lectureable.attachment_content_type
   end
 end
