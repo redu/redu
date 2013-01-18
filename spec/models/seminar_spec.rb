2a0a7be448c25a0593fa317b5cff2e1bc8056970
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-01-18 10:23:11 -0300

    Adiciona Seminar#external_resource_url que retornar URL completa de um Seminar no Youtube

--- spec/models/seminar_spec.rb
+++ spec/models/seminar_spec.rb
@@ -12,4 +12,20 @@ describe Seminar do
   it "validates youtube URL"
   it "truncates youtube URL"
 
+  context "#external_resource_url" do
+    subject { Factory(:seminar_youtube) }
+
+    it "should return fully fladged URL" do
+      subject.external_resource_url.should == \
+        "http://www.youtube.com/watch?v=#{subject.external_resource}"
+    end
+
+    context do
+      subject { Factory(:seminar_upload) }
+      it "should return nil when there isn't a external_resource" do
+        subject.external_resource_url.should be_nil
+      end
+    end
+  end
+
 end
