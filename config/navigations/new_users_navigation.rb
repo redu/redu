d9cb693e09f63fb4f791cbc392e0f4df33c090f6
Juliana Lucena <julianalucenaa@gmail.com>
2013-01-29 11:35:17 -0300

    Adiciona sub abas na área de Aplicativos.

--- config/navigations/new_users_navigation.rb
+++ config/navigations/new_users_navigation.rb
@@ -89,14 +89,25 @@ SimpleNavigation::Configuration.run do |navigation|
         apps_tab.selected_class = 'tab-active'
 
         apps_tab.item :new_app, 'Novo Aplicativo', new_user_oauth_client_path(@user), :class => 'tab',
-          :highlights_on => action_matcher({ 'oauth_clients' => ['new', 'create'] }),
+          :highlights_on => create_action_matcher('oauth_clients'),
           :link => { :class => 'tab-title', :title => 'Novo Aplicativo' }
         apps_tab.item :my_apps, 'Meus Aplicativos', user_oauth_clients_path(@user), :class => 'tab',
           :highlights_on => action_matcher({ 'oauth_clients' => ['show', 'index', 'edit', 'update'] }),
-          :link => { :class => 'tab-title', :title => 'Meus Aplicativos' },
-          :details => { :text => 'Visualização', :class => 'tab-sub-title legend',
-                        :if => action_matcher({ 'oauth_clients' => ['show', 'edit',
-        'update'] }) }
+          :link => { :class => 'tab-title', :title => 'Meus Aplicativos' } do |apps_subtab|
+          # Sub abas
+
+          apps_subtab.dom_class = 'tab-buttons'
+          apps_subtab.selected_class = 'tab-button-active'
+
+          if @client_application && !@client_application.new_record?
+            apps_subtab.item :about, "Sobre", user_oauth_client_path(@user, @client_application),
+              :highlights_on => action_matcher({ 'oauth_clients', 'show' }),
+              :class => 'tab-button tab-button-without-icon', :title => 'Sobre'
+            apps_subtab.item :edit, "Editar", edit_user_oauth_client_path(@user, @client_application),
+              :highlights_on => update_action_matcher('oauth_clients'),
+              :class => 'tab-button tab-button-without-icon', :title => 'Editar'
+          end
+        end
       end
       sidebar.item :my_contacts, 'Meus Contatos', user_friendships_path(@user), :class => 'icon-contacts_16_18-before nav-local-item', :link => { :class => 'nav-local-link link-target', :title => 'Meus Contatos' }
     end
