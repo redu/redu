e0f7616f5fe92e377aca6ba89fe76facb94918b9
Jéssica Barbalho <jessica@redu.com.br>
2013-02-01 09:16:53 -0300

    Helper para papel do usuário no Curso

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -1,2 +1,10 @@
 module CoursesHelper
+  # Define o papel do usuário no Curso
+  def user_course_role(course, user)
+    uca = course.user_course_associations.select{
+      |assoc| assoc.user_id == user.id }.first
+
+    role = uca ? uca.role : nil
+    role
+  end
 end
--- app/models/space_search.rb
+++ app/models/space_search.rb
@@ -6,7 +6,8 @@ class SpaceSearch < Search
   def self.perform(query, format = nil, page = nil, per_page = 10)
     searcher = SpaceSearch.new
     # Instant search não necessita dos includes
-    includes = format == "json" ? [] : [{ :course => [:environment, :owner] }]
+    includes = format == "json" ? [] : [{ :course => [:user_course_associations,
+                                                      :environment, :owner] }]
 
     searcher.search({ :query => query, :page => page,
                       :per_page => per_page, :include => includes })
