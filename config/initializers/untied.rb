1b258788b5e29466f304c742d129a18d5c6c24df
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-02-05 09:15:33 -0300

    Habilita entrega de mensagens somente em produção

--- config/initializers/untied.rb
+++ config/initializers/untied.rb
@@ -6,7 +6,7 @@
 #
 # Para mais informações visite http://github.com/redu/untied
 Untied::Publisher.configure do |config|
-  config.deliver_messages = true
+  config.deliver_messages = !(Rails.env.test? || Rails.env.development?)
   config.logger = Rails.logger
   config.service_name = "core"
   config.doorkeeper = ::BaseDoorkeeper
