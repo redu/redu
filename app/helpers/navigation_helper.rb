bb13e37297cde5f16118f96c74b2efabadaba17b
Jéssica Barbalho <jessica@redu.com.br>
2013-02-25 09:57:21 -0300

    Correções do pull request. Remove função eval_context, avalia navegação apenas dentro do item que está ativado. Melhora comentários no arquivo 'navigation_helper'. Conserta sidebar de spaces passando símbolo para o método nav_local. Remove :expand_all não utilizada no render_navigation. Troca current_user.login para current_user.to_param na avaliação da url.

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -1,3 +1,10 @@
+# Módulo para renderização dinâmica das navegações.
+#
+# Aqui o contexto no qual a navegação será renderizada é
+# definido em tempo de execução removendo qualquer lógica dos controllers.
+# Os módulos específicos para cada navegação são incluídos no começo deste módulo.
+# Qualquer modificação na navegação global é feita neste arquivo, assim como
+# que área estará ativada, usando o método 'global_highlighted'.
 module NavigationHelper
   include UsersNavigation
   include UserProfileNavigation
@@ -13,57 +20,43 @@ module NavigationHelper
 
   # Renderiza navegação global
   def render_dynamic_navigation(opts = { :level => 1, :renderer => :list })
-    # method é o nome do contexto mais "_navigation" always
-    method = opts[:context].to_s + "_navigation"
+    # O método da navegação é o nome do contexto + "_navigation" always
+    navigation_method = opts[:context].to_s + "_navigation"
 
-    render_navigation(:level => opts[:level], :renderer => opts[:renderer],
-                      :expand_all => opts[:expand_all]) do |primary|
+    render_navigation(:level => opts[:level], :renderer => opts[:renderer]) do |primary|
       primary.dom_class = 'nav-global-buttons'
       primary.selected_class = 'nav-global-button-active'
 
-      # Os itens ficam ativados de acordo com a função global_highlights
+      # Os itens ficam ativados de acordo com a função global_highlighted
       # Os métodos são renderizados de acordo com seu contexto
+      #   Se o contexto for :global só renderiza a navegação global
+      #   Se for outro contexto este será renderizado dentro do item que está ativado
       primary.item :start, 'Início', home_user_path(current_user),
         :title => 'Início', :class => 'nav-global-button',
         :highlights_on => lambda{ global_highlighted == :start } do |sidebar|
-          eval_context(method, :start, sidebar)
+          unless opts[:context] == :global
+            method(navigation_method).call(sidebar) if active_navigation_item_key(:level => 1) != :courses
+          end
         end
       primary.item :teach, 'Ensine', teach_index_path,
         :title => 'Ensine', :class => 'nav-global-button'
       primary.item :courses, 'Cursos', courses_index_path,
         :title => 'Cursos', :class => 'nav-global-button',
         :highlights_on => lambda{ global_highlighted == :courses } do |sidebar|
-          eval_context(method, :courses, sidebar)
+          unless opts[:context] == :global
+            method(navigation_method).call(sidebar) if active_navigation_item_key(:level => 1) == :courses
+          end
         end
       primary.item :apps, 'Aplicativos', Redu::Application.config.redu_services[:apps][:url],
         :title => 'Aplicativos', :class => 'nav-global-button'
     end
   end
 
-  # Executa navegação dentro do seu contexto
-  # navigation_method => arquivo da navegação que será renderizado
-  # context => item primário que está sendo avaliado
-  # sidebar => parametro do item primario para continuação da navegação
-  def eval_context(navigation_method, context, sidebar)
-    # Avalia se a navegação é do Início
-    start_evaluation = (navigation_method.include?("user") ||
-                        navigation_method.include?("partner"))
-
-    # Avalia se a navegação é do Curso, se não for do Início e nem global apenas
-    course_evaluation = !(start_evaluation || (navigation_method.include? "global"))
-
-    if context == :start
-      method(navigation_method).call(sidebar) if start_evaluation
-    elsif context == :courses
-      method(navigation_method).call(sidebar) if course_evaluation
-    end
-  end
-
   # Define item ativo na navegação global, se a url não usar o sidebar
   # de pessoas, aciona o highlights para Cursos
   def global_highlighted
-    # Início só é selecionado se estiver realacionado ao current user
-    if !request.fullpath.match(%r(\A#{ users_path }/#{ current_user.login })).nil?
+    # Início só é selecionado se estiver relacionado ao current user
+    if !request.fullpath.match(%r(\A#{ users_path }/#{ current_user.to_param })).nil?
       :start
     # Não aciona a navegacão global se estiver na área relacionada a partners
     # ou ao profile de outro usuário
