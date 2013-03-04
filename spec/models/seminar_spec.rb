99cea4e73ccea978e881df90ac2fec6d3e44f870
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-03-04 11:01:05 -0300

    Garante que o código do Youtube é pego independente da ordem dos parâmetros enviados por querystring. Closes #1388

--- spec/models/seminar_spec.rb
+++ spec/models/seminar_spec.rb
@@ -20,6 +20,13 @@ describe Seminar do
         "http://www.youtube.com/watch?v=#{subject.external_resource}"
     end
 
+    it "should accept Youtube URL in any order" do
+      url = "http://www.youtube.com/watch?feature=player_embedded&v=3ZNdzDglRms"
+      Factory(:seminar_youtube, :external_resource => url).external_resource.
+        should == "3ZNdzDglRms"
+
+    end
+
     context do
       subject { Factory(:seminar_upload) }
       it "should return nil when there isn't a external_resource" do
