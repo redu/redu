e16b86906144c7abbcd96132b612b408b6dc8ee4
Juliana Lucena <julianalucenaa@gmail.com>
2013-03-06 18:36:57 -0300

    Remove lógica de autenticação de User e AuthenticationsController, passa a utilizar o AuthenticationService.

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -3,46 +3,39 @@ class AuthenticationsController < BaseController
   def create
     logger.info "Logging in with facebook" # Logging
     logger.info request.env['omniauth.auth']
+
     auth = request.env['omniauth.auth']
-    authentication = Authentication.find_by_provider_and_uid(auth[:provider],
-                                                             auth[:uid])
+    auth_service = AuthenticationService.new(:omniauth => auth)
+
     current_user = nil
-    if authentication # Autenticação já existe
-      user = authentication.user
-      @user_session = UserSession.new(user)
-      current_user = @user_session.record if @user_session.save
-      flash[:notice] = t :thanks_youre_now_logged_in
-    else # Autenticação inexistente
-      user = User.find_by_email(auth[:info][:email])
-      if user # Existe conta no Redu com o e-mail associado à conta do FB
-        user.activated_at ||= Time.now
-        user.authentications.create!(:provider => auth[:provider],
-                                     :uid => auth[:uid])
+
+    if auth_service.authenticate?
+      if auth_service.connected_accounts?
         flash[:notice] = t :facebook_connect_account_association
-      else # Não existe conta do Redu associada ao e-mail do usuário no FB
-        begin
-          user = User.create_with_omniauth(auth)
-        rescue ActiveRecord::RecordNotUnique
-          user = User.find_by_email(auth[:info][:email])
-        end
-        flash[:notice] = t :facebook_connect_new_user
+      else
+        flash[:notice] = t :thanks_youre_now_logged_in
       end
 
-      logger.info user.errors if !user.valid?
-
       # Usuário criado / atualizado com sucesso
-      @user_session = UserSession.new(user)
+      @user_session = UserSession.new(auth_service.authenticated_user)
       current_user = @user_session.record if @user_session.save
-    end
 
-    # Lida com tokens de convites
-    Authentication.handle_invitation_token(params[:state], current_user) if params[:state]
+      # Lida com tokens de convites
+      if params[:state]
+        Authentication.handle_invitation_token(params[:state], current_user)
+      end
 
-    if valid_url?(params[:state])
-      redirect_to params[:state]
+      if valid_url?(params[:state])
+        redirect_to params[:state]
+      else
+        redirect_to session[:return_to] || home_user_path(current_user)
+        session[:return_to] = nil
+      end
     else
-      redirect_to session[:return_to] || home_user_path(current_user)
-      session[:return_to] = nil
+      logger.info "Facebook with invalid user:",
+        auth_service.authenticated_user.errors
+      flash[:notice] = t :facebook_connect_error
+        redirect_to application_path
     end
   end
 
