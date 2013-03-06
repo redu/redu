f5ec8e72571d82db08b03f685b81199e198cbb20
Jéssica Barbalho <jessica@redu.com.br>
2013-03-06 09:50:27 -0300

    Garante que seminars do tipo youtube serão criados como 'converted'. Closes #1392

--- app/observers/lecture_observer.rb
+++ app/observers/lecture_observer.rb
@@ -1,5 +1,9 @@
 class LectureObserver < ActiveRecord::Observer
   def after_create(lecture)
     Log.setup(lecture, :action => :create)
+
+    if lecture.lectureable.external?
+      lecture.lectureable.update_attributes(:state => "converted")
+    end
   end
 end
