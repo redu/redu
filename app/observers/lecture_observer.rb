2f7a25a8864c8cbeea599d6284cf097bda219b46
Jéssica Barbalho <jessica@redu.com.br>
2013-03-06 15:25:30 -0300

    Cria observer para seminar, movendo lógica de lecture observer

--- app/observers/lecture_observer.rb
+++ app/observers/lecture_observer.rb
@@ -1,9 +1,5 @@
 class LectureObserver < ActiveRecord::Observer
   def after_create(lecture)
     Log.setup(lecture, :action => :create)
-
-    if lecture.lectureable.external?
-      lecture.lectureable.update_attributes(:state => "converted")
-    end
   end
 end
