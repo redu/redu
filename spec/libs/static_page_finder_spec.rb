a71393c8be48cdb6f12dfdd22e7d507bb9eb6bb0
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-03-19 17:18:08 -0300

    Refatora specs de StaticPageFinder

--- spec/libs/static_page_finder_spec.rb
+++ spec/libs/static_page_finder_spec.rb
@@ -1,25 +1,31 @@
 require 'spec_helper'
 
 describe StaticPageFinder do
+  let(:options) do
+    { :content_path => Rails.root.join("spec/support/static_page_finder_views/pages") }
+  end
+
   context "#template" do
     it "should default to pages/:layout/:page_id" do
-      StaticPageFinder.new(:page_id => "authoring").template.should == \
-        "pages/basic/authoring"
+      finder("authoring").template.should == "pages/basic/authoring"
     end
 
     it "should fallback to pages/:page_id when there is no view with page_id" do
-      StaticPageFinder.new(:page_id => "foo_bar").template.should ==\
-        "pages/foo_bar"
+      finder("foo_bar").template.should == "pages/foo_bar"
     end
   end
 
   context "#layout" do
     it "should return nil when there is no view with page_id" do
-      StaticPageFinder.new(:page_id => "foo_bar").layout.should be_nil
+      finder("foo_bar").layout.should be_nil
     end
 
     it "should return page_id dir name" do
-      StaticPageFinder.new(:page_id => "authoring").layout.should == "basic"
+      finder("authoring").layout.should == "basic"
     end
+  end
+
+  def finder(page_id)
+    StaticPageFinder.new(options.merge(:page_id => page_id))
   end
 end
