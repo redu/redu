9b58b1ac209b55eaa868380ba030811f978b91d5
Jéssica Barbalho <jessica@redu.com.br>
2013-03-20 17:46:41 -0300

    Representer para Conexão

---
+++ app/representers/connection_representer.rb
@@ -0,0 +1,27 @@
+module ConnectionRepresenter
+  include Roar::Representer::JSON
+  include Roar::Representer::Feature::Hypermedia
+
+  property :id
+  property :status
+
+  #TODO Fazer um representar menor para User
+  property :contact, :extend => UserRepresenter, :class => User
+  property :user, :extend => UserRepresenter, :class => User
+
+  def contact
+    self.friend
+  end
+
+  link :self do
+    api_connection_url(self)
+  end
+
+  link :contact do
+    api_user_url(self.user)
+  end
+
+  link :user do
+    api_user_url(self.user)
+  end
+end
