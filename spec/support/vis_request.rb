8afbbf11824937c5d05911e8b4fac80c7fac18f1
fltiago <fltiago@gmail.com>
2013-03-25 10:55:11 -0300

    Modificação dos métodos de suporte para envio de notificação para vis. Adicionando encode base::64 para as autorizações.

--- spec/support/vis_request.rb
+++ spec/support/vis_request.rb
@@ -2,16 +2,21 @@ module VisSpecHelper
   def vis_a_request(params = "")
     a_request(:post, Redu::Application.config.vis_client[:url]).
       with(:body => params,
-           :headers => {'Authorization'=>['JOjLeRjcK', 'core-team'],
+           :headers => {'Authorization'=> auth,
                         'Content-Type'=>'application/json'})
   end
 
-  def vis_stub_request
+  def vis_stub_request(params = "")
     stub_request(:post, Redu::Application.config.vis_client[:url]).
-      with(:headers => {'Authorization'=>['JOjLeRjcK', 'core-team'],
+      with(:headers => {'Authorization'=> auth,
                         'Content-Type'=>'application/json'}).
-                        to_return(:status => 200, :body => "",
-                                  :headers => {})
+      to_return(:status => 200, :body => "",
+                :headers => {})
 
+  end
+
+  private
+  def auth
+    Base64::encode64("core-team:JOjLeRjcK").chomp
   end
 end
