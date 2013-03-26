08f034be4d939468ecd202dc44898cbabb9ca9e1
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-03-26 15:29:45 -0300

    Namespacing Seminar request specs

--- spec/requests/api/lectures/seminar_spec.rb
+++ spec/requests/api/lectures/seminar_spec.rb
@@ -11,48 +11,50 @@ describe "Media API" do
     { :oauth_token => token, :format => :json }
   end
 
-  context "when external media (ie. Youtube)" do
-    subject do
-      Factory(:lecture, :lectureable => Factory(:seminar_youtube), :subject => sub)
-    end
-    before do
-      get "/api/lectures/#{subject.id}", params
-    end
+  context "GET /api/lectures/:id" do
+    context "when external media (ie. Youtube)" do
+      subject do
+        Factory(:lecture, :lectureable => Factory(:seminar_youtube), :subject => sub)
+      end
+      before do
+        get "/api/lectures/#{subject.id}", params
+      end
 
-    it_should_behave_like "a lecture"
+      it_should_behave_like "a lecture"
 
-    it "should have video/x-youtube as mimetype" do
-      parse(response.body)["mimetype"].should == "video/x-youtube"
-    end
+      it "should have video/x-youtube as mimetype" do
+        parse(response.body)["mimetype"].should == "video/x-youtube"
+      end
 
-    it "should have a link to Youtube" do
-      href_to('raw', parse(response.body)).
-        should_not be_blank
-    end
+      it "should have a link to Youtube" do
+        href_to('raw', parse(response.body)).
+          should_not be_blank
+      end
 
-    it "should have the Media type" do
-      parse(response.body)["type"].should == "Media"
-    end
-  end
-
-  context "when uploaded media" do
-    subject do
-      Factory(:lecture, :lectureable => Factory(:seminar_upload), :subject => sub)
-    end
-    before do
-      get "/api/lectures/#{subject.id}", params
+      it "should have the Media type" do
+        parse(response.body)["type"].should == "Media"
+      end
     end
 
-    it_should_behave_like "a lecture"
+    context "when uploaded media" do
+      subject do
+        Factory(:lecture, :lectureable => Factory(:seminar_upload), :subject => sub)
+      end
+      before do
+        get "/api/lectures/#{subject.id}", params
+      end
 
-    it "should have the correct mimetype" do
-      parse(response.body)["mimetype"].
-        should == subject.lectureable.original_content_type
-    end
+      it_should_behave_like "a lecture"
 
-    it "should have a link to the raw file" do
-      href_to('raw', parse(response.body)).
-        should_not be_blank
+      it "should have the correct mimetype" do
+        parse(response.body)["mimetype"].
+          should == subject.lectureable.original_content_type
+      end
+
+      it "should have a link to the raw file" do
+        href_to('raw', parse(response.body)).
+          should_not be_blank
+      end
     end
   end
 end
