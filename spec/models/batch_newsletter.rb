b570925c5c04bc3028fecacde0df082c1e09e9bd
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-04-03 14:25:20 -0300

    Cria BatchNewsletter para enviar newsletter para muitos usuários sem explodir a memória

---
+++ spec/models/batch_newsletter.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe BatchNewsletter do
+  let(:arel) { User.limit(2) }
+  subject do
+    BatchNewsletter.
+      new(:template => "newsletter/newsletter.html.erb", :users => arel)
+  end
+
+  context "#send" do
+    it "should call find_each" do
+      arel.should_receive(:find_each)
+      subject.send
+    end
+  end
+
+  context "#deliver" do
+    it "should yield controll" do
+      args = arel.collect { |u| [u.email, {}] }
+      expect { |block|
+        subject.deliver(&block)
+      }.to yield_successive_args(*args)
+    end
+  end
+end
