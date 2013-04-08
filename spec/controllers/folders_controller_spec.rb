6fb137fdf7f501b37c6a2b843f1f96e553874d0f
Jéssica Barbalho <jessica@redu.com.br>
2013-04-08 13:31:57 -0300

    Cria rspec para FoldersController

---
+++ spec/controllers/folders_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'authlogic/test_case'
+
+describe FoldersController do
+  let(:user) { Factory(:user) }
+  let(:space) { Factory(:space, :owner => user) }
+  let(:params) do
+    { :locale => 'pt-BR', :format => :html,
+      :space_id => space.id,
+      :folder => { :name => 'New folder',
+                   :parent_id => space.folders.first.id,
+                   :space_id => space.id }}
+  end
+
+  before do
+    login_as user
+  end
+
+  context "POST create" do
+    before do
+      post :create, params
+    end
+
+    it "should assigns folder" do
+      assigns[:folder].should_not be_nil
+    end
+
+    it "should update user" do
+      assigns[:folder].user.should == user
+    end
+
+    it "should update date_modified" do
+      assigns[:folder].date_modified.should be_within(0.5).of(Time.now)
+    end
+
+    it "should redirect to space_folders" do
+      parent = space.folders.first
+      response.should redirect_to space_folders_path(space, :id => parent)
+    end
+  end
+end
