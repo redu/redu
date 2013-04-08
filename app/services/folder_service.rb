f752ffcb906721fc7838dfadd89fec00696ee7b3
Jéssica Barbalho <jessica@redu.com.br>
2013-04-08 18:32:49 -0300

    Atualiza FolderService para lidar com regras de negócio associadas a remoção de folders.

--- app/services/folder_service.rb
+++ app/services/folder_service.rb
@@ -1,13 +1,19 @@
 class FolderService
 
   def initialize(opts)
+    @ability = opts.delete(:ability)
+    @quota = opts.delete(:quota)
     @attrs = opts
   end
 
-  # Cria Folder com os atributos passados na inicialização
-  # Retorna a instância de Folder.
+  # Cria Folder com os atributos passados na inicialização garantindo a
+  # autorização (:manage).
+  #
+  # Retorna a instância do MyFfile.
+  # Lança CanCan::AccessDenied caso não haja autorização
   def create(&block)
     instance = build(&block)
+    authorize!(instance)
     instance.save
     instance
   end
@@ -20,11 +26,30 @@ class FolderService
     end
   end
 
+  # Destroy Folder e garante a autorização (:manage).
+  #
+  # Retorna a instância do Folder.
+  # Lança CanCan::AccessDenied caso não haja autorização
+  def destroy(folder)
+    authorize!(folder)
+    folder.destroy
+    refresh! #só atualiza quotas na destruição
+    folder
+  end
+
   protected
 
-  attr_reader :attrs
+  attr_reader :attrs, :quota, :ability
 
   def model
     Folder
+  end
+
+  def authorize!(folder)
+    ability.authorize!(:manage, folder)
+  end
+
+  def refresh!
+    quota.refresh!
   end
 end
