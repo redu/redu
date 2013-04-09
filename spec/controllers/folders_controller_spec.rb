f5046c5f0eac305c27245487ad56f380de83b4ac
Juliana Lucena <julianalucenaa@gmail.com>
2013-04-09 15:32:34 -0300

    Utiliza FolderService em Folders#create.

--- spec/controllers/folders_controller_spec.rb
+++ spec/controllers/folders_controller_spec.rb
@@ -4,12 +4,8 @@ require 'authlogic/test_case'
 describe FoldersController do
   let(:user) { Factory(:user) }
   let(:space) { Factory(:space, :owner => user) }
-  let(:params) do
-    { :locale => 'pt-BR', :format => :js,
-      :space_id => space.id,
-      :folder => { :name => 'New folder',
-                   :parent_id => space.folders.first.id,
-                   :space_id => space.id }}
+  let(:base_params) do
+    { :locale => 'pt-BR', :format => :js, :space_id => space.id }
   end
 
   before do
@@ -17,20 +13,35 @@ describe FoldersController do
   end
 
   context "POST create" do
-    before do
+    let(:params) do
+      base_params.merge( :folder => { :name => 'New folder',
+                                      :parent_id => space.folders.first.id,
+                                      :space_id => space.id })
+    end
+
+    it "should assign folder" do
+      post :create, params
+      assigns[:folder].should == folder
+    end
+
+    it "should assing space" do
+      post :create, params
+      assigns[:space].should == space
+    end
+
+    it "should call FolderService.create" do
+      FolderService.any_instance.should_receive(:create).and_call_original
       post :create, params
     end
 
-    it "should assigns folder" do
-      assigns[:folder].should_not be_nil
-    end
-
-    it "should update user" do
+    it "should set folder#user" do
+      post :create, params
       assigns[:folder].user.should == user
     end
 
-    it "should update date_modified" do
-      assigns[:folder].date_modified.should be_within(0.5).of(Time.now)
+    it "should set folder#date_modified" do
+      post :create, params
+      assigns[:folder].date_modified.should_not be_nil
     end
   end
 end
