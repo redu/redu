035d57889cafaf10d41d0d930f0c31745a20aa9d
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-04-09 15:02:42 -0300

    Move para o bootstrap JS o foco do campo de login ao abrir dropdown de 'Entrar no Redu'.

--- public/javascripts/bootstrap-redu.js
+++ public/javascripts/bootstrap-redu.js
@@ -2224,3 +2224,20 @@ $(function() {
   })
 
 }(window.jQuery);
+$(function() {
+  var settings = {
+    // Engloba todo o botão dropdown e formulário de login.
+    buttonSignInWrapper: ".header-button-sign-in"
+    // O botão dropdown.
+  , buttonDropdown: ".dropdown-toggle"
+    // O campo de login (logicamente deve ser o primeiro).
+  , inputLogin: "input:text:first"
+  }
+
+  // Foca no primeiro campo (de login) quando o botão dropdown "Entrar no Redu" é aberto.
+  $("body").on("click", settings.buttonSignInWrapper + " " + settings.buttonDropdown, function() {
+    setTimeout(function() {
+      $(settings.buttonSignInWrapper).find(settings.inputLogin).focus()
+    }, 100)
+  })
+})
