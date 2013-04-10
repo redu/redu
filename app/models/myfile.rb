2872a32c85849b24367b6b9e3f07ddf9e070e92a
Juliana Lucena <julianalucenaa@gmail.com>
2013-04-10 13:42:02 -0300

    Move verificação de espaço do modelo Myfile para Folder.

--- app/models/myfile.rb
+++ app/models/myfile.rb
@@ -18,16 +18,4 @@ class Myfile < ActiveRecord::Base
     :less_than => 100.megabytes
   validates_uniqueness_of :attachment_file_name, :scope => 'folder_id'
   validates_attachment_content_type :attachment, :content_type => CONTENT_TYPES
-
-  # Verifica se o curso tem espaço suficiente para o arquivo
-  def can_upload_file?
-    space = self.folder.space
-    plan = space.course.plan || space.course.environment.plan
-    return false if plan.state != "active"
-
-    plan = space.course.plan || space.course.environment.plan
-    quota = space.course.quota || space.course.environment.quota
-
-    quota.files <= plan.file_storage_limit
-  end
 end
