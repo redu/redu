33b5eb438f54d45465e9e7be6ad17e95cc8c94d8
Juliana Lucena <julianalucenaa@gmail.com>
2013-04-10 13:42:02 -0300

    Move verificação de espaço do modelo Myfile para Folder.

--- app/abilites/ability.rb
+++ app/abilites/ability.rb
@@ -136,8 +136,8 @@ class Ability
       can :multiauth, User
 
       # My file
-      cannot :upload_file, Myfile do |myfile|
-        !myfile.can_upload_file?
+      cannot :upload_file, Folder do |folder|
+        !folder.can_upload_file?
       end
 
       # Join in a Course
