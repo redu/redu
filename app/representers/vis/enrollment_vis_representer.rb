4e111e8eb12482237bf3f214aaa767516f28c56a
fltiago <fltiago@gmail.com>
2013-04-12 13:53:35 -0300

    Adiciona namespace para os representers de Vis.

--- app/representers/vis/enrollment_vis_representer.rb
+++ app/representers/vis/enrollment_vis_representer.rb
@@ -1,19 +1,21 @@
-module EnrollmentVisRepresenter
-  include Roar::Representer::JSON
-  include Roar::Representer::Feature::Hypermedia
+module Vis
+  module EnrollmentVisRepresenter
+    include Roar::Representer::JSON
+    include Roar::Representer::Feature::Hypermedia
 
-  property :user_id
-  property :subject_id
-  property :space_id
-  property :course_id
-  property :created_at
-  property :updated_at
+    property :user_id
+    property :subject_id
+    property :space_id
+    property :course_id
+    property :created_at
+    property :updated_at
 
-  def space_id
-    self.subject.space.id
-  end
+    def space_id
+      self.subject.space.id
+    end
 
-  def course_id
-    self.subject.space.course.id
+    def course_id
+      self.subject.space.course.id
+    end
   end
 end
--- app/representers/vis/result_vis_representer.rb
+++ app/representers/vis/result_vis_representer.rb
@@ -1,29 +1,31 @@
-module ResultVisRepresenter
-  include Roar::Representer::JSON
-  include Roar::Representer::Feature::Hypermedia
+module Vis
+  module ResultVisRepresenter
+    include Roar::Representer::JSON
+    include Roar::Representer::Feature::Hypermedia
 
-  property :user_id
-  property :lecture_id
-  property :subject_id
-  property :space_id
-  property :course_id
-  property :grade
-  property :created_at
-  property :updated_at
+    property :user_id
+    property :lecture_id
+    property :subject_id
+    property :space_id
+    property :course_id
+    property :grade
+    property :created_at
+    property :updated_at
 
-  def lecture_id
-    self.exercise.lecture.id
-  end
+    def lecture_id
+      self.exercise.lecture.id
+    end
 
-  def subject_id
-    self.exercise.lecture.subject.id
-  end
+    def subject_id
+      self.exercise.lecture.subject.id
+    end
 
-  def space_id
-    self.exercise.lecture.subject.space.id
-  end
+    def space_id
+      self.exercise.lecture.subject.space.id
+    end
 
-  def course_id
-    self.exercise.lecture.subject.space.course.id
+    def course_id
+      self.exercise.lecture.subject.space.course.id
+    end
   end
 end
