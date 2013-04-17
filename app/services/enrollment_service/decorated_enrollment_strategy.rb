36a68b77e36031be311945b805f62fb215cc1f00
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-04-17 14:58:57 -0300

    Coloca DecoratedEnrollmentsStrategy e InferredEnrollmentStrategy no namespace EnrollmentService

--- app/services/enrollment_service/decorated_enrollment_strategy.rb
+++ app/services/enrollment_service/decorated_enrollment_strategy.rb
@@ -1,18 +1,22 @@
-class DecoratedEnrollmentsStrategy < Struct.new(:subjects, :user_role_pairs)
-  def to_a
-    decorate_with_subject_ids
-  end
+module EnrollmentService
+  class DecoratedEnrollmentsStrategy < Struct.new(:subjects, :user_role_pairs)
+    # Define estratégia para construir uma matriz do tipo
+    # [user_id, subject_id, role] baseado em uma coleção de Subject e pares
+    # [(User,Role)].
 
-  private
+    def to_a
+      decorate_with_subject_ids
+    end
 
-  def decorate_with_subject_ids
-    subjects.reduce([]) do |memo, subject|
-      user_role_pairs.each do |(user, role)|
-        memo << [user.id, subject.id, role]
+    private
+
+    def decorate_with_subject_ids
+      subjects.reduce([]) do |memo, subject|
+        user_role_pairs.each do |(user, role)|
+          memo << [user.id, subject.id, role]
+        end
+        memo
       end
-      memo
     end
   end
 end
-
-
