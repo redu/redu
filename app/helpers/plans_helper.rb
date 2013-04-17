936659d760b4df77182cd7c5faa068ee02d68033
Bruno Machado <brunoalvirubro@gmail.com>
2013-04-17 13:22:20 -0300

    Adiciona cores aos status de pagamento do plano

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,2 +1,18 @@
 module PlansHelper
+  def plan_payment_status(plan)
+    invoice = plan.invoice
+    if plan.pending_payment?
+      if invoice.overdue?
+        'overdue'
+      else
+        'pending'
+      end
+    else
+      if invoice.closed?
+        'closed'
+      else
+        'no-pending'
+      end
+    end
+  end
 end
