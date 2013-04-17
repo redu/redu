85f3140bd1420411d95eef013deabea757e41b99
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-04-17 13:17:02 -0300

    Torna possível passar uma lista de (user, role) para o método CreateEnrollment#create
    - Define estratégia para inferir os enrollments baseado no Subject e
    Users do Space
    - Define estratégia para construir o enrollments baseado numa liste de
    User e seus Roles.

--- app/services/enrollment_service.rb
+++ app/services/enrollment_service.rb
@@ -1,65 +1,103 @@
-require 'ruby-prof'
+module EnrollmentService
+  class CreateEnrollment
+    attr_reader :subjects
 
-class EnrollmentService
-  attr_reader :subjects
+    # Parâmetros:
+    #   - :subject Subject ou coleção de Subjects para os quais os Enrollments
+    #       serão criados.
+    def initialize(opts={})
+      @subjects = opts.delete(:subject)
+      @subjects = @subjects.respond_to?(:map) ? @subjects : [@subjects]
+    end
 
-  # Parâmetros:
-  #   - :subject Subject ou coleção de Subjects para os quais os Enrollments
-  #       serão criados.
-  def initialize(opts={})
-    @subjects = opts.delete(:subject)
-    @subjects = @subjects.respond_to?(:map) ? @subjects : [@subjects]
-  end
+    # Matricula usuários Subject.
+    # Parâmetros:
+    #   - Opcional: lista de pares do tipo [[User,Role]] representando os usuários
+    #     a serem matriculados. Caso não seja passado os usuários do Space serão
+    #     utilizados.
+    def create(users_and_roles=nil)
+      values = if users_and_roles
+                 DecoratedEnrollmentsStrategy.new(subjects, users_and_roles).to_a
+               else
+                 InferredEnrollmentStrategy.new(subjects).to_a
+               end
 
-  # Matricula todos os usuários do Space no Subject passado na inicialização
-  # mantendo seus papéis.
-  def create
-    import(values)
-  end
+      bulk_insert(values)
+    end
 
-  protected
+    protected
 
-  # Constrói matriz do tipo [user_id, subject_id, role] para os usuários
-  # matriculados nos spaces dos subjects passados na inicialização
-  def values
-    user_space_associations.reduce([]) do |memo, (user_id, space_id, role)|
-      space_subjects_idx[space_id].each do |subject_id|
-        memo << [user_id, subject_id, role]
+    def bulk_insert(values)
+      columns = [:user_id, :subject_id, :role]
+      options = { :validate => false, :on_duplicate_key_update => [:user_id, :role] }
+
+      Enrollment.import(columns, values, options)
+    end
+
+    class DecoratedEnrollmentsStrategy < Struct.new(:subjects, :user_role_pairs)
+      def to_a
+        decorate_with_subject_ids
       end
-      memo
+
+      private
+
+      def decorate_with_subject_ids
+        subjects.reduce([]) do |memo, subject|
+          user_role_pairs.each do |(user, role)|
+            memo << [user.id, subject.id, role]
+          end
+          memo
+        end
+      end
     end
-  end
 
-  # Contrói uma Lista de pares do tipo [subject_id, space_id] para os Subjects
-  # passados na inicialização
-  def subject_space_pairs
-    @subject_space_pairs ||= subjects.map { |s| [s.id, s.space_id] }
-  end
+    class InferredEnrollmentStrategy < Struct.new(:subjects)
+      # Define estratégia para definir uma matriz do tipo [user_id, subject_id, role]
+      # baseado em uma coleção de Subject
 
-  # Contrói um índice spaces e seus subjects
-  # {
-  #   space_id => [s1_id, s2_id],
-  #   space2_id = > [s3_id]
-  # }
-  def space_subjects_idx
-    @space_subjects_idx ||= subject_space_pairs.reduce({}) do |memo, (subj, space)|
-      memo[space] ||= []
-      memo[space] << subj
-      memo
+      def to_a
+        infer_from_subjects
+      end
+
+      private
+
+      # Constrói matriz do tipo [user_id, subject_id, role] para os usuários
+      # matriculados nos spaces dos subjects passados na inicialização
+      def infer_from_subjects
+        user_space_associations.reduce([]) do |memo, (user_id, space_id, role)|
+          space_subjects_idx[space_id].each do |subject_id|
+          memo << [user_id, subject_id, role]
+        end
+        memo
+        end
+      end
+
+      # Contrói um índice spaces e seus subjects
+      # {
+      #   space_id => [s1_id, s2_id],
+      #   space2_id = > [s3_id]
+      # }
+      def space_subjects_idx
+        @space_subjects_idx ||= subject_space_pairs.reduce({}) do |memo, (subj, space)|
+          memo[space] ||= []
+          memo[space] << subj
+          memo
+        end
+      end
+
+      def user_space_associations
+        space_ids = subject_space_pairs.map(&:last).uniq
+
+        UserSpaceAssociation.
+          where(:space_id => space_ids).values_of(:user_id, :space_id, :role)
+      end
+
+      # Contrói uma Lista de pares do tipo [subject_id, space_id] para os Subjects
+      # passados na inicialização
+      def subject_space_pairs
+        @subject_space_pairs ||= subjects.map { |s| [s.id, s.space_id] }
+      end
+
     end
-  end
-
-  def user_space_associations
-    space_ids = subject_space_pairs.map(&:last).uniq
-
-    UserSpaceAssociation.
-      where(:space_id => space_ids).values_of(:user_id, :space_id, :role)
-  end
-
-  def import(values)
-    columns = [:user_id, :subject_id, :role]
-    options = { :validate => false, :on_duplicate_key_update => [:user_id, :role] }
-
-    Enrollment.import(columns, values, options)
   end
 end
--- spec/services/enrollment_service_spec.rb
+++ spec/services/enrollment_service_spec.rb
@@ -1,34 +1,7 @@
 require 'spec_helper'
 
-describe EnrollmentService do
-  context "with one subject" do
-    let(:space) { Factory(:space) }
-    let(:usas) do
-      2.times.collect { Factory(:user_space_association, :space => space) }
-    end
-    let(:subj) { Factory(:subject, :space => space) }
-    subject { EnrollmentService.new(:subject => subj) }
-
-    context "#create" do
-      it "should delegate to import with correct arguments" do
-        columns = space.user_space_associations.map { |uca| [uca.user_id, subj.id, uca.role.to_s] }
-
-        Enrollment.should_receive(:import).
-          with([:user_id, :subject_id, :role], columns, :validate => false,
-               :on_duplicate_key_update => [:user_id, :role])
-
-          subject.create
-      end
-
-      it "should create the correct Enrollments quantity" do
-        expect {
-          subject.create
-        }.to change(Enrollment, :count).by(space.user_space_associations.count)
-      end
-    end
-  end
-
-  context "with multiple subjects" do
+module EnrollmentService
+  describe CreateEnrollment do
     let(:spaces) { 2.times.map { Factory(:space) } }
     let(:usas) do
       spaces.map do |space|
@@ -41,7 +14,7 @@ describe EnrollmentService do
       end.flatten
     end
 
-    subject { EnrollmentService.new(:subject => subjects) }
+    subject { CreateEnrollment.new(:subject => subjects) }
 
     context "#create" do
       it "should delegate to import with correct arguments" do
@@ -67,6 +40,18 @@ describe EnrollmentService do
           subject.create
         }.to change(Enrollment, :count).by(user_count)
       end
+
+      it "should accept an optional list of [[user, role]]" do
+        users = 2.times.collect { Factory(:user) }
+        user_role_pairs = users.collect { |u| [u, Role[:member]] }
+
+        subject.create(user_role_pairs)
+
+        Enrollment.where(:user_id => users.map(&:id)).count.
+          should == users.count * subjects.count
+      end
+
+      it "should not duplicate records"
     end
   end
 end
