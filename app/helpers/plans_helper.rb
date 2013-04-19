25c8dc1edc8434088fb9abe6bdac6adfaadafaa4
Bruno Machado <brunoalvirubro@gmail.com>
2013-04-19 12:26:15 -0300

    Corrige condição de helper que define cor de status do plano

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,15 +1,15 @@
-# define cor de texto de acordo com o status de pagamento do plano
 module PlansHelper
+  # define cor de texto de acordo com o status de pagamento do plano
   def plan_payment_status(plan)
     invoice = plan.invoice
     if plan.pending_payment?
-      if invoice.overdue?
+      if invoice.try(:overdue?)
         'plan-status-overdue'
       else
         'plan-status-pending'
       end
     else
-      if invoice.closed?
+      if invoice.try(:closed?)
         'plan-status-closed'
       else
         'plan-status-no-pending'
