940a1e99b54297787a1d2b436078c8a0bc9dd371
Jéssica Barbalho <jessica@redu.com.br>
2013-04-26 12:03:09 -0300

    Adiciona stub para 'total_pages' do will_paginate

--- spec/support/search.rb
+++ spec/support/search.rb
@@ -5,6 +5,7 @@ module SearchSpecHelper
     collection.stub!(:current_page).and_return(1)
     collection.stub!(:num_pages).and_return(1)
     collection.stub!(:limit_value).and_return(1)
+    collection.stub!(:total_pages).and_return(1)
 
     # Stub para resultado da busca com o sunspot
     klass.stub_chain(:perform, :results).and_return(collection)
