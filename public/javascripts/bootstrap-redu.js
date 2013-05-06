419118b41ccb4efa7dc3b5a70c56447e9ac0ea6c
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-05-06 15:09:16 -0300

    Torna toda a linha de um plano clicável.

--- public/javascripts/bootstrap-redu.js
+++ public/javascripts/bootstrap-redu.js
@@ -1455,7 +1455,7 @@ $(function() {
 
           container.on('click', function(e) {
             if (!$(e.target).is('input[type="checkbox"]')) {
-              link.click()
+              window.location = link.attr('href')
             }
           })
         })
