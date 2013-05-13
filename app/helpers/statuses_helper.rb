94487b2e68472fb74d3c50cbe1035d3a21f0b13e
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-05-13 15:03:22 -0300

    Extrai lógica da partial do tipo de log.

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -10,4 +10,17 @@ module StatusesHelper
     # Processa aspas envolvendo links e quebras de linha
     raw auto_link(h(msg).gsub(/\n/, '</br>').gsub(/&quot\;/, '"'))
   end
+
+  # Retorna o nome da partial correta de um dado tipo de log.
+  def log_partial(logeable_type)
+    case logeable_type
+    when "User" || "Education" || "Experience" then "user"
+    when "Friendship" then "friendship"
+    when "Course" then "course"
+    when "Space" then "space"
+    when "Subject" then "subject"
+    when "Lecture" then "lecture"
+    when "CourseEnrollment" then "user_course_association"
+    end
+  end
 end
