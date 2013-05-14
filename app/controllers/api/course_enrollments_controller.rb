84bea9f6ca1f9806be67d2b34a7988ea1b7174a9
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-05-14 13:18:26 -0300

    Resolve typo relacionado ao commit 8e8063568254dac68cc7d85cb6826d16ba0fef34

--- app/controllers/api/course_enrollments_controller.rb
+++ app/controllers/api/course_enrollments_controller.rb
@@ -25,7 +25,7 @@ module Api
     def index
       @entity = find_and_authorize_entity
       course_enrollments = @entity.course_enrollments.
-        includes({ :course => :enrollment }, :user)
+        includes({ :course => :environment }, :user)
       if params.has_key?(:user_id)
         course_enrollments = filter_by_course_id(course_enrollments)
       end
