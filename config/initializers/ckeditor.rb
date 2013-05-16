38a91a6535e61cec0a9fe35ca9bbdf588d8543d2
Juliana Lucena <julianalucenaa@gmail.com>
2013-05-16 11:35:09 -0300

    Move monkey patch do CKEditor para initializers/extensions.

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,26 +1,6 @@
 # Use this hook to configure ckeditor
 if Object.const_defined?("Ckeditor")
 
-  # Monkeypatch necessário para utilizar a versão 3.4.2.pre do
-  # ckeditor gem com o Rails 3.2.
-  module Ckeditor
-    mattr_accessor :file_manager_image_model
-    @@file_manager_image_model = "Ckeditor::Picture"
-
-    mattr_accessor :file_manager_file_model
-    @@file_manager_file_model = "Ckeditor::AttachmentFile"
-
-    # Get the image class from the image reference object.
-    def self.image_model
-      @@file_manager_image_model.to_s.classify.constantize
-    end
-
-    # Get the file class from the file reference object.
-    def self.file_model
-      @@file_manager_file_model.to_s.classify.constantize
-    end
-  end
-
   Ckeditor.setup do |config|
     # The file_post_name allows you to set the value name used to post the file.
     # This is not related to the file name. The default value is 'data'.
