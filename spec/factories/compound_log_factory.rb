34a71a87156c2d8760fed6a72bb5be3d9ce3b6fd
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2013-05-20 12:45:41 -0300

    Evita erro de sintaxe para ruby 1.9.3

--- spec/factories/compound_log_factory.rb
+++ spec/factories/compound_log_factory.rb
@@ -3,5 +3,5 @@ Factory.define :compound_log do |e|
   e.association :statusable, :factory => :user
   e.association :user, :factory => :user
   e.logeable_type 'User'
-  e.logs { |logs| [logs.association :log] }
+  e.logs { |logs| [logs.association(:log)] }
 end
