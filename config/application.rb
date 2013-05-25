98f3ff63367336a309b2f952eb74aaaec931a2a9
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-05-25 03:00:50 -0300

    Adiciona link do blog na configuração.

--- config/application.rb
+++ config/application.rb
@@ -271,6 +271,9 @@ module Redu
     config.redu_services[:dev] = {
       :url => "http://developers.redu.com.br/"
     }
+    config.redu_services[:blog] = {
+      :url => "http://blog.redu.com.br/"
+    }
 
     # Seta locale defaul para pt-br
     config.i18n.default_locale = :"pt-BR"
