e4e782d01a74e24fda726053f98b73c6c9fb5a6c
Filipe W. Lima <fwl.ufpe@gmail.com>
2013-05-26 02:00:29 -0300

    Atualiza o bootstrap com os ícones.

--- app/navigations/user_profile_navigation.rb
+++ app/navigations/user_profile_navigation.rb
@@ -4,16 +4,15 @@ module UserProfileNavigation
     sidebar.dom_class = 'nav-local'
     sidebar.selected_class = 'nav-local-item-active icon-arrow-right-nav-local-lightblue_11_32-after'
 
-    # TODO: Ajeitar os ícones.
     sidebar.item :user, "#{@user.display_name}", user_path(@user),
-      class: 'icon-profile-lightblue_16_18-before nav-local-item',
+      class: 'icon-profile_16_18-before nav-local-item',
       link: { class: 'nav-local-link text-truncate', title: @user.display_name }
     sidebar.item :wall, 'Mural', show_mural_user_path(@user),
-      class: 'icon-wall-lightblue_16_18-before nav-local-item',
+      class: 'icon-wall_16_18-before nav-local-item',
       link: { class: 'nav-local-link' }
     sidebar.item :members, "Contatos: #{@user.friends.count}",
       user_friendships_path(@user, profile: true),
-      class: 'icon-contacts-lightblue_16_18-before nav-local-item',
+      class: 'icon-contacts_16_18-before nav-local-item',
       link: { class: 'nav-local-link' }
   end
 end
