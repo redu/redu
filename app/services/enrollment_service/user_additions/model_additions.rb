88d9c1bbb3a30f8b539b45ed39be39c77a2e9049
Juliana Lucena <julianalucenaa@gmail.com>
2013-05-27 17:16:52 -0300

    Extrai método em EnrollmentService::UserAdditions.

--- app/services/enrollment_service/user_additions/model_additions.rb
+++ app/services/enrollment_service/user_additions/model_additions.rb
@@ -11,17 +11,17 @@ module EnrollmentService
       private
 
       def destroy_asset_reports
-        subjects_ids = self.enrollments.values_of(:subject_id)
         lectures = Lecture.where(subject_id: subjects_ids)
-
         enrollment_service.destroy_asset_report(lectures, self.enrollments)
       end
 
       def destroy_enrollments
-        subjects_ids = self.enrollments.values_of(:subject_id)
         subjects = Subject.where(id: subjects_ids)
-
         enrollment_service.destroy_enrollment(subjects, self)
+      end
+
+      def subjects_ids
+        self.enrollments.values_of(:subject_id)
       end
     end
   end
