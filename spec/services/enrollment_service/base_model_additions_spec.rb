d761be084f5ce04d6c988acea24c0bfe311a774d
Juliana Lucena <julianalucenaa@gmail.com>
2013-05-28 17:23:02 -0300

    Adiciona BaseModelAdditions a StatusService. Extrai shared example para BaseModelAdditions.

--- spec/services/enrollment_service/base_model_additions_spec.rb
+++ spec/services/enrollment_service/base_model_additions_spec.rb
@@ -1,35 +1,8 @@
 # -*- enconding : utf-8 -*-
+require 'spec_helper'
+
 module EnrollmentService
   describe BaseModelAdditions do
-    class DummyClass
-      include BaseModelAdditions
-    end
-
-    let(:dummy_class) { DummyClass }
-    let(:dummy) { DummyClass.new }
-
-    describe "#enrollment_service" do
-      let(:facade) { mock(Facade) }
-
-      before do
-        Facade.stub(:instance).and_return(facade)
-      end
-
-      it "should return the Facade" do
-        dummy.enrollment_service.should == facade
-      end
-    end
-
-    describe ".enrollment_service" do
-      let(:facade) { mock(Facade) }
-
-      before do
-        Facade.stub(:instance).and_return(facade)
-      end
-
-      it "should return the Facade" do
-        dummy_class.enrollment_service.should == facade
-      end
-    end
+    it_should_behave_like "BaseModelAdditions", EnrollmentService
   end
 end
