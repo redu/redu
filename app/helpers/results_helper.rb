82a1ba71e701749324e15a3b449bbb50b03ddc31
fltiago <fltiago@gmail.com>
2013-05-28 10:26:24 -0300

    Adiciona helper para representar completudo de um exercício.

--- app/helpers/results_helper.rb
+++ app/helpers/results_helper.rb
@@ -1,3 +1,12 @@
 # -*- encoding : utf-8 -*-
 module ResultsHelper
+  def completude_message(choices_count, questions_count)
+    content_tag(:span, class: "exercise-call") do
+      if choices_count == questions_count
+        "Parabéns você respondeu #{content_tag(:strong, "todas", class: "exercise-full")} as questões do exercício!".html_safe
+      else
+        "Você só respondeu #{choices_count} de #{pluralize questions_count, "questão", "questões"}"
+      end
+    end
+  end
 end
