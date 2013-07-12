e71ad4f003a17c88e953b6fcc0aba9133ed4baaf
filipewl <fwl.ufpe@gmail.com>
2013-07-12 14:24:28 -0300

    Atualiza o formulário de editar ambiente.

--- app/navigations/environments_admin_navigation.rb
+++ app/navigations/environments_admin_navigation.rb
@@ -4,25 +4,30 @@ module EnvironmentsAdminNavigation
     # A navegação de Environment não possui sidebar, bloco apenas para manter o padrão
     sidebar.item :sidebar, 'Sidebar', home_path do |tabs|
       # Abas
-      tabs.dom_class = 'clearfix ui-tabs-nav'
+      tabs.dom_class = 'tabs'
+      tabs.selected_class = 'tab-active'
       tabs.item :infos, 'Informações', edit_environment_path(@header_environment || @environment),
         :highlights_on => action_matcher({'environments' => ['edit', 'update']}),
-        :class => 'ui-state-default',
-        :link => { :class => "icon-bio_16_18-before" }
+        :class => 'tab',
+        :link => { :class => "tab-title icon-help-central-lightblue_16_18-before" }
       tabs.item :infos, 'Cursos', admin_courses_environment_path(@header_environment || @environment),
         :highlights_on => action_matcher({'environments' => ['admin_courses'],
                                           'courses' => ['new', 'create']}),
-        :class => 'ui-state-default',
-        :link => { :class => "icon-course_16_18-before" },
+        :class => 'tab',
+        :link => { :class => "tab-title icon-course-lightblue_16_18-before" },
         :details => { :text => 'novo', :class => 'details',
                       :if => action_matcher({'courses' => ['new', 'create']})}
-      tabs.item :members, 'Membros', admin_members_environment_path(@header_environment || @environment),
+      tabs.item :members, 'Papéis', admin_members_environment_path(@header_environment || @environment),
         :highlights_on => action_matcher({'environments' => ['admin_members'],
                                           'roles' => ['index']}),
-        :class => 'ui-state-default',
-        :link => { :class => "icon-members_16_18-before" },
+        :class => 'tab',
+        :link => { :class => "tab-title icon-members-lightblue_16_18-before" },
         :details => { :text => 'papéis', :class => 'details',
                       :if => action_matcher({'roles' => ['index']})}
+      # TODO: Link para nova aba de licenças e planos.
+      tabs.item :plans, 'Licenças / Faturas', "#",
+        class: 'tab',
+        link: { class: "tab-title icon-plan-lightblue_16_18-before" }
     end
   end
 end
