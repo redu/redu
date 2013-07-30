a7f0c156b4faaedc6354e58b3ec8bde50a49e3fb
Juliana Lucena <julianalucenaa@gmail.com>
2013-07-30 13:41:24 -0300

    Atualiza sintaxe do RSpec para expect.

--- spec/services/status_service/lecture_model_additions_spec.rb
+++ spec/services/status_service/lecture_model_additions_spec.rb
@@ -4,11 +4,11 @@ require 'spec_helper'
 module StatusService
   describe Lecture do
     it "should include BaseModelAdditions" do
-      described_class.should include(BaseModelAdditions)
+      expect(described_class).to include(BaseModelAdditions)
     end
 
     it "should include StatusableAdditions::ModelAdditions" do
-      described_class.should include(StatusableAdditions::ModelAdditions)
+      expect(described_class).to include(StatusableAdditions::ModelAdditions)
     end
   end
 end
