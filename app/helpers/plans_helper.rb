5a8e5100c3184e24d615a55805151671c51e5992
Rafael Albuquerque <rafaellsma@gmail.com>
2017-02-07 18:00:43 -0300

    Removendo metodo não utilizado do helper de plan

--- app/helpers/plans_helper.rb
+++ app/helpers/plans_helper.rb
@@ -1,10 +1,5 @@
 # -*- encoding : utf-8 -*-
 module PlansHelper
-  # Define a classe (cor de texto) de acordo com o status de pagamento do plano.
-  def plan_payment_status_class(plan)
-    'plan-status-no-pending'
-  end
-
   # Converte de bytes para megabytes.
   def bytes_to_mb(bytes)
     number_with_precision((bytes / 1.megabyte.to_f), :precision => 2)
