6244a59b93ebd306e466d71cadbcfecfdbcde955
Rafael Albuquerque <rafaellsma@gmail.com>
2017-09-28 09:36:41 -0300

    Implements conversation model

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,16 @@
 class Conversation < ActiveRecord::Base
-  attr_accessible :recipient_id, :sender_id
+  belongs_to :sender, foreign_key: :sender_id, class_name: 'User'
+  belongs_to :recipient, foreign_key: :recipient_id, class_name: 'User'
+
+  has_many :messages, dependent: :destroy
+
+  validates_uniqueness_of :sender_id, :scope => :recipient_id
+
+  def involving(user)
+    where("conversations.sender_id =? OR conversations.recipient_id =?",user.id,user.id)
+  end
+
+  def between(sender_id,recipient_id)
+    where("(conversations.sender_id = ? AND conversations.recipient_id =?) OR (conversations.sender_id = ? AND conversations.recipient_id =?)", sender_id,recipient_id, recipient_id, sender_id)
+  end
 end
