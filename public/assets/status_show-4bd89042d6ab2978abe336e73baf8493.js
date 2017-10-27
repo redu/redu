b65b8e21b68267455d5684e206b8325cbe5d8b89
Rafael Albuquerque <rafaellsma@gmail.com>
2017-10-27 11:16:26 -0300

    Remove public/assets

--- public/assets/status_show-4bd89042d6ab2978abe336e73baf8493.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  // Expande todas as respostas.
-  var $responses = $(".responses").children();
-  $responses.slideDown(150, "swing");
-  $responses.first().hide();
-  $responses.first().next().find("hr").remove();
-  $responses.last().hide();
-});
