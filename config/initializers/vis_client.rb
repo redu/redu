09a24ea9a621991b777743a8977591ca25462738
Heitorado <hscs@cin.ufpe.br>
2018-10-19 08:00:21 -0300

    Adiciona configurações para delayedjob e vis

--- config/initializers/vis_client.rb
+++ config/initializers/vis_client.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 VisClient.configure do |config|
-  config.deliver_notifications = !(Rails.env.test? || Rails.env.development?)
+  config.deliver_notifications = true
   config.logger = Rails.logger
   config.endpoint = Redu::Application.config.vis_client[:host] if (Rails.env.development? || Rails.env.test?)
 end
