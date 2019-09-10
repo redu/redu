5b93cab6c2b16580cff4e35099c36c598b4a588f
Heitor Carvalho <heitor@viitra.com>
2019-09-10 17:46:11 -0300

    Update grant_travis_privileges_on_mysql.sh

--- script/grant_travis_privileges_on_mysql.sh
+++ script/grant_travis_privileges_on_mysql.sh
@@ -2,6 +2,8 @@
 set -x
 set -e
 sudo service mysql stop || echo "mysql not stopped"
+sudo mkdir -p /var/run/mysqld
+sudo chown mysql:mysql /var/run/mysqld
 sudo  mysqld_safe &
 sleep 16
 sudo mysql -e "GRANT ALL PRIVILEGES ON openredu_test.* to 'travis'@'%';"
