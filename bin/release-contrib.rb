54559f26e8ff1d597ce5cce8c28f6420772cf473
Matheus Santana <matheus@guava.software>
2020-08-08 18:03:54 -0300

    Conserta script de contribuições pro release
    * Muda forma como os commits do release são selecionados
    
    O `git describe` não estava dando a tag mais recente.  Talvez seja por
    que, de acordo com a
    [documentação](https://git-scm.com/docs/git-describe#_description), esse
    comando só mostra "tags anotadas" -- seja lá o que isso for.
    
    * Conserta diff do release pra considerar só alguns commits
    
    Tava pegando o diff de todo o histórico. :sweat_smile:
    
    Ou seja: a tabela do release anterior tá errada (a menos que,
    coincidentemente, todas as modficações da autora tenham sido feitas nos
    commits daquela release).

--- bin/release-contrib.rb
+++ bin/release-contrib.rb
@@ -13,11 +13,11 @@ puts <<-HEADER
 |-|Commits|Arquivos modificados|Linhas adicionadas|Linhas removidas|
 |-|-|-|-|-|
 HEADER
-CSV.parse(`git shortlog HEAD...$(git describe --abbrev=0) -sn`).each do |line|
+CSV.parse(`git shortlog HEAD...$(git tag | tail -n1) -sn`).each do |line|
   commits_number, author = line.first.split("\t")
   commits_number = commits_number.gsub(/\s+/, '')
 
-  diff = `git log --shortstat --author="#{author}" | grep -E "fil(e|es) changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print files, "|", inserted, "|", deleted, "|" }'`.gsub(/\n/, '')
+  diff = `git log --shortstat --author="#{author}" HEAD...$(git tag | tail -n1) | grep -E "fil(e|es) changed" | awk '{files+=$1; inserted+=$4; deleted+=$6} END {print files, "|", inserted, "|", deleted, "|" }'`.gsub(/\n/, '')
 
   puts "|#{author}|#{commits_number}|#{diff}"
 end
